ed0a5cea5aa88b32a22df7efc60f22f86f9243d0
Martin Kielhorn <kielhorn.martin@gmail.com>
2018-10-26 01:14:32 +0200

    more code

--- example/02_qt/gen.lisp
+++ example/02_qt/gen.lisp
@@ -12,25 +12,58 @@
 		   random
 		   matplotlib))
 	 (matplotlib.use (string "Qt5Agg"))
-	 (imports (PySide2.QtWidgets
-		   PySide2.QtCore
+	 (imports ((qw PySide2.QtWidgets)
+		   (qc PySide2.QtCore)
 		   (np numpy)
 		   (pd pandas)
 		   pathlib
-		   matplotlib.backends.backend_qt5agg
-		   matplotlib.figure))
-	 (class PlotCanvas (matplotplib.backends.backend_qt5agg.FigureCanvasQTAgg)
+		   (agg matplotlib.backends.backend_qt5agg)
+		   (mf matplotlib.figure)))
+	 (class PlotCanvas (agg.FigureCanvasQTAgg)
 		(def __init__ (self &key
 				    (parent None)
 				    (width 5)
 				    (height 4)
 				    (dpi 100))
-		  (setf fig (matplotlib.figure.Figure
+		  (setf fig (mf.Figure
 			     :figsize (tuple width height)
 			     :dpi dpi)
 			self.axes (fig.add_subplot 111))
 		  (self.compute_initial_figure)
-		  (matplotplib.backends.backend_qt5agg.FigureCanvasQTAgg.__init__ self fig)
-		  (self.setParent parent))))))
+
+		  (agg.FigureCanvasQTAgg.__init__ self fig)
+		  (self.setParent parent)
+		  (agg.FigureCanvasQTAgg.setSizePolicy
+		   self qw.QSizePolicy.Expanding
+		   qw.QSizePolicy.Expanding)
+		  (agg.FigureCanvasQTAgg.updateGeometry self))
+		(def compute_initial_figure (self)
+		  pass))
+	 (class StaticCanvas (PlotCanvas)
+		(def compute_initial_figure (self)
+		  (setf t (np.arange 0 3 .01)
+			s (np.sin (* 2 pi t))
+			)
+		  (self.axes.plot t s)))
+	 (class DynamicCanvas (PlotCanvas)
+		(def __init__ (self *args **kwargs)
+		  (PlotCanvas.__init__ self *args **kwargs)
+		  (setf timer (qc.QTimer self))
+		  (timer.timeout.connect self.update_figure)
+		  (timer.start 1000))
+		(def compute_initial_figure (self)
+		  (self.axes.plot (list 0 1 2 3)
+				  (list 1 2 0 4)
+				  (string "r")))
+		(def update_figure (self)
+		  (setf l (list))
+		  (for (i (range 4))
+		       (l.append (random.randint 0 10)))
+		  (self.axes.cla)
+		  (self.axes.plot (list 0 1 2 3)
+				  l
+				  (string "r"))
+		  (self.draw)))
+	 )))
   ;(run code)
   (write-source "/home/martin/stage/cl-py-generator/example/02_qt/source/code" code))
--- example/02_qt/source/code.py
+++ example/02_qt/source/code.py
@@ -4,21 +4,56 @@ import random
 import matplotlib
 
 matplotlib.use("Qt5Agg")
-import PySide2.QtWidgets
-import PySide2.QtCore
+import PySide2.QtWidgets as qw
+import PySide2.QtCore as qc
 import numpy as np
 import pandas as pd
 import pathlib
-import matplotlib.backends.backend_qt5agg
-import matplotlib.figure
+import matplotlib.backends.backend_qt5agg as agg
+import matplotlib.figure as mf
 
-class PlotCanvas(matplotplib.backends.backend_qt5agg.FigureCanvasQTAgg):
+class PlotCanvas(agg.FigureCanvasQTAgg):
     def __init__(self, parent=None, width=5, height=4, dpi=100):
-        fig=matplotlib.figure.Figure(figsize=(width, height), dpi=dpi)
+        fig=mf.Figure(figsize=(width, height), dpi=dpi)
         self.axes=fig.add_subplot(111)
 
         self.compute_initial_figure()
-        matplotplib.backends.backend_qt5agg.FigureCanvasQTAgg.__init__(self, fig)
+        agg.FigureCanvasQTAgg.__init__(self, fig)
         self.setParent(parent)
+        agg.FigureCanvasQTAgg.setSizePolicy(self, qw.QSizePolicy.Expanding, qw.QSizePolicy.Expanding)
+        agg.FigureCanvasQTAgg.updateGeometry(self)
+
+    def compute_initial_figure(self):
+        pass
+
+
+class StaticCanvas(PlotCanvas):
+    def compute_initial_figure(self):
+        t=np.arange(0, 3, (9.999999776482582e-3))
+        s=np.sin(((2)*(pi)*(t)))
+
+        self.axes.plot(t, s)
+
+
+class DynamicCanvas(PlotCanvas):
+    def __init__(self, *args, **kwargs):
+        PlotCanvas.__init__(self, *args, **kwargs)
+        timer=qc.QTimer(self)
+
+        timer.timeout.connect(self.update_figure)
+        timer.start(1000)
+
+    def compute_initial_figure(self):
+        self.axes.plot([0, 1, 2, 3], [1, 2, 0, 4], "r")
+
+    def update_figure(self):
+        l=[]
+
+        for i in range(4):
+            l.append(random.randint(0, 10))
+
+        self.axes.cla()
+        self.axes.plot([0, 1, 2, 3], l, "r")
+        self.draw()
 
 
