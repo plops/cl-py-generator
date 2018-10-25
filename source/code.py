d0d6d838f2391fb5c85f71ad95d283c4ff1914d9
Martin Kielhorn <kielhorn.martin@gmail.com>
2018-10-25 20:10:50 +0200

    add the generated python file

---
+++ source/code.py
@@ -0,0 +1,12 @@
+import sys
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import pathlib
+
+plt.ion()
+x=np.linspace(0, (2.e+0), 30)
+y=np.sin(x)
+
+plt.plot(x, y)
+plt.grid()
