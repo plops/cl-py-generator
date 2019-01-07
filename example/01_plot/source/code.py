c23611758ed4f5c14ed89efd31eb4a2e45118fc1
Martin Kielhorn <kielhorn.martin@gmail.com>
2019-01-07 22:46:42 +0100

    rerun generator on example code

--- example/01_plot/source/code.py
+++ example/01_plot/source/code.py
@@ -3,10 +3,8 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 import pathlib
-
 plt.ion()
 x=np.linspace(0, (2.e+0), 30)
 y=np.sin(x)
-
 plt.plot(x, y)
 plt.grid()
--- example/02_qt/source/code.py
+++ example/02_qt/source/code.py
@@ -2,7 +2,6 @@ import sys
 import os
 import random
 import matplotlib
-
 matplotlib.use("Qt5Agg")
 import PySide2.QtWidgets as qw
 import PySide2.QtCore as qc
@@ -11,52 +10,37 @@ import pandas as pd
 import pathlib
 import matplotlib.backends.backend_qt5agg as agg
 import matplotlib.figure as mf
-
 class PlotCanvas(agg.FigureCanvasQTAgg):
     def __init__(self, parent=None, width=5, height=4, dpi=100):
-        fig=mf.Figure(figsize=(width, height), dpi=dpi)
+        fig=mf.Figure(figsize=(width,height,), dpi=dpi)
         self.axes=fig.add_subplot(111)
-
         self.compute_initial_figure()
         agg.FigureCanvasQTAgg.__init__(self, fig)
         self.setParent(parent)
         agg.FigureCanvasQTAgg.setSizePolicy(self, qw.QSizePolicy.Expanding, qw.QSizePolicy.Expanding)
         agg.FigureCanvasQTAgg.updateGeometry(self)
-
     def compute_initial_figure(self):
         pass
-
-
 class StaticCanvas(PlotCanvas):
     def compute_initial_figure(self):
         t=np.arange(0, 3, (9.999999776482582e-3))
         s=np.sin(((2)*(np.pi)*(t)))
-
         self.axes.plot(t, s)
-
-
 class DynamicCanvas(PlotCanvas):
     def __init__(self, *args, **kwargs):
         PlotCanvas.__init__(self, *args, **kwargs)
         timer=qc.QTimer(self)
-
         timer.timeout.connect(self.update_figure)
         timer.start(1000)
-
     def compute_initial_figure(self):
         self.axes.plot([0, 1, 2, 3], [1, 2, 0, 4], "r")
-
     def update_figure(self):
         l=[]
-
         for i in range(4):
             l.append(random.randint(0, 10))
-
         self.axes.cla()
         self.axes.plot([0, 1, 2, 3], l, "r")
         self.draw()
-
-
 class ApplicationWindow(qw.QMainWindow):
     def __init__(self):
         qw.QMainWindow.__init__(self)
@@ -65,15 +49,11 @@ class ApplicationWindow(qw.QMainWindow):
         l=qw.QVBoxLayout(self.main_widget)
         sc=StaticCanvas(self.main_widget, width=5, height=4, dpi=100)
         dc=DynamicCanvas(self.main_widget, width=5, height=4, dpi=100)
-
         l.addWidget(sc)
         l.addWidget(dc)
         self.main_widget.setFocus()
         self.setCentralWidget(self.main_widget)
-
-
 qApp=qw.QApplication(sys.argv)
 aw=ApplicationWindow()
-
 aw.show()
 sys.exit(qApp.exec_())
