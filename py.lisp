6ccdb7f1350b5edbeb24fc941a55bb97d3a1e68c
Martin Kielhorn <kielhorn.martin@gmail.com>
2019-01-07 10:19:15 +0100

    add string3

--- py.lisp
+++ py.lisp
@@ -179,6 +179,7 @@
 	      (or (let ((args (cdr code)))
 		    (format nil "(~{(~a)~^ or ~})" (mapcar #'emit args))))
 	      (string (format nil "\"~a\"" (cadr code)))
+	      (string3 (format nil "\"\"\"~a\"\"\"" (cadr code)))
 	      (return_ (format nil "return ~a" (emit (caadr code))))
 	      (return (let ((args (cdr code)))
 			(format nil "~a" (emit `(return_ ,args)))))
