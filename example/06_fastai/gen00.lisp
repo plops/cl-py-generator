ec226239f992c9613c4cdc7aedd780c87c92473a
Martin Kielhorn <kielhorn.martin@gmail.com>
2019-10-18 03:59:33 +0200

    29:00

--- example/06_fastai/gen00.lisp
+++ example/06_fastai/gen00.lisp
@@ -12,8 +12,9 @@
 
   (let* ((code
 	  `(do0
+	    "# lesson 1: image classification"
+	    "# export LANG=en_US.utf8"
 	    
-					
 	    #+nil(do0
 	     (imports (matplotlib))
 			      ;(matplotlib.use (string "Agg"))
@@ -39,9 +40,24 @@
 
 	    "from fastai import *"
 	    "from fastai.vision import *"
-
+	    "from PIL import Image"
 	    (setf path (untar_data URLs.PETS))
-	    (print path)
+	    (print (dot (string "using image data from: {}")
+			(format path)))
+	    (np.random.seed 2)
+	    (do0
+	     "# %%"
+	     (setf (ntuple path_anno path_img) (path.ls)
+		   fnames (get_image_files path_img)
+		   pat (rstring3 "/([^/]+)_\\d+.jpg$")
+		   data (ImageDataBunch.from_name_re path_img
+						     fnames
+						     pat
+						     :ds_tfms (get_transforms)
+						     :size 224))
+	     (data.normalize imagenet_stats))
+	    
+	    
 	    ))
 	 )
     (write-source (format nil "~a/source/~a" *path* *code-file*) code)
--- example/06_fastai/source/run_00_pets.py
+++ example/06_fastai/source/run_00_pets.py
@@ -1,4 +1,14 @@
+# lesson 1: image classification
+# export LANG=en_US.utf8
 from fastai import *
 from fastai.vision import *
+from PIL import Image
 path=untar_data(URLs.PETS)
-print(path)
+print("using image data from: {}".format(path))
+np.random.seed(2)
+# %%
+path_anno, path_img=path.ls()
+fnames=get_image_files(path_img)
+pat=r"""/([^/]+)_\d+.jpg$"""
+data=ImageDataBunch.from_name_re(path_img, fnames, pat, ds_tfms=get_transforms(), size=224)
+data.normalize(imagenet_stats)
