607530b58256d1b6773ed0dfcd765eff5b7b02a9
Martin Kielhorn <kielhorn.martin@gmail.com>
2019-10-18 06:12:31 +0200

    first train
    martin@labolg ~/stage/cl-py-generator/example/06_fastai/source $ python -i run_00_pets.py
    python-exec: Invalid impl in /etc/python-exec/python-exec.conf: python3.4
    /usr/lib64/python3.6/site-packages/torchvision-0.5.0a0+53b062c-py3.6-linux-x86_64.egg/torchvision/io/_video_opt.py:17: UserWarning: video reader based on ffmpeg c++ ops not available
    using image data from: /home/martin/.fastai/data/oxford-iiit-pet
    epoch     train_loss  valid_loss  error_rate  time
    0         1.370178    0.337561    0.111637    04:36
    Epoch 2/4 :
    |âââââââââââââââââââââââââââââââââââââââââââââââââââââââââ-------------------------------------------|
    57.61% [53/92 02:27<01:48 0.8108]
    
    martin@labolg ~ $ sudo nvidia-smi
    Fri Oct 18 06:13:13 2019
    +-----------------------------------------------------------------------------+
    | NVIDIA-SMI 375.26                 Driver Version: 435.21                    |
    |-------------------------------+----------------------+----------------------+
    | GPU  Name        Persistence-M| Bus-Id        Disp.A | Volatile Uncorr. ECC |
    | Fan  Temp  Perf  Pwr:Usage/Cap|         Memory-Usage | GPU-Util  Compute M. |
    |===============================+======================+======================|
    |   0  GeForce RTX 206...  Off  | 0000:07:00.0      On |                  N/A |
    | 29%   30C    P8    11W / 175W |    520MiB /  7981MiB |     17%      Default |
    +-------------------------------+----------------------+----------------------+
    
    +-----------------------------------------------------------------------------+
    | Processes:                                                       GPU Memory |
    |  GPU       PID  Type  Process name                               Usage      |
    |=============================================================================|
    Internal error
    
    hmm. looks like the gpu isn't doing anything

--- example/06_fastai/gen00.lisp
+++ example/06_fastai/gen00.lisp
@@ -14,8 +14,9 @@
 	  `(do0
 	    "# lesson 1: image classification"
 	    "# export LANG=en_US.utf8"
+	    "# https://github.com/fastai/course-v3/blob/master/nbs/dl1/lesson1-pets.ipynb"
 	    
-	    #+nil(do0
+	    (do0
 	     (imports (matplotlib))
 			      ;(matplotlib.use (string "Agg"))
 			      (imports ((plt matplotlib.pyplot)))
@@ -40,11 +41,13 @@
 
 	    "from fastai import *"
 	    "from fastai.vision import *"
-	    "from PIL import Image"
+	    "from fastai.metrics import error_rate"
+	    ;"from PIL import Image"
 	    (setf path (untar_data URLs.PETS))
 	    (print (dot (string "using image data from: {}")
 			(format path)))
 	    (np.random.seed 2)
+	    (setf bs 64)
 	    (do0
 	     "# %%"
 	     (setf (ntuple path_anno path_img) (path.ls)
@@ -54,10 +57,18 @@
 						     fnames
 						     pat
 						     :ds_tfms (get_transforms)
-						     :size 224))
+						     :size 224
+						     :bs bs))
 	     (data.normalize imagenet_stats))
-	    
-	    
+	    (def look ()
+	     (do0
+	      (data.show_batch :rows 3)
+	      (print data.classes)))
+	    (setf learn (cnn_learner data
+				     models.resnet34
+				     :metrics error_rate))
+	    (learn.fit_one_cycle 4)
+	    (learn.save (string "save-1"))
 	    ))
 	 )
     (write-source (format nil "~a/source/~a" *path* *code-file*) code)
--- example/06_fastai/source/run_00_pets.py
+++ example/06_fastai/source/run_00_pets.py
@@ -1,14 +1,25 @@
 # lesson 1: image classification
 # export LANG=en_US.utf8
+# https://github.com/fastai/course-v3/blob/master/nbs/dl1/lesson1-pets.ipynb
+import matplotlib
+import matplotlib.pyplot as plt
+plt.ion()
 from fastai import *
 from fastai.vision import *
-from PIL import Image
+from fastai.metrics import error_rate
 path=untar_data(URLs.PETS)
 print("using image data from: {}".format(path))
 np.random.seed(2)
+bs=64
 # %%
 path_anno, path_img=path.ls()
 fnames=get_image_files(path_img)
 pat=r"""/([^/]+)_\d+.jpg$"""
-data=ImageDataBunch.from_name_re(path_img, fnames, pat, ds_tfms=get_transforms(), size=224)
+data=ImageDataBunch.from_name_re(path_img, fnames, pat, ds_tfms=get_transforms(), size=224, bs=bs)
 data.normalize(imagenet_stats)
+def look():
+    data.show_batch(rows=3)
+    print(data.classes)
+learn=cnn_learner(data, models.resnet34, metrics=error_rate)
+learn.fit_one_cycle(4)
+learn.save("save-1")
