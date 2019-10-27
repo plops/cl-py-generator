e75193265ebf812b478421e556e913f99c8ba8a6
Martin Kielhorn <kielhorn.martin@gmail.com>
2019-10-27 19:42:30 +0100

    drawing a row of circles works

--- example/08_glumpy/gen01.lisp
+++ example/08_glumpy/gen01.lisp
@@ -36,119 +36,120 @@
      `(do
 					;"varying vec4 v_color;"
 					;"varying vec4 v_position;"
-       "varying vec4 v_center;"
+       "varying vec2 v_center;"
        "varying float v_radius;"
-	(defun distance (p center radius)
-	  (declare (type vec2 p center)
-		   (type float radius)
-		   (values float))
-	  (return (- (length (- p center))
-		     radius)))
-	(defun SDF_circle (p radius)
-	  (declare (type vec2 p)
-		   (type float radius)
-		   (values float))
-	  (return (- (length p)
-		     radius)))
-	(defun SDF_plane (p p0 p1)
-	  (declare (type vec2 p p0 p1)
-		   (values float))
-	  (let ((tt (- p1 p0))
-		(o (normalize (vec2 tt.y (- tt.x)))))
-	    (declare (type vec2 tt o))
-	    (return ("dot" o (- p0 p)))))
-	(defun SDF_box (p size)
-	  (declare (type vec2 p size)
-		   (values float))
-	  (let ((d (- (abs p) size))
-		)
-	    (declare (type vec2 d))
-	    (return (+ (min (max d.x d.y)
-			    0s0)
-		       (length (max d 0s0))))))
-	(defun SDF_round_box (p size radius)
-	  (declare (type vec2 p size)
-		   (type float radius)
-		   (values float))
-	  (return (- (SDF_box p size)
-		     radius)))
-	(defun SDF_fake_box (p size)
-	  (declare (type vec2 p size)
-		   (values float))
-	  (return (max (- (abs p.x) size.x)
-		       (- (abs p.y) size.y))))
-	(defun SDF_triangle (p p0 p1 p2)
-	  (declare (type vec2 p p0 p1 p2)
-		   (values float))
-	  ,@(loop for (e f) in `((1 0)
-				 (2 1)
-				 (0 2)) and i from 0
-	       collect
-		 (let ((name (format nil "e~a" i)))
-		   `(let ((,name
-			   (- ,(format nil "p~a" e)
-			      ,(format nil "p~a" f))))
-		      (declare (type vec2 ,name)))))
-	  ,@(loop for (e f) in `(("" 0)
-				 ("" 1)
-				 ("" 2)) and i from 0
-	       collect
-		 (let ((name (format nil "v~a" i)))
-		   `(let ((,name
-			   (- ,(format nil "p~a" e)
-			      ,(format nil "p~a" f))))
-		      (declare (type vec2 ,name)))))
-	  ,@(loop for i below 3 collect
-		 (let ((name (format nil "pq~a" i))
-		       (v (format nil "v~a" i))
-		       (e (format nil "e~a" i)))
-		   `(let ((,name
-			   (- ,v
-			      (* ,e
-				 (clamp (/ ("dot" ,v ,e)
-					   ("dot" ,e ,e))
-					0s0 1s0)))))
-		      (declare (type vec2 ,name)))))
-	  (let ((s (sign (- (* e0.x e2.y)
-			    (* e0.y e2.x))))
-		)
-	    y	       (declare 
+	#+nil (do0
+	 (defun distance (p center radius)
+	   (declare (type vec2 p center)
+		    (type float radius)
+		    (values float))
+	   (return (- (length (- p center))
+		      radius)))
+	 (defun SDF_circle (p radius)
+	   (declare (type vec2 p)
+		    (type float radius)
+		    (values float))
+	   (return (- (length p)
+		      radius)))
+	 (defun SDF_plane (p p0 p1)
+	   (declare (type vec2 p p0 p1)
+		    (values float))
+	   (let ((tt (- p1 p0))
+		 (o (normalize (vec2 tt.y (- tt.x)))))
+	     (declare (type vec2 tt o))
+	     (return ("dot" o (- p0 p)))))
+	 (defun SDF_box (p size)
+	   (declare (type vec2 p size)
+		    (values float))
+	   (let ((d (- (abs p) size))
+		 )
+	     (declare (type vec2 d))
+	     (return (+ (min (max d.x d.y)
+			     0s0)
+			(length (max d 0s0))))))
+	 (defun SDF_round_box (p size radius)
+	   (declare (type vec2 p size)
+		    (type float radius)
+		    (values float))
+	   (return (- (SDF_box p size)
+		      radius)))
+	 (defun SDF_fake_box (p size)
+	   (declare (type vec2 p size)
+		    (values float))
+	   (return (max (- (abs p.x) size.x)
+			(- (abs p.y) size.y))))
+	 (defun SDF_triangle (p p0 p1 p2)
+	   (declare (type vec2 p p0 p1 p2)
+		    (values float))
+	   ,@(loop for (e f) in `((1 0)
+				  (2 1)
+				  (0 2)) and i from 0
+		collect
+		  (let ((name (format nil "e~a" i)))
+		    `(let ((,name
+			    (- ,(format nil "p~a" e)
+			       ,(format nil "p~a" f))))
+		       (declare (type vec2 ,name)))))
+	   ,@(loop for (e f) in `(("" 0)
+				  ("" 1)
+				  ("" 2)) and i from 0
+		collect
+		  (let ((name (format nil "v~a" i)))
+		    `(let ((,name
+			    (- ,(format nil "p~a" e)
+			       ,(format nil "p~a" f))))
+		       (declare (type vec2 ,name)))))
+	   ,@(loop for i below 3 collect
+		  (let ((name (format nil "pq~a" i))
+			(v (format nil "v~a" i))
+			(e (format nil "e~a" i)))
+		    `(let ((,name
+			    (- ,v
+			       (* ,e
+				  (clamp (/ ("dot" ,v ,e)
+					    ("dot" ,e ,e))
+					 0s0 1s0)))))
+		       (declare (type vec2 ,name)))))
+	   (let ((s (sign (- (* e0.x e2.y)
+			     (* e0.y e2.x))))
+		 )
+	     y	       (declare 
 			(type float s)))
-	  ,@(loop for i below 3 collect
-		 (let ((name (format nil "vv~a" i))
-		       (v (format nil "v~a" i))
-		       (e (format nil "e~a" i))
-		       (pq (format nil "pq~a" i)))
-		   `(let ((,name
-			   (vec2 ("dot" ,pq ,pq)
-				 (* s (- (* (dot ,v x) (dot ,e y))
-					 (* (dot ,v y) (dot ,e x)))))))
-		      (declare (type vec2 ,name)))))
-	  (let (
-		(d (min (min vv0 vv1)
-			vv2))
-		)
-	    (declare (type vec2 d)
-		     )
-	    (return (* (- (sqrt d.x))
-		       (sign d.y)))))
-	
-	
-	
-	(defun color (d)
-	  (declare (type float d)
-		   (values vec4))
-	  (let ((white (vec3 1 1 1))
-		(blue (vec3 .1 .4 .7))
-		(color (- white (* (sign d) blue))))
-	    (declare (type vec3 white blue color))
-	    (setf color (* color
-			   (- 1s0
-			      (* (exp (* -4s0 (abs d)))
-				 (+ .8s0 (* .2s0 (cos (* 140s0 d))))))))
-	    (setf color (mix color white
-			     (- 1s0 (smoothstep 0s0 .02s0 (abs d)))))
-	    (return (vec4 color 1s0))))
+	   ,@(loop for i below 3 collect
+		  (let ((name (format nil "vv~a" i))
+			(v (format nil "v~a" i))
+			(e (format nil "e~a" i))
+			(pq (format nil "pq~a" i)))
+		    `(let ((,name
+			    (vec2 ("dot" ,pq ,pq)
+				  (* s (- (* (dot ,v x) (dot ,e y))
+					  (* (dot ,v y) (dot ,e x)))))))
+		       (declare (type vec2 ,name)))))
+	   (let (
+		 (d (min (min vv0 vv1)
+			 vv2))
+		 )
+	     (declare (type vec2 d)
+		      )
+	     (return (* (- (sqrt d.x))
+			(sign d.y)))))
+	 
+	 
+	 
+	 (defun color (d)
+	   (declare (type float d)
+		    (values vec4))
+	   (let ((white (vec3 1 1 1))
+		 (blue (vec3 .1 .4 .7))
+		 (color (- white (* (sign d) blue))))
+	     (declare (type vec3 white blue color))
+	     (setf color (* color
+			    (- 1s0
+			       (* (exp (* -4s0 (abs d)))
+				  (+ .8s0 (* .2s0 (cos (* 140s0 d))))))))
+	     (setf color (mix color white
+			      (- 1s0 (smoothstep 0s0 .02s0 (abs d)))))
+	     (return (vec4 color 1s0)))))
 	(defun main ()
 	  (let ((p (- gl_FragCoord.xy v_center))
 		(a 1s0)
@@ -160,7 +161,7 @@
 	    (setf d (abs d))
 	    (when (< 0s0 d)
 	      (setf a (exp (* -1 d d))))
-	    (setf gl_FragColor (vec3 (vec3 0s0) a))))))))
+	    (setf gl_FragColor (vec4 (vec3 0s0) a))))))))
 
 
 
--- example/08_glumpy/source/run_01_window.py
+++ example/08_glumpy/source/run_01_window.py
@@ -20,56 +20,8 @@ vertex="""        uniform vec2 resolution;
         gl_PointSize=(((2.e+0f))+(ceil((((2.e+0f))*(radius)))));
                         gl_Position=vec4((((-1.e+0f))+((((2.e+0f))*(((center)/(resolution)))))), (0.0e+0f), (1.e+0f));
 }"""
-fragment="""        varying vec4 v_center;
+fragment="""        varying vec2 v_center;
         varying float v_radius;
-        float distance (vec2 p, vec2 center, float radius){
-                return ((length(((p)-(center))))-(radius));
-}
-        float SDF_circle (vec2 p, float radius){
-                return ((length(p))-(radius));
-}
-        float SDF_plane (vec2 p, vec2 p0, vec2 p1){
-                        vec2 tt  = ((p1)-(p0));
-        vec2 o  = normalize(vec2(tt.y, (-(tt.x))));
-        return dot(o, ((p0)-(p)));
-}
-        float SDF_box (vec2 p, vec2 size){
-                        vec2 d  = ((abs(p))-(size));
-        return ((min(max(d.x, d.y), (0.0e+0f)))+(length(max(d, (0.0e+0f)))));
-}
-        float SDF_round_box (vec2 p, vec2 size, float radius){
-                return ((SDF_box(p, size))-(radius));
-}
-        float SDF_fake_box (vec2 p, vec2 size){
-                return max(((abs(p.x))-(size.x)), ((abs(p.y))-(size.y)));
-}
-        float SDF_triangle (vec2 p, vec2 p0, vec2 p1, vec2 p2){
-                        vec2 e0  = ((p1)-(p0));
-                        vec2 e1  = ((p2)-(p1));
-                        vec2 e2  = ((p0)-(p2));
-                        vec2 v0  = ((p)-(p0));
-                        vec2 v1  = ((p)-(p1));
-                        vec2 v2  = ((p)-(p2));
-                        vec2 pq0  = ((v0)-(((e0)*(clamp(((dot(v0, e0))/(dot(e0, e0))), (0.0e+0f), (1.e+0f))))));
-                        vec2 pq1  = ((v1)-(((e1)*(clamp(((dot(v1, e1))/(dot(e1, e1))), (0.0e+0f), (1.e+0f))))));
-                        vec2 pq2  = ((v2)-(((e2)*(clamp(((dot(v2, e2))/(dot(e2, e2))), (0.0e+0f), (1.e+0f))))));
-                        auto s  = sign(((((e0.x)*(e2.y)))-(((e0.y)*(e2.x)))));
-        y;
-        declare(type(float, s));
-                        vec2 vv0  = vec2(dot(pq0, pq0), ((s)*(((((v0.x)*(e0.y)))-(((v0.y)*(e0.x)))))));
-                        vec2 vv1  = vec2(dot(pq1, pq1), ((s)*(((((v1.x)*(e1.y)))-(((v1.y)*(e1.x)))))));
-                        vec2 vv2  = vec2(dot(pq2, pq2), ((s)*(((((v2.x)*(e2.y)))-(((v2.y)*(e2.x)))))));
-                        vec2 d  = min(min(vv0, vv1), vv2);
-        return (((-(sqrt(d.x))))*(sign(d.y)));
-}
-        vec4 color (float d){
-                        vec3 white  = vec3(1, 1, 1);
-        vec3 blue  = vec3((1.e-1f), (4.e-1f), (7.e-1f));
-        vec3 color  = ((white)-(((sign(d))*(blue))));
-                color=((color)*((((1.e+0f))-(((exp((((-4.e+0f))*(abs(d)))))*((((8.e-1f))+((((2.e-1f))*(cos((((1.4e+2f))*(d)))))))))))));
-                color=mix(color, white, (((1.e+0f))-(smoothstep((0.0e+0f), (1.9999999e-2f), abs(d)))));
-        return vec4(color, (1.e+0f));
-}
         void main (){
                         vec2 p  = ((gl_FragCoord.xy)-(v_center));
         float a  = (1.e+0f);
@@ -78,7 +30,7 @@ fragment="""        varying vec4 v_center;
         if ( (0.0e+0f)<d ) {
                                                 a=exp(((-1)*(d)*(d)));
 };
-                gl_FragColor=vec3(vec3((0.0e+0f)), a);
+                gl_FragColor=vec4(vec3((0.0e+0f)), a);
 }"""
 points=gloo.Program(vertex, fragment)
 points.bind(V.view(gloo.VertexBuffer))
