c342cda1db2513a6e9ac1d962f9f93024bcd8a54
Martin Kielhorn <kielhorn.martin@gmail.com>
2019-10-27 12:53:20 +0100

    start with opengl shader example

---
+++ example/08_glumpy/gen00.lisp
@@ -0,0 +1,40 @@
+(eval-when (:compile-toplevel :execute :load-toplevel)
+  (ql:quickload "cl-py-generator"))
+(in-package :cl-py-generator)
+
+
+(progn
+  (defparameter *path* "/home/martin/stage/cl-py-generator/example/08_glumpy")
+  (defparameter *code-file* "run_00_window")
+  (defparameter *source* (format nil "~a/source/~a" *path* *code-file*))
+  (defparameter *inspection-facts*
+    `((10 "")))
+
+  (let* ((code
+	  `(do0
+	    (do0
+	     #+nil (imports (matplotlib))
+			      ;(matplotlib.use (string "Agg"))
+			      (imports ((plt matplotlib.pyplot)))
+			 (plt.ion))
+	    
+	    (imports (			;os
+					;sys
+					;time
+					;docopt
+					;pathlib
+			    (np numpy)
+					;serial
+					;(pd pandas)
+					;(xr xarray)
+					;(xrp xarray.plot)
+					;skimage.restoration
+					;(u astropy.units)
+					;scipy.ndimage
+					;scipy.optimize
+			    ))
+
+	    )))
+    (write-source (format nil "~a/source/~a" *path* *code-file*) code)))
+
+
