ec8d91a8bdf520e75b430b991bf108e13eebd829
Martin Kielhorn <kielhorn.martin@gmail.com>
2019-10-28 18:19:45 +0100

    start linestrip

---
+++ example/08_glumpy/gen04.lisp
@@ -0,0 +1,206 @@
+(eval-when (:compile-toplevel :execute :load-toplevel)
+  (ql:quickload "cl-py-generator")
+  (ql:quickload "cl-cpp-generator2"))
+
+(in-package :cl-cpp-generator2)
+
+(progn
+  (defparameter *vertex-code* (cl-cpp-generator2::emit-c
+			       :code
+			       `(do
+				 "uniform vec2 resolution;"
+				 "uniform float antialias;"
+				  "attribute float thickness;"
+				  "attribute vec2 p0, p1, uv;"
+				  "varying float v_alpha, v_thickness;"
+				  "varying vec2 v_p0, v_p1, v_p;"
+				  (defun main ()
+				    ;; handle thin lines with v_alpha
+				    (if (< (abs thickness) 1s0)
+					(setf v_thickness 1s0
+					      v_alpha (abs thickness))
+					(setf v_thickness (abs thickness)
+					      v_alpha 1s0))
+				    
+				    (let (;; half of the width that the shader will touch
+					  (tt (+ (/ v_thickness 2s0)
+						 antialias ))
+					  ;; length of the segment (without caps)
+					  (l ;(length (- p0 p1))
+					     (distance p0 p1)
+					     )
+					  ;; u in [0..1] .. distance along segment
+					  ;; u<0, u>l    .. cap area
+					  (u (- (* 2s0 uv.x) 1s0))
+					  ;; coordinate of line thickness
+					  (v (- (* 2s0 uv.y) 1s0))
+					  ;; unit vector tangential to segment
+					  (TT (normalize (- p1 p0)))
+					  ;; unit vector normal to segment 
+					  (O (vec2 -TT.y TT.x))
+					  (p (+ p0
+						(vec2 .5s0 .5s0)
+						(* uv.x TT l)
+						(* u TT tt)
+						(* v O tt))))
+				      (declare (type float tt l u v)
+					       (type vec2 TT O p))
+				      (setf gl_Position (vec4 (- (/ (* 2s0 p)
+								    resolution)
+								 1s0)
+							      0s0 1s0))
+				      (do0 "// local space"
+					   (setf TT (vec2 1s0 0s0)
+						 O (vec2 0s0 1s0)
+						 p (+ (* uv.x TT l)
+						      (* u TT tt)
+						      (* v O tt)))
+					   (setf v_p0 (vec2 0s0 0s0)
+						 v_p1 (vec2 1s0 0s0)
+						 v_p p)))
+				    
+				    ))))
+  (defparameter *fragment-code*
+    (cl-cpp-generator2::emit-c
+     :code
+     `(do
+       "uniform float antialias;"
+       "varying float v_alpha, v_thickness;"
+	"varying vec2 v_p0, v_p1, v_p;"
+	
+	
+	(defun main ()
+	  (let ((d 0s0)
+		(offset (+ (/ v_thickness -2s0)
+			   (/ antialias 2s0))))
+	    (declare (type float d offset))
+	    ;; compute signed distance to envelope
+	    (if (< v_p.x 0)
+		(setf d (+ (distance v_p v_p0)
+			   offset))
+		(if (< (distance v_p1 v_p0)
+		       v_p.x)
+		    (setf d (+ (distance v_p v_p1)
+			       offset))
+		    (setf d (+ (abs v_p.y)
+			       offset))))
+	    (if (< d 0)
+		(setf gl_FragColor (vec4 1s0 0s0 0s0 v_alpha)
+		      )
+		(when (< d antialias)
+			(setf d (exp (* -d d))
+			      gl_FragColor (vec4 0s0 .2s0 0s0 (* v_alpha d)))))))))))
+
+
+
+(in-package :cl-py-generator)
+
+(progn
+  
+  
+  (defparameter *path* "/home/martin/stage/cl-py-generator/example/08_glumpy")
+  (defparameter *code-file* "run_04_linestrip")
+  (defparameter *source* (format nil "~a/source/~a" *path* *code-file*))
+  (defparameter *inspection-facts*
+    `((10 "")))
+
+  (let* (
+	 
+	 (code
+	  `(do0
+	    "# https://www.labri.fr/perso/nrougier/python-opengl/code/chapter-09/linestrip.py"
+	    "# https://www.labri.fr/perso/nrougier/python-opengl/#id39"
+	    #+nil (do0
+	     (imports (matplotlib))
+					;(matplotlib.use (string "Agg"))
+	     (imports ((plt matplotlib.pyplot)))
+	     (plt.ion))
+	    
+	     (imports (			;os
+					;sys
+					;time
+					;docopt
+					;pathlib
+		      (np numpy)
+					;serial
+					;(pd pandas)
+					;(xr xarray)
+					;(xrp xarray.plot)
+					;skimage.restoration
+					;(u astropy.units)
+					;scipy.ndimage
+					;scipy.optimize
+		      ))
+	    "from glumpy import app, gloo, gl"
+	    (do0
+	     (setf vertex (string3 ,cl-cpp-generator2::*vertex-code*)
+		   fragment (string3 ,cl-cpp-generator2::*fragment-code*)
+		   
+		   )
+	     (app.use (string "glfw"))
+	     (setf window (app.Window 1200 400 :color (tuple 1 1 1 1)))
+	     
+	     (setf n 100
+		   V (np.zeros (tuple n 4)
+			       :dtype (list (tuple (string "p0") np.float32 2)
+					    (tuple (string "p1") np.float32 2)
+					    (tuple (string "uv") np.float32 2)
+					    (tuple (string "thickness") np.float32 1)
+					    ))
+		   
+		   (aref V (string "uv")) (tuple (tuple 0 0)
+						 (tuple 0 1)
+						 (tuple 1 0)
+						 (tuple 1 1))
+		   (aref V (string "thickness")) (dot (np.linspace .1s0 8s0 n)
+						      (reshape n 1))
+		   )
+	     #+nil((aref V (string "p0")) (dot (np.dstack
+					   (tuple (np.linspace 100 1100 n)
+						  (* (np.ones n) 50)))
+					  (reshape n 1 2))
+	      (aref V (string "p1")) (dot (np.dstack
+					   (tuple (np.linspace 110 1110 n)
+						  (* (np.ones n) 350)))
+					  (reshape n 1 2)))
+		   
+		   ,@(loop for i below 2 and e in `((100 1100 50)
+						    (100 1110 350))
+			collect
+			  (destructuring-bind (xstart xend y) e
+			    `(setf (aref V (string ,(format nil "p~a" i)))
+				   (dot (np.dstack
+					 (tuple
+					  (np.linspace ,xstart ,xend n)
+					  (* (np.ones n) ,y)))
+					(reshape n 1 2)))))
+		   
+	     (setf segments (gloo.Program vertex fragment :count (* 4 n)))
+	     (segments.bind (dot V
+				 (ravel)
+				 (view gloo.VertexBuffer)))
+	     (setf (aref segments (string "antialias")) 2s0)
+
+	     (setf I (np.zeros (tuple n 6) :dtype np.uint32)
+		   (aref I ":") (list 0 1 2 1 2 3)
+		   
+		   I (+ I (* 4 (dot (np.arange n :dtype np.uint32)
+				    (reshape n 1))))
+		   I (dot I (ravel) (view gloo.IndexBuffer)))
+	     (do0
+	      "@window.event"
+	      (def on_resize (width height)
+		(setf (aref segments (string "resolution"))
+		      (tuple width height))))
+	     (do0
+	      "@window.event"
+	      (def on_draw (dt)
+		(window.clear)
+		(segments.draw gl.GL_TRIANGLES I)))
+	     (app.run))
+	    
+	    )))
+    (cl-py-generator::write-source (format nil "~a/source/~a" *path* *code-file*) code)))
+
+
+ 
---
+++ example/08_glumpy/source/run_04_linestrip.py
@@ -0,0 +1,81 @@
+# https://www.labri.fr/perso/nrougier/python-opengl/code/chapter-09/linestrip.py
+# https://www.labri.fr/perso/nrougier/python-opengl/#id39
+import numpy as np
+from glumpy import app, gloo, gl
+vertex="""        uniform vec2 resolution;
+        uniform float antialias;
+        attribute float thickness;
+        attribute vec2 p0, p1, uv;
+        varying float v_alpha, v_thickness;
+        varying vec2 v_p0, v_p1, v_p;
+        void main (){
+                if ( abs(thickness)<(1.e+0f) ) {
+                                    v_thickness=(1.e+0f);
+            v_alpha=abs(thickness);
+} else {
+                                    v_thickness=abs(thickness);
+            v_alpha=(1.e+0f);
+}
+                        float tt  = ((((v_thickness)/((2.e+0f))))+(antialias));
+        float l  = distance(p0, p1);
+        float u  = (((((2.e+0f))*(uv.x)))-((1.e+0f)));
+        float v  = (((((2.e+0f))*(uv.y)))-((1.e+0f)));
+        vec2 TT  = normalize(((p1)-(p0)));
+        vec2 O  = vec2(-TT.y, TT.x);
+        vec2 p  = ((p0)+(vec2((5.e-1f), (5.e-1f)))+(((uv.x)*(TT)*(l)))+(((u)*(TT)*(tt)))+(((v)*(O)*(tt))));
+                gl_Position=vec4((((((((2.e+0f))*(p)))/(resolution)))-((1.e+0f))), (0.0e+0f), (1.e+0f));
+                // local space
+                TT=vec2((1.e+0f), (0.0e+0f));
+        O=vec2((0.0e+0f), (1.e+0f));
+        p=((((uv.x)*(TT)*(l)))+(((u)*(TT)*(tt)))+(((v)*(O)*(tt))));
+                v_p0=vec2((0.0e+0f), (0.0e+0f));
+        v_p1=vec2((1.e+0f), (0.0e+0f));
+        v_p=p;
+}"""
+fragment="""        uniform float antialias;
+        varying float v_alpha, v_thickness;
+        varying vec2 v_p0, v_p1, v_p;
+        void main (){
+                        float d  = (0.0e+0f);
+        float offset  = ((((v_thickness)/((-2.e+0f))))+(((antialias)/((2.e+0f)))));
+        if ( v_p.x<0 ) {
+                                    d=((distance(v_p, v_p0))+(offset));
+} else {
+                        if ( distance(v_p1, v_p0)<v_p.x ) {
+                                                d=((distance(v_p, v_p1))+(offset));
+} else {
+                                                d=((abs(v_p.y))+(offset));
+}
+}
+        if ( d<0 ) {
+                                    gl_FragColor=vec4((1.e+0f), (0.0e+0f), (0.0e+0f), v_alpha);
+} else {
+                        if ( d<antialias ) {
+                                                                d=exp(((-d)*(d)));
+                gl_FragColor=vec4((0.0e+0f), (2.e-1f), (0.0e+0f), ((v_alpha)*(d)));
+};
+};
+}"""
+app.use("glfw")
+window=app.Window(1200, 400, color=(1,1,1,1,))
+n=100
+V=np.zeros((n,4,), dtype=[("p0",np.float32,2,), ("p1",np.float32,2,), ("uv",np.float32,2,), ("thickness",np.float32,1,)])
+V["uv"]=((0,0,),(0,1,),(1,0,),(1,1,),)
+V["thickness"]=np.linspace((1.0000000149011612e-1), (8.e+0), n).reshape(n, 1)
+V["p0"]=np.dstack((np.linspace(100, 1100, n),((np.ones(n))*(50)),)).reshape(n, 1, 2)
+V["p1"]=np.dstack((np.linspace(100, 1110, n),((np.ones(n))*(350)),)).reshape(n, 1, 2)
+segments=gloo.Program(vertex, fragment, count=((4)*(n)))
+segments.bind(V.ravel().view(gloo.VertexBuffer))
+segments["antialias"]=(2.e+0)
+I=np.zeros((n,6,), dtype=np.uint32)
+I[:]=[0, 1, 2, 1, 2, 3]
+I=((I)+(((4)*(np.arange(n, dtype=np.uint32).reshape(n, 1)))))
+I=I.ravel().view(gloo.IndexBuffer)
+@window.event
+def on_resize(width, height):
+    segments["resolution"]=(width,height,)
+@window.event
+def on_draw(dt):
+    window.clear()
+    segments.draw(gl.GL_TRIANGLES, I)
+app.run()
