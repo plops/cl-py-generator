584f9d0ff7833351fd1ce777cb72c7079fe99eb9
Martin Kielhorn <kielhorn.martin@gmail.com>
2020-04-25 13:07:43 +0200

    load data

--- example/13_fastai2_nlp/gen00.lisp
+++ example/13_fastai2_nlp/gen00.lisp
@@ -45,18 +45,21 @@
 	    (setf path (untar_data URLs.IMDB))
 	    (comment "=> Path('/home/martin/.fastai/data/imdb')")
 
-	    (setf files (get_text_files path
-					:folders
-					(list (string "train")
-					      (string "test")
-					      (string "unsup"))))
+	    #+nil (do0
+	     (setf files (get_text_files path
+					 :folders
+
+					 (list (string "train")
+					       (string "test")
+					       (string "unsup"))))
 
 
-	    (setf txts (L (for-generator (o (aref files ":2000"))
-			   (dot o
-				(open)
-				(read)))))
+	     (setf txts (L (for-generator (o (aref files ":2000"))
+					  (dot o
+					       (open)
+					       (read))))))
 	    
+	    #+nil
 	    (do0
 	     (setf spacy (WordTokenizer)
 		   tkn (Tokenizer spacy))
@@ -78,7 +81,23 @@
 		  (setf dl (LMDataLoader nums200)))
 	     
 	     (comment "From the book: At every epoch we shuffle our collection of documents and concatenate them into a stream of tokens. We then cut that stream into a batch of fixed-size consecutive mini-streams. Our model will then read the mini-streams in order, and thanks to an inner state, it will produce the same activation whatever sequence length you picked."))
-	    
+
+	    (do0
+	     (setf get_imdb (partial get_text_files
+				     :folders
+				     (list (string "train")
+					       (string "test")
+					       (string "unsup")))
+		   dls_lm (dot (DataBlock
+				:blocks (TextBlock.from_folder
+					 path
+					 :is_lm True)
+				:get_items get_imdb
+				:splitter (RandomSplitter .1))
+			       (dataloaders path
+					    :path path
+					    :bs 128
+					    :seq_len 80))))
 	    
 	    
 	    
