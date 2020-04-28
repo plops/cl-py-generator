a721fb6c32b726d07ad4aa28ff708232a128af89
Martin Kielhorn <kielhorn.martin@gmail.com>
2020-04-28 09:38:37 +0200

    write some comments for the examples

--- example/13_fastai2_nlp/source/run_00_nlp.py
+++ example/13_fastai2_nlp/source/run_00_nlp.py
@@ -1,60 +1,66 @@
 # https://github.com/fastai/fastbook/blob/master/10_nlp.ipynb
 # export LANG=en_US.utf8
+from fastai2.text.all import *
+import pathlib
+import os
 import matplotlib
 import matplotlib.pyplot as plt
 plt.ion()
-import os
-import pathlib
-from fastai2.text.all import *
-path=pathlib.Path("/home/martin/txt_utf8")
-get_imdb=partial(get_text_files, folders=["unsup"])
-dls_lm=DataBlock(blocks=TextBlock.from_folder(path, is_lm=True), get_items=get_imdb, splitter=RandomSplitter((0.10    ))).dataloaders(path, path=path, bs=64, seq_len=80)
-learn=language_model_learner(dls_lm, AWD_LSTM, drop_mult=(0.30    ), metrics=[accuracy, Perplexity()]).to_fp16()
-problem="txt"
-fn_1epoch="{}_1epoch".format(problem)
-fn_finetuned="{}_finetuned".format(problem)
-fn_classifier="{}_classifier".format(problem)
-path_1epoch=((((path)/("models")))/("{}.pth".format(fn_1epoch)))
-path_finetuned=((((path)/("models")))/("{}.pth".format(fn_finetuned)))
-path_classifier=((((path)/("models")))/("{}.pth".format(fn_classifier)))
+path = pathlib.Path("/home/martin/txt_utf8")
+get_imdb = partial(get_text_files, folders=["unsup"])
+dls_lm = DataBlock(blocks=TextBlock.from_folder(path, is_lm=True), get_items=get_imdb,
+                   splitter=RandomSplitter((0.10))).dataloaders(path, path=path, bs=64, seq_len=80)
+learn = language_model_learner(dls_lm, AWD_LSTM, drop_mult=(
+    0.30), metrics=[accuracy, Perplexity()]).to_fp16()
+problem = "txt"
+fn_1epoch = "{}_1epoch".format(problem)
+fn_finetuned = "{}_finetuned".format(problem)
+fn_classifier = "{}_classifier".format(problem)
+path_1epoch = ((((path)/("models")))/("{}.pth".format(fn_1epoch)))
+path_finetuned = ((((path)/("models")))/("{}.pth".format(fn_finetuned)))
+path_classifier = ((((path)/("models")))/("{}.pth".format(fn_classifier)))
 print("pid={}".format(os.getpid()))
-if ( not(path_1epoch.is_file()) ):
+if (not(path_1epoch.is_file())):
     print("{} doesnt exist".format(path_1epoch))
-if ( not(path_finetuned.is_file()) ):
+if (not(path_finetuned.is_file())):
     print("{} doesnt exist".format(path_finetuned))
-if ( path_classifier.is_file() ):
+if (path_classifier.is_file()):
     print("load pre-existing classifier")
-    dls_class=DataBlock(blocks=(TextBlock.from_folder(path, vocab=dls_lm.vocab),CategoryBlock,), get_y=parent_label, get_items=partial(get_text_files, folders=["train", "test"]), splitter=GrandparentSplitter(valid_name="test")).dataloaders(path, path=path, bs=32, seq_len=72)
-    learn=text_classifier_learner(dls_class, AWD_LSTM, drop_mult=(0.50    ), metrics=accuracy).to_fp16()
-    learn=learn.load(fn_classifier)
+    dls_class = DataBlock(blocks=(TextBlock.from_folder(path, vocab=dls_lm.vocab), CategoryBlock,), get_y=parent_label, get_items=partial(
+        get_text_files, folders=["train", "test"]), splitter=GrandparentSplitter(valid_name="test")).dataloaders(path, path=path, bs=32, seq_len=72)
+    learn = text_classifier_learner(
+        dls_class, AWD_LSTM, drop_mult=(0.50), metrics=accuracy).to_fp16()
+    learn = learn.load(fn_classifier)
 else:
-    if ( path_finetuned.is_file() ):
+    if (path_finetuned.is_file()):
         print("load finetuned encoder")
-        learn=learn.load(fn_1epoch)
-        learn=learn.load_encoder(fn_finetuned)
+        learn = learn.load(fn_1epoch)
+        learn = learn.load_encoder(fn_finetuned)
         print("create classifier (will take 11min)")
-        dls_class=DataBlock(blocks=(TextBlock.from_folder(path, vocab=dls_lm.vocab),CategoryBlock,), get_y=parent_label, get_items=partial(get_text_files, folders=["train", "test"]), splitter=GrandparentSplitter(valid_name="test")).dataloaders(path, path=path, bs=32, seq_len=72)
-        learn=text_classifier_learner(dls_class, AWD_LSTM, drop_mult=(0.50    ), metrics=accuracy).to_fp16()
-        learn=learn.load_encoder(fn_finetuned)
+        dls_class = DataBlock(blocks=(TextBlock.from_folder(path, vocab=dls_lm.vocab), CategoryBlock,), get_y=parent_label, get_items=partial(
+            get_text_files, folders=["train", "test"]), splitter=GrandparentSplitter(valid_name="test")).dataloaders(path, path=path, bs=32, seq_len=72)
+        learn = text_classifier_learner(
+            dls_class, AWD_LSTM, drop_mult=(0.50), metrics=accuracy).to_fp16()
+        learn = learn.load_encoder(fn_finetuned)
         print("unfreeze layer 1")
         learn.fit_one_cycle(1, (2.00e-2))
         print("unfreeze 2 layers")
         learn.freeze_to(-2)
-        val=(1.00e-2)
-        learn.fit_one_cycle(1, slice(((val)/((((2.60    ))**(4)))), val))
+        val = (1.00e-2)
+        learn.fit_one_cycle(1, slice(((val)/((((2.60))**(4)))), val))
         print("unfreeze 3 layers")
         learn.freeze_to(-3)
-        val=(5.00e-3)
-        learn.fit_one_cycle(1, slice(((val)/((((2.60    ))**(4)))), val))
+        val = (5.00e-3)
+        learn.fit_one_cycle(1, slice(((val)/((((2.60))**(4)))), val))
         print("unfreeze all layers")
         learn.unfreeze()
-        val=(1.00e-3)
-        learn.fit_one_cycle(2, slice(((val)/((((2.60    ))**(4)))), val))
+        val = (1.00e-3)
+        learn.fit_one_cycle(2, slice(((val)/((((2.60))**(4)))), val))
         learn.save(fn_classifier)
     else:
-        if ( path_1epoch.is_file() ):
+        if (path_1epoch.is_file()):
             print("load preexisting 1epoch")
-            learn=learn.load(fn_1epoch)
+            learn = learn.load(fn_1epoch)
             print("finetune encoder will take 10x 20min")
             learn.unfreeze()
             learn.fit_one_cycle(10, (2.00e-3))
@@ -66,3 +72,10 @@ else:
             # epoch     train_loss  valid_loss  accuracy  perplexity  time
             # 0         4.152357    3.935240    0.297858  51.174419   17:51
             learn.save(fn_1epoch)
+print("text review generation (refined model needs to be loaded)")
+TEXT = "We drove past the station"
+N_WORDS = 80
+N_SENTENCES = 6
+preds = [learn.predict(TEXT, N_WORDS, temperature=(0.750))
+         for _ in range(N_SENTENCES)]
+print("\n".join(preds))
--- example/14_megatron_gpt/source/data/populate.sh
+++ example/14_megatron_gpt/source/data/populate.sh
@@ -1,2 +1,8 @@
 curl -O https://s3.amazonaws.com/models.huggingface.co/bert/gpt2-vocab.json
 curl -O https://s3.amazonaws.com/models.huggingface.co/bert/gpt2-merges.txt
+
+
+# wget -O ngccli_cat_linux.zip https://ngc.nvidia.com/downloads/ngccli_cat_linux.zip && unzip -o ngccli_cat_linux.zip && chmod u+x ngc
+
+# pre-trained model from here (i downloaded this with browser, not ngc):
+# https://ngc.nvidia.com/catalog/models/nvidia:megatron_lm_345m/version
