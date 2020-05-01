a5b4c16130317b6eb31c9287639668c4c7bf0d1a
Martin Kielhorn <kielhorn.martin@gmail.com>
2020-05-01 02:03:55 +0200

    create sparse derivative matrix
    martin@labolg ~/stage/cl-py-generator/example/15_slab_waveguide_modes/source $ python3 -i run_00_slab_mode.py
    lam0=1.0 n1=2.0 n2=1.0 a=3.0 b=5.0 dx=0.05 M=5
    Sx=13.0 Nx=260 nx=60 nx1=100 nx2=159
    [[-2.  1.  0. ...  0.  0.  0.]
     [ 1. -2.  1. ...  0.  0.  0.]
     [ 0.  1. -2. ...  0.  0.  0.]
     ...
     [ 0.  0.  0. ... -2.  1.  0.]
     [ 0.  0.  0. ...  1. -2.  1.]
     [ 0.  0.  0. ...  0.  1. -2.]]

--- example/15_slab_waveguide_modes/gen00.lisp
+++ example/15_slab_waveguide_modes/gen00.lisp
@@ -10,6 +10,12 @@
   (defparameter *inspection-facts*
     `((10 "")))
 
+  (defun show (vars)
+    `(do0
+      (print (dot (string ,(format nil "~{~a={}~^ ~}"
+				   vars))
+		  (format ,@vars)))))
+  
   (let* ((code
 	  `(do0
 	    "# "
@@ -22,14 +28,14 @@
 			      (imports ((plt matplotlib.pyplot)))
 			 (plt.ion))
 	    
-	    (imports (			os
+	    (imports (			;os
 					;sys
 					;time
 					;docopt
 					;pathlib
-		      ;(np numpy)
+		      (np numpy)
 					;serial
-			    pathlib
+					;pathlib
 					;(pd pandas)
 					;(xr xarray)
 					;(xrp xarray.plot)
@@ -37,16 +43,51 @@
 					;(u astropy.units)
 					;scipy.ndimage
 					;scipy.optimize
+		      scipy.sparse.linalg
+		      ;scipy.sparses
 		      ))
-
-	    (setf lam0 1s0
-		  n1 2s0
-		  n2 1s0
-		  a (* 3 lam0) ;; core thickness
-		  b (* 5 lam0) ;; substrate thickness
-		  dx (/ lam0 20) ;; grid resolution
-		  M 5 ;; number of modes to calculate
-		  )
+	    (do0
+	     (comment "simulation parameters")
+	     (setf lam0 1s0
+		   n1 2s0
+		   n2 1s0
+		   a (* 3 lam0)	 ;; core thickness
+		   b (* 5 lam0)	 ;; substrate thickness
+		   dx (/ lam0 20) ;; grid resolution
+		   M 5		  ;; number of modes to calculate
+		   )
+	     ,(show `(lam0 n1 n2 a b dx M)))
+	    (do0
+	     (comment "compute grid")
+	     (setf Sx (+ a (* 2 b))
+		   Nx (int (np.ceil (/ Sx dx)))
+		   Sx (* Nx dx)
+		   xa (* dx (np.linspace .5 (- Nx .5) (+ Nx -1)))
+		   xa (- xa (np.mean xa)))
+	     (comment "start and stop indices (centered in grid)")
+	     (setf nx (int (np.round (/ a dx)))
+		    
+		   nx1 (int (np.round (/ (- Nx nx) 2))) ;; start
+		   nx2 (+ nx1 nx -1) ;; end
+		   )
+	     (setf N (np.ones (tuple Nx 1))
+		   (aref N "0:nx1-2") n2
+		   (aref N "nx1:nx2") n1
+		   (aref N "nx2+1:Nx") n2)
+	     ,(show `(Sx Nx ;xa
+			 nx nx1 nx2
+			 )))
+	    (do0
+	     (comment "perform fd analysis")
+	     (setf k0 (/ (* 2 np.pi) lam0))
+	     (setf DX2 (scipy.sparse.diags
+			(tuple
+			 (* +1 (np.ones (- Nx 1) ))
+			 (* -2 (np.ones (- Nx 0) ))
+			 (* +1 (np.ones (- Nx 1) )))
+			(tuple -1 0 1)))
+	     (print (DX2.toarray)))
+	    
 	    ))) 
     (write-source (format nil "~a/source/~a" *path* *code-file*) code)))
  
--- example/15_slab_waveguide_modes/source/run_00_slab_mode.py
+++ example/15_slab_waveguide_modes/source/run_00_slab_mode.py
@@ -3,8 +3,9 @@
 import matplotlib
 import matplotlib.pyplot as plt
 plt.ion()
-import os
-import pathlib
+import numpy as np
+import scipy.sparse.linalg
+# simulation parameters
 lam0=(1.0    )
 n1=(2.0    )
 n2=(1.0    )
@@ -12,3 +13,23 @@ a=((3)*(lam0))
 b=((5)*(lam0))
 dx=((lam0)/(20))
 M=5
+print("lam0={} n1={} n2={} a={} b={} dx={} M={}".format(lam0, n1, n2, a, b, dx, M))
+# compute grid
+Sx=((a)+(((2)*(b))))
+Nx=int(np.ceil(((Sx)/(dx))))
+Sx=((Nx)*(dx))
+xa=((dx)*(np.linspace((0.50    ), ((Nx)-((0.50    ))), ((Nx)+(-1)))))
+xa=((xa)-(np.mean(xa)))
+# start and stop indices (centered in grid)
+nx=int(np.round(((a)/(dx))))
+nx1=int(np.round(((((Nx)-(nx)))/(2))))
+nx2=((nx1)+(nx)+(-1))
+N=np.ones((Nx,1,))
+N[0:nx1:2]=n2
+N[nx1:nx2]=n1
+N[nx2+1:Nx]=n2
+print("Sx={} Nx={} nx={} nx1={} nx2={}".format(Sx, Nx, nx, nx1, nx2))
+# perform fd analysis
+k0=((((2)*(np.pi)))/(lam0))
+DX2=scipy.sparse.diags((((1)*(np.ones(((Nx)-(1))))),((-2)*(np.ones(((Nx)-(0))))),((1)*(np.ones(((Nx)-(1))))),), (-1,0,1,))
+print(DX2.toarray())
