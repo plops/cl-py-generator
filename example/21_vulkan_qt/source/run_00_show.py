41f65b9491e73eab469411b9242898a5b6d62baa
Martin Kielhorn <kielhorn.martin@gmail.com>
2020-06-15 20:41:53 +0200

    start to go through vulkan and qt tutorial

---
+++ example/21_vulkan_qt/source/run_00_show.py
@@ -0,0 +1,7 @@
+import numpy as np
+import pandas as pd
+import pathlib
+# %%
+_code_git_version="4de1816a2db573db4865bd1499b6e70873ac1b27"
+_code_repository="https://github.com/plops/cl-py-generator/tree/master/example/21_vulkan_qt/source/run_00_show.py"
+_code_generation_time="20:41:40 of Monday, 2020-06-15 (GMT+1)"
