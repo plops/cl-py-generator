2317ebe426fc803d19a96a5fb7edd74634e003ba
Martin Kielhorn <kielhorn.martin@gmail.com>
2020-07-04 23:23:19 +0200

    start with a helium browser automation example

---
+++ example/22_helium/gen00.lisp
@@ -0,0 +1,132 @@
+(eval-when (:compile-toplevel :execute :load-toplevel)
+  (ql:quickload "cl-py-generator"))
+
+(in-package :cl-py-generator)
+
+;; https://github.com/mherrmann/selenium-python-helium
+;; currently helium 3.0.4
+
+
+(progn
+  (defparameter *repo-sub-path* "22_helium")
+  (defparameter *path* (format nil "/home/martin/stage/cl-py-generator/example/~a" *repo-sub-path*))
+  (defparameter *code-file* "run_00_show")
+  (defparameter *source* (format nil "~a/source/~a" *path* *code-file*))
+  (defparameter *inspection-facts*
+    `((10 "")))
+  (defparameter *day-names*
+    '("Monday" "Tuesday" "Wednesday"
+      "Thursday" "Friday" "Saturday"
+      "Sunday"))
+
+  
+  (let* ((code
+	  `(do0
+	    "#!/usr/bin/python3"
+	    (comments "pip3 install --user helium")
+	    (do0
+	     #+nil (do0
+		    (imports (matplotlib))
+                                        ;(matplotlib.use (string "QT5Agg"))
+		    "from matplotlib.backends.backend_qt5agg import (FigureCanvas, NavigationToolbar2QT as NavigationToolbar)"
+		    "from matplotlib.figure import Figure"
+		    (imports ((plt matplotlib.pyplot)
+			      matplotlib.colors
+			      (animation matplotlib.animation)
+			      (xrp xarray.plot)))
+		    
+		    (plt.ion)
+					;(plt.ioff)
+		    (setf font (dict ((string size) (string 8))))
+		    (matplotlib.rc (string "font") **font)
+		    )
+
+
+	     
+	     )
+	    
+
+	    
+	    (imports (			;os
+					;sys
+					;traceback
+					;pdb
+					;time
+					;docopt
+					;pathlib
+		      ;(yf yfinance)
+		      (np numpy)
+		      ;collections
+					;serial
+		      (pd pandas)
+					;(xr xarray)
+					;(xrp xarray.plot)
+					;skimage.restoration 
+					;skimage.feature
+					;skimage.morphology
+					;skimage.measure
+					; (u astropy.units)
+					; EP_SerialIO
+					;scipy.ndimage
+					;scipy.optimize
+					;scipy.ndimage.morphology
+					; nfft
+					; ttv_driver
+		      pathlib
+					;re
+					;requests
+					;zipfile
+					;io
+					;sklearn
+					;sklearn.linear_model
+		      
+		      ))
+	    
+	    #+nil
+	    (do0
+	     (imports (PyQt5)
+		      )
+	     "from PyQt5 import QtCore, QtGui, QtWidgets"
+	     "from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QVBoxLayout, QHBoxLayout, QLabel"
+	     "from PyQt5.QtCore import QAbstractTableModel, Qt")
+
+	    "from helium import *"
+	    
+	    (do0
+	     (comment "%%")
+	     (setf
+	      _code_git_version
+	      (string ,(let ((str 
+			      #-sbcl "xxx"
+			      #+sbcl (with-output-to-string (s)
+				       (sb-ext:run-program "/usr/bin/git" (list "rev-parse" "HEAD") :output s))))
+			 (subseq str 0 (1- (length str)))))
+	      _code_repository (string ,(format nil "https://github.com/plops/cl-py-generator/tree/master/example/~a/source/~a.py" *repo-sub-path* *code-file*)
+				       )
+
+	      _code_generation_time
+	      (string ,(multiple-value-bind
+			     (second minute hour date month year day-of-week dst-p tz)
+			   (get-decoded-time)
+			 (declare (ignorable dst-p))
+			 (format nil "~2,'0d:~2,'0d:~2,'0d of ~a, ~d-~2,'0d-~2,'0d (GMT~@d)"
+				 hour
+				 minute
+				 second
+				 (nth day-of-week *day-names*)
+				 year
+				 month
+				 date
+				 (- tz))))))
+
+
+	    (do0
+	     (comment "%%")
+	     (start_chrome)
+	     ))))
+    (write-source (format nil "~a/source/~a" *path* *code-file*) code)))
+
+
+
+
+
---
+++ example/22_helium/source/run_00_show.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+# pip3 install --user helium
+import numpy as np
+import pandas as pd
+import pathlib
+from helium import *
+# %%
+_code_git_version="e5d26e3ab14d31f021d2d9bc385c481d05bd5acf"
+_code_repository="https://github.com/plops/cl-py-generator/tree/master/example/22_helium/source/run_00_show.py"
+_code_generation_time="23:23:10 of Saturday, 2020-07-04 (GMT+1)"
+# %%
+start_chrome()
