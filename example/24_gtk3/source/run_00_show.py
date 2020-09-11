637eafbead5ed8bf2eb2fbf71d4e2588b84950a8
Martin Kielhorn <kielhorn.martin@gmail.com>
2020-09-11 11:47:21 +0200

    start gtk3 example

---
+++ example/24_gtk3/source/run_00_show.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+import wx
+# %%
+_code_git_version="570f9d89220b3b917e2de6d700b833ca2e40732f"
+_code_repository="https://github.com/plops/cl-py-generator/tree/master/example/24_gtk3/source/run_00_show.py"
+_code_generation_time="11:47:07 of Friday, 2020-09-11 (GMT+1)"
