dcb329ee8ebeb565adbaee5163f1ecf2284c9861
Martin Kielhorn <kielhorn.martin@gmail.com>
2020-09-27 21:54:45 +0200

    try to run the pets example
    martin@labolg ~/stage/cl-py-generator/example/25_fastai2_again/source $ python3 -i run_00_pets.py
     |ââââââââââââââââââââââââââââââââââââââââââââââââââ--------------------------------------------------| 50.90% [413138944/811706944 00:16<00:15]

--- example/25_fastai2_again/gen00.lisp
+++ example/25_fastai2_again/gen00.lisp
@@ -109,6 +109,20 @@
 				 date
 				 (- tz))))))
 
+	    (do0
+
+	     (setf path (/  (untar_data URLs.PETS)
+			    (string "images")))
+	     (def is_cat (x)
+	       (return (dot (aref x 0)
+			    (isupper))))
+	     (setf dls (ImageDataLoaders.from_name_func path (get_image_files path)
+							:valid_pct .2
+							:seed 42
+							:label_func is_cat
+							:item_tfms (Resize 224)))
+	     (setf learn (cnn_learner dls resnet34 :metrics error_rate))
+	     (learn.fine_tune 1))
 
 	    )))
     (write-source (format nil "~a/source/~a" *path* *code-file*) code)))
--- example/25_fastai2_again/source/run_00_pets.py
+++ example/25_fastai2_again/source/run_00_pets.py
@@ -1,6 +1,12 @@
 #!/usr/bin/python3
 from fastai.vision.all import *
 # %%
-_code_git_version="10e9ba51b7ea3002a0d6432f48bc8ac84bcf16d9"
+_code_git_version="c5557a12bcc9003c00cb2eaffedd98c5e1b4913f"
 _code_repository="https://github.com/plops/cl-py-generator/tree/master/example/25_fastai2_again/source/run_00_pets.py"
-_code_generation_time="21:51:49 of Sunday, 2020-09-27 (GMT+1)"
+_code_generation_time="21:54:20 of Sunday, 2020-09-27 (GMT+1)"
+path=((untar_data(URLs.PETS))/("images"))
+def is_cat(x):
+    return x[0].isupper()
+dls=ImageDataLoaders.from_name_func(path, get_image_files(path), valid_pct=(0.20    ), seed=42, label_func=is_cat, item_tfms=Resize(224))
+learn=cnn_learner(dls, resnet34, metrics=error_rate)
+learn.fine_tune(1)
