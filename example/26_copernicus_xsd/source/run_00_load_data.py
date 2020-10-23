eeb472647a4e399fff817d13dddf725dbde91a5f
Martin Kielhorn <kielhorn.martin@gmail.com>
2020-10-23 19:21:43 +0200

    add code for a new project. i try to use xsd files to open
    copernicus sentinel 1 files

---
+++ example/26_copernicus_xsd/source/run_00_load_data.py
@@ -0,0 +1,17 @@
+# %% imports
+import matplotlib
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+import xarray.plot as xrp
+plt.ion()
+import sys
+import time
+import pathlib
+import numpy as np
+import serial
+import pandas as pd
+import xarray as xr
+import xarray.plot as xrp
+_code_git_version="dcb329ee8ebeb565adbaee5163f1ecf2284c9861"
+_code_repository="https://github.com/plops/cl-py-generator/tree/master/example/26_copernicus_xsd/source/run_00_load_data.py"
+_code_generation_time="19:21:36 of Friday, 2020-10-23 (GMT+1)"
