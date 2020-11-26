9a398ee2ccb4120889401bfd2b2cd9ac1d9560cf
Martin Kielhorn <kielhorn.martin@gmail.com>
2020-11-26 18:55:46 +0100

    start with some code

---
+++ example/29_ondrejs_challenge/gen00.lisp
@@ -0,0 +1,96 @@
+(eval-when (:compile-toplevel :execute :load-toplevel)
+  (ql:quickload "cl-py-generator")
+  (ql:quickload "alexandria"))
+(in-package :cl-py-generator)
+
+
+
+(progn
+  (defparameter *path* "/home/martin/stage/cl-py-generator/example/29_ondrejs_challenge")
+  (defparameter *code-file* "run_00_start")
+  (defparameter *source* (format nil "~a/source/~a" *path* *code-file*))
+  (defparameter *host*
+    "10.1.99.12")
+  (defparameter *inspection-facts*
+    `((10 "")))
+
+  (defparameter *day-names*
+    '("Monday" "Tuesday" "Wednesday"
+      "Thursday" "Friday" "Saturday"
+      "Sunday"))
+
+     
+  (let* (
+	 
+	 (code
+	  `(do0
+	    (do0 "# %% imports"
+		 (do0
+		  
+		  (imports (matplotlib))
+                                        ;(matplotlib.use (string "QT5Agg"))
+					;"from matplotlib.backends.backend_qt5agg import (FigureCanvas, NavigationToolbar2QT as NavigationToolbar)"
+					;"from matplotlib.figure import Figure"
+		  (imports ((plt matplotlib.pyplot)
+			    (animation matplotlib.animation) 
+                            ;(xrp xarray.plot)
+			    ))
+                  
+		  (plt.ion)
+					;(plt.ioff)
+		  ;;(setf font (dict ((string size) (string 6))))
+		  ;; (matplotlib.rc (string "font") **font)
+		  )
+		 (imports (		;os
+			   sys
+			   time
+					;docopt
+			   pathlib
+			   (np numpy)
+			   ;serial
+			   (pd pandas)
+			   ;(xr xarray)
+			   ;(xrp xarray.plot)
+					;skimage.restoration
+					;(u astropy.units)
+					; EP_SerialIO
+			   ;scipy.ndimage
+			   ;scipy.optimize
+					;nfft
+			   ;sklearn
+			   ;sklearn.linear_model
+			   ;itertools
+			   ;datetime
+			   ;dask.distributed
+			   ;(da dask.array)
+			   ))
+		 
+
+		 (setf
+	       _code_git_version
+		  (string ,(let ((str (with-output-to-string (s)
+					(sb-ext:run-program "/usr/bin/git" (list "rev-parse" "HEAD") :output s))))
+			     (subseq str 0 (1- (length str)))))
+		  _code_repository (string ,(format nil "https://github.com/plops/cl-py-generator/tree/master/example/28_dask_test/source/run_00_start.py")
+					   )
+
+		  _code_generation_time
+		  (string ,(multiple-value-bind
+				 (second minute hour date month year day-of-week dst-p tz)
+			       (get-decoded-time)
+			     (declare (ignorable dst-p))
+		      (format nil "~2,'0d:~2,'0d:~2,'0d of ~a, ~d-~2,'0d-~2,'0d (GMT~@d)"
+			      hour
+			      minute
+			      second
+			      (nth day-of-week *day-names*)
+			      year
+			      month
+			      date
+			      (- tz)))))
+		 (do0
+		  
+		  )))
+	   ))
+    (write-source (format nil "~a/source/~a" *path* *code-file*) code)))
+
---
+++ example/29_ondrejs_challenge/source/run_00_start.py
@@ -0,0 +1,13 @@
+# %% imports
+import matplotlib
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+plt.ion()
+import sys
+import time
+import pathlib
+import numpy as np
+import pandas as pd
+_code_git_version="cf0a3ef9c589c5226a781f68ce8c487388056a03"
+_code_repository="https://github.com/plops/cl-py-generator/tree/master/example/28_dask_test/source/run_00_start.py"
+_code_generation_time="18:55:33 of Thursday, 2020-11-26 (GMT+1)"
