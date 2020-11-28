7ab2392faef1465f652e168b36b1a6b75899084c
Martin Kielhorn <kielhorn.martin@gmail.com>
2020-11-28 14:02:54 +0100

    decoded sound

--- example/29_ondrejs_challenge/source/run_01_audio.py
+++ example/29_ondrejs_challenge/source/run_01_audio.py
@@ -10,8 +10,15 @@ import numpy as np
 import pandas as pd
 import scipy.io.wavfile
 import libtiff
-_code_git_version="ac8a07b2c1d68aed56bddb6880a4c679594d001b"
+_code_git_version="11d174e8861127a6b334e9795795573452655401"
 _code_repository="https://github.com/plops/cl-py-generator/tree/master/example/29_ondrejs_challenge/source/run_00_start.py"
-_code_generation_time="13:54:38 of Saturday, 2020-11-28 (GMT+1)"
+_code_generation_time="14:01:13 of Saturday, 2020-11-28 (GMT+1)"
 fn="supplementary_materials/zdravice.wav"
-a=scipy.io.wavfile.read(fn)
+rate, a=scipy.io.wavfile.read(fn)
+# => 11025, array with 242550x2 elements, int16
+z=((a[:,0])+(((1j)*(a[:,1]))))
+zs=np.fft.fftshift(z)
+k=np.fft.ifft(zs)
+kr=np.real(k)
+ki=np.imag(k)
+scipy.io.wavfile.write("/dev/shm/r.wav", rate, kr)
