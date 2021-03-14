13180afe144a504ac9efb955e967a9f18423b566
Martin Kielhorn <kielhorn.martin@gmail.com>
2021-03-14 19:49:28 +0100

    start playing around with freecad cadquery

---
+++ example/36_cadquery/source/run_00_start.py
@@ -0,0 +1,5 @@
+# %% imports
+import cadquery as cq
+_code_git_version="f8f64bb299440d7c9e776a533d148374c4fe9164"
+_code_repository="https://github.com/plops/cl-py-generator/tree/master/example/29_ondrejs_challenge/source/run_00_start.py"
+_code_generation_time="19:49:19 of Sunday, 2021-03-14 (GMT+1)"
