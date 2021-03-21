79a59a10d9bf2bf96ff3a65905c4030094f9f99c
Martin Kielhorn <kielhorn.martin@gmail.com>
2021-03-21 19:03:33 +0100

    try jax

---
+++ example/37_jax/source/run_00_start.py
@@ -0,0 +1,6 @@
+# %% imports
+import cadquery as cq
+from Helpers import show
+_code_git_version="eb2156de68e2618dd6ae620e568f038d2e4f901a"
+_code_repository="https://github.com/plops/cl-py-generator/tree/master/example/29_ondrejs_challenge/source/run_00_start.py"
+_code_generation_time="19:03:19 of Sunday, 2021-03-21 (GMT+1)"
