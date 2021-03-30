efafa2dc8581773b9521093d336c32c10247ab31
Martin Kielhorn <kielhorn.martin@gmail.com>
2021-03-30 20:21:46 +0200

    bounding box works

--- example/38_dlib/gen00.lisp
+++ example/38_dlib/gen00.lisp
@@ -95,6 +95,40 @@
 			     )))
 
 		 (do0
+		  (def calc_bounding_rect (image landmarks)
+		    (setf (ntuple height width)
+			  (aref image.shape "0:2")
+			  )
+		    (setf landmark_array (np.empty (tuple 0 2)
+						   int))
+		    (for (landmark
+			  landmarks.landmark)
+			 (setf lx (min (int (* landmark.x width))
+				       (- width 1)))
+			 (setf ly (min (int (* landmark.y height))
+				       (- height 1)))
+			 (setf landmark_point (list (np.array (tuple lx ly))))
+			 (setf landmark_array (np.append
+					       landmark_array
+					       landmark_point
+					       :axis 0)))
+		    (setf (ntuple x y w h)
+			  (cv.boundingRect landmark_array))
+		    (return (list x
+				  y
+				  (+ x w)
+				  (+ y h))))
+		  (def draw_bounding_rect (image brect)
+		    (cv.rectangle image
+				  (tuple (aref brect 0)
+					 (aref brect 1))
+				  (tuple (aref brect 2)
+					 (aref brect 3))
+				  (tuple 0 255 0)
+				  2)
+		    (return image)
+		    ))
+		 (do0
 		  (setf cap (cv.VideoCapture (string "/dev/video0")))
 		  (setf mp_face_mesh mp.solutions.face_mesh)
 		  (setf face_mesh (dot mp_face_mesh
@@ -111,6 +145,16 @@
 			 (setf debug_image (copy.deepcopy image))
 			 (setf image (cv.cvtColor image cv.COLOR_BGR2RGB))
 			 (setf results (face_mesh.process image))
+			 (do0
+			  (unless (is results.multi_face_landmarks
+				      None)
+			    (for (face_landmarks results.multi_face_landmarks)
+				 (setf brect (calc_bounding_rect
+					      debug_image
+					      face_landmarks))
+				 (setf debug_image
+				       (draw_bounding_rect
+					debug_image brect)))))
 			 (do0
 			  (setf key (cv.waitKey 1))
 			  (when (== 27 key)
