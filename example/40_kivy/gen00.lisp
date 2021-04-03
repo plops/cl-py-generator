288c4e009dd06fde973eb798b1c4369d13d531c5
Martin Kielhorn <kielhorn.martin@gmail.com>
2021-04-03 10:47:08 +0200

    add kv file

--- example/40_kivy/gen00.lisp
+++ example/40_kivy/gen00.lisp
@@ -6,8 +6,8 @@
 
 
 (progn
-  (defparameter *path* "/home/martin/stage/cl-py-generator/example/39_opencv")
-  (defparameter *code-file* "run_00_raw_cam")
+  (defparameter *path* "/home/martin/stage/cl-py-generator/example/40_kivy")
+  (defparameter *code-file* "run_00_kivy")
   (defparameter *source* (format nil "~a/source/~a" *path* *code-file*))
   (defparameter *host*
     "10.1.99.12")
@@ -41,7 +41,7 @@
 		  ;;(setf font (dict ((string size) (string 6))))
 		  ;; (matplotlib.rc (string "font") **font)
 		  )
-		 (imports (		;os
+		#+nil  (imports (		;os
 					;sys
 					;time
 					;docopt
@@ -70,7 +70,10 @@
 			   copy
 			   
 			   ))
-		;"from mss import mss"
+		,@(loop for (pkg exprs) in `((kivy.app (App))
+					     (kivy.uix.widget (Widget)))
+			collect
+			(format nil "from ~a import ~{~a~^, ~}" pkg exprs))
 		 (setf
 		  _code_git_version
 		  (string ,(let ((str (with-output-to-string (s)
@@ -94,69 +97,47 @@
 				     date
 				     (- tz))
 			     )))
-		 #+nil
-		 (print (cv.getBuildInformation))
-		 
-		 #-nil (do0
-			(setf cap (cv.VideoCapture (string "/dev/video0")))
-			,@(loop for e in `((MODE 0)
-					  
-					   (FRAME_WIDTH 640)
-					   (FRAME_HEIGHT 480)
-					   (AUTO_EXPOSURE 1)
-					   (EXPOSURE 50))
-				collect
-					  (destructuring-bind (name &optional value)
-					      e
-					    (let ((full-mode (format nil "cv.CAP_PROP_~a" name)))
-					      `(do0
-						
-						(do0
-						 (setf r
-						       ,(if value
-							    `(cap.set ,full-mode ,value)
-							    `(cap.set ,full-mode )))
-						 (unless r
-						   (print (string ,(format nil "problem with ~a"  name))
-							  )))
-						(do0
-						 (setf r
-						       (cap.get ,full-mode ))
-						 (print (dot  (string ,(format nil "~a={}"  name))
-							      (format r))
-							)))))))
-		 
-		 
-		 (cv.namedWindow (string "image")
-				 cv.WINDOW_NORMAL)
-		 (cv.resizeWindow (string "image")
-				  (// 1920 2) 1080)
-		 (do0
-		  (while True
-			 (do0 (setf (ntuple ret image)
-				    (cap.read))
-			      (unless ret
-				break))
-			 
-					;(setf debug_image (copy.deepcopy image))
-			 (do0 ;(setf image (cv.cvtColor image cv.COLOR_YUYV2RGB))
-			     ; (setf debug_image (copy.deepcopy image))
-
-			      (setf image.flags.writeable False)
-			      )
-
-			
-			 
-			 (do0
-			  (setf key (cv.waitKey 1))
-			  (when (== 27 key)
-			    break)
-			  )
-			 (cv.imshow (string "image")
-				    image ))
-		  )
-		 (do0
-		  (cap.release)
-		  (cv.destroyAllWindows))))))
-    (write-source (format nil "~a/source/~a" *path* *code-file*) code)))
+		 (class PongGame (Widget)
+			pass)
+		 (class PongApp (App)
+			(def build (self)
+			  (return (PongGame))))
+		 (class PongBall (Widget)
+			(setf vx (NumericProperty 0)
+			      vy (NumericProperty 0)
+			      v (ReferenceListProperty vx vy))
+			(def move (self)
+			  (setf self.pos
+				(+ (Vector *self.v)
+				   self.pos))))
+		 (when (== __name__ (string "__main__"))
+		   (dot (PongApp)
+			(run)))))))
+    (write-source (format nil "~a/source/~a" *path* *code-file*) code)
+    (with-output-to-file (s (format nil "~a/source/pong.kv" *path*)
+			    :if-exists :supersede
+			    :if-does-not-exist :create)
+      (format s "#:kivy 1.0.9
+<PongGame>:
+ canvas:
+  Rectangle:
+   pos: self.center_x-5, 0
+   size: 10, self.height
+  Label:
+   font_size: 70
+   center_x: root.width / 4
+   top: root.top - 50
+   text: '0'
+  Label:
+   font_size: 70
+   center_x: root.width * 3 / 4
+   top: root.top - 50
+   text: '0'
+<PongBall>:
+ size: 50, 50
+ canvas:
+  Ellipse:
+   pos: self.pos
+   size: self.size
+  "))))
 
--- example/40_kivy/source/run_00_kivy.py
+++ example/40_kivy/source/run_00_kivy.py
@@ -1,12 +1,18 @@
 from kivy.app import App
 from kivy.uix.widget import Widget
-_code_git_version="b18e0b2c769c533c0019ebba36266f34db93f93b"
+_code_git_version="f21161ca42846b070ae51b27ccee46da2ce381c3"
 _code_repository="https://github.com/plops/cl-py-generator/tree/master/example/29_ondrejs_challenge/source/run_00_start.py"
-_code_generation_time="10:39:59 of Saturday, 2021-04-03 (GMT+1)"
+_code_generation_time="10:47:06 of Saturday, 2021-04-03 (GMT+1)"
 class PongGame(Widget):
     pass
 class PongApp(App):
     def build(self):
         return PongGame()
+class PongBall(Widget):
+    vx=NumericProperty(0)
+    vy=NumericProperty(0)
+    v=ReferenceListProperty(vx, vy)
+    def move(self):
+        self.pos=((Vector(*self.v))+(self.pos))
 if ( ((__name__)==("__main__")) ):
     PongApp().run()
