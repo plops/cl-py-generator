7a83a61dcbb3ca096bd6ccd8093d1a0068943e33
Martin Kielhorn <kielhorn.martin@gmail.com>
2021-04-04 09:23:22 +0200

    try to make build with jupyter

---
+++ example/42_android_repl/gen00.lisp
@@ -0,0 +1,144 @@
+(eval-when (:compile-toplevel :execute :load-toplevel)
+  (ql:quickload "cl-py-generator")
+  (ql:quickload "alexandria"))
+(in-package :cl-py-generator)
+
+
+
+(progn
+  (defparameter *path* "/home/martin/stage/cl-py-generator/example/42_android_repl")
+  (defparameter *code-file* "main")
+  (defparameter *source* (format nil "~a/source/~a" *path* *code-file*))
+  (defparameter *host*
+    "10.1.99.12")
+  (defparameter *inspection-facts*
+    `((10 "")))
+
+  (defparameter *day-names*
+    '("Monday" "Tuesday" "Wednesday"
+      "Thursday" "Friday" "Saturday"
+      "Sunday"))
+
+     
+  (let* (
+	 
+	 (code
+	  `(do0
+	    (do0 
+		 #+nil(do0
+		  
+		  (imports (matplotlib))
+                                        ;(matplotlib.use (string "QT5Agg"))
+					;"from matplotlib.backends.backend_qt5agg import (FigureCanvas, NavigationToolbar2QT as NavigationToolbar)"
+					;"from matplotlib.figure import Figure"
+		  (imports ((plt matplotlib.pyplot)
+			  ;  (animation matplotlib.animation) 
+                            ;(xrp xarray.plot)
+			    ))
+                  
+		  (plt.ion)
+					;(plt.ioff)
+		  ;;(setf font (dict ((string size) (string 6))))
+		  ;; (matplotlib.rc (string "font") **font)
+		  )
+		#+nil  (imports (		;os
+					;sys
+					;time
+					;docopt
+					;pathlib
+					;(np numpy)
+					;serial
+					;(pd pandas)
+					;(xr xarray)
+					;(xrp xarray.plot)
+					;skimage.restoration
+					;(u astropy.units)
+					; EP_SerialIO
+					;scipy.ndimage
+					;   scipy.optimize
+					;nfft
+					;sklearn
+					;sklearn.linear_model
+					;itertools
+					;datetime
+			   (np numpy)
+			   (cv cv2)
+					;(mp mediapipe)
+					;jax
+			   ; jax.random
+			   ;jax.config
+			   copy
+			   
+				 ))
+		(imports (time))
+		,@(loop for (pkg exprs) in `((kivy.app (App))
+					;(kivy.uix.widget (Widget))
+					     (kivy.uix.boxlayout (BoxLayout))
+					     #+nil (kivy.properties (NumericProperty
+							       ReferenceListProperty
+							       ObjectProperty)
+							      )
+					     ;(kivy.vector (Vector))
+					(kivy.clock (Clock))
+					     (camera (Camera2))
+					     )
+			collect
+			(format nil "from ~a import ~{~a~^, ~}" pkg exprs))
+		 (setf
+		  _code_git_version
+		  (string ,(let ((str (with-output-to-string (s)
+					(sb-ext:run-program "/usr/bin/git" (list "rev-parse" "HEAD") :output s))))
+			     (subseq str 0 (1- (length str)))))
+		  _code_repository (string ,(format nil "https://github.com/plops/cl-py-generator/tree/master/example/29_ondrejs_challenge/source/run_00_start.py")
+					   )
+
+		  _code_generation_time
+		  (string ,(multiple-value-bind
+				 (second minute hour date month year day-of-week dst-p tz)
+			       (get-decoded-time)
+			     (declare (ignorable dst-p))
+			     (format nil "~2,'0d:~2,'0d:~2,'0d of ~a, ~d-~2,'0d-~2,'0d (GMT~@d)"
+				     hour
+				     minute
+				     second
+				     (nth day-of-week *day-names*)
+				     year
+				     month
+				     date
+				     (- tz))
+			     )))
+		 (class MainLayout (BoxLayout)
+			pass)
+		 (class MainApp (App)
+		    	(def build (self)
+			  (return (MainLayout)))
+			(def on_start (self)
+			  (Clock.schedule_once self.detect 5))
+			(def detect (self nap)
+			  (setf 
+				ctime (aref (time.ctime)
+					    "11:19")
+				;self.root.ids.label.text
+				#+nil (dot (string "{:s} {}x{} image")
+					   (format ctime rows cols)))
+			  (print (dot (string "{:s} {}x{} image")
+				     (format ctime rows cols)))
+			  (Clock.schedule_once self.detect 1)))
+		 
+		 (when (== __name__ (string "__main__"))
+		   (setf app (MainApp))
+		   (dot app
+			(run)))))))
+    (write-source (format nil "~a/source/~a" *path* *code-file*) code)
+    
+    (with-output-to-file (s (format nil "~a/source/main.kv" *path*)
+			    :if-exists :supersede
+			    :if-does-not-exist :create)
+      (format s "#:kivy 1.0.9
+
+<MainLayout>:
+    orientation: 'vertical'
+    padding: (36, 36)
+
+"))))
+
---
+++ example/42_android_repl/source/main.py
@@ -0,0 +1,22 @@
+import time
+from kivy.app import App
+from kivy.uix.boxlayout import BoxLayout
+from kivy.clock import Clock
+from camera import Camera2
+_code_git_version="ab22595738935642489147d86dc781adcbd67280"
+_code_repository="https://github.com/plops/cl-py-generator/tree/master/example/29_ondrejs_challenge/source/run_00_start.py"
+_code_generation_time="09:18:23 of Sunday, 2021-04-04 (GMT+1)"
+class MainLayout(BoxLayout):
+    pass
+class MainApp(App):
+    def build(self):
+        return MainLayout()
+    def on_start(self):
+        Clock.schedule_once(self.detect, 5)
+    def detect(self, nap):
+        ctime=time.ctime()[11:19]
+        print("{:s} {}x{} image".format(ctime, rows, cols))
+        Clock.schedule_once(self.detect, 1)
+if ( ((__name__)==("__main__")) ):
+    app=MainApp()
+    app.run()
