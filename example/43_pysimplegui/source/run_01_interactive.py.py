25c56e0cf54acb5b223ce6bc53070c71fd5f9fbe
Martin Kielhorn <kielhorn.martin@gmail.com>
2021-04-08 06:23:31 +0200

    interaction works

---
+++ example/43_pysimplegui/source/run_01_interactive.py.py
@@ -0,0 +1,9 @@
+import PySimpleGUI as sg
+layout=[[sg.Text("name:")], [sg.Input(key="-INPUT-")], [sg.Text(size=(40,1,), key="-OUTPUT-")], [sg.Button("Ok"), sg.Button("Quit")]]
+window=sg.Window("Window Title", layout)
+while (True):
+    event, values=window.read()
+    if ( (event in (sg.WIN_CLOSED,"Quit",)) ):
+        break
+    window["-OUTPUT-"].update((("Hello ")+(values["-INPUT-"])+("! Thanks for trying.")))
+window.close()
