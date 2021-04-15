eca19aaa25ddc856c2e95191064ce803f7ccd0cb
Martin Kielhorn <kielhorn.martin@gmail.com>
2021-04-15 17:37:16 +0200

    load system table into opticspy

--- example/46_opticspy/gen01.lisp
+++ example/46_opticspy/gen01.lisp
@@ -7,7 +7,7 @@
 
 (progn
   (defparameter *path* "/home/martin/stage/cl-py-generator/example/46_opticspy")
-  (defparameter *code-file* "run_00_lens")
+  (defparameter *code-file* "run_01_lens_jax")
   (defparameter *source* (format nil "~a/source/~a" *path* *code-file*))
   (defparameter *host*
     "10.1.99.12")
@@ -19,9 +19,7 @@
       "Thursday" "Friday" "Saturday"
       "Sunday"))
 
-  (let* (
-	 
-	 (code
+  (let* ((code
 	  `(do0
 	    (do0 
 		 #-nil(do0
@@ -87,72 +85,88 @@
 		  (l.list_wavelengths)
 		  (l.list_fields))
 		 (do0
-		  ,@(loop for e
-			    in `((1e9 1e9 air)
-				 (41.15909 6.09755 S-BSM18_ohara)
-				 (-957.83146 9.349 air)
-				 (-51.32 2.032 N-SF2_schott)
-				 (42.378 5.996 air)
-				 (1e9 4.065 air :STO True)
-				 (247.45 6.097 S-BSM18_ohara)
-				 (-40.04 85.59 air)
-				 (1e9 0 air))
-			  and i from 1
-			  collect
-			  (destructuring-bind (radius thickness material &key (STO 'False) (output 'True)) e
-			   `(l.add_surface :number ,i
-					  :radius ,radius
-					  :thickness ,thickness
-					  :glass (string ,material)
-					  :output ,output
-					   :STO ,STO))))
-		 
+		  (setf df (pd.DataFrame
+			    (list
+			     ,@(loop for e
+				       in `((1e9 1e9 air)
+					    (41.15909 6.09755 S-BSM18_ohara)
+					    (-957.83146 9.349 air)
+					    (-51.32 2.032 N-SF2_schott)
+					    (42.378 5.996 air)
+					    (1e9 4.065 air :STO True)
+					    (247.45 6.097 S-BSM18_ohara)
+					    (-40.04 85.59 air)
+					    (1e9 0 air))
+				       and i from 1
+				     collect
+				     (destructuring-bind (radius thickness material &key (STO 'False) (output 'True)) e
+				       `(dict ,@(loop for e in `(radius thickness material STO output)
+						      and f in (list radius thickness `(string ,material) STO output)
+						      collect
+						      
+						      `((string ,e) ,f)))
+				       
+				       ))))))
 		 (do0
-		  (plt.figure)
-		  (l.refresh_paraxial)
-		  (setf d (opr.trace.trace_draw_ray l))
-		  (opr.draw.draw_system l)
-		  (plt.grid))
-		 (do0
+		  (for ((ntuple idx row) (df.iterrows))
+		       (l.add_surface
+			 :number idx
+			 :radius row.radius
+			 :thickness row.thickness
+			 :glass row.material
+			 :output row.output
+			 :STO row.STO)))
+
+		 #+nil 
+		 ((do0
+		   (plt.figure)
+		   (l.refresh_paraxial)
+		   (setf d (opr.trace.trace_draw_ray l))
+		   (opr.draw.draw_system l)
+		   (plt.grid))
+		  (do0
 		  ;; grid n .. rays through y axis of entrance pupil
 		  ;; circular n .. rays rings in entrance pupil
 		  ;; random n .. rays through entrance pupil
 
-		  (plt.figure)
-		  (opr.field.grid_generator :n 6 :grid_type (string "circular")
-					      :output True)
-		  (plt.grid))
-		 (do0
-		  (plt.figure)
-		  (opr.analysis.spotdiagram l (list 1 2 3)
-					    (list 1 2 3)
-					    :n 6
-					    :grid_type (string "circular")))
+		   (plt.figure)
+		   (opr.field.grid_generator :n 6 :grid_type (string "circular")
+					     :output True)
+		   (plt.grid))
+		  (do0
+		   (plt.figure)
+		   (opr.analysis.spotdiagram l (list 1 2 3)
+					     (list 1 2 3)
+					     :n 6
+					     :grid_type (string "circular")))
 
-		 (do0
-		  (plt.figure)
-		  (opr.analysis.Ray_fan l (list 1 2 3)
-					(list 1 2 3)
-					))
-		 (do0
-		  (opr.trace.trace_one_ray l :field_num 2 :wave_num 1
-					     :ray (list 0 -1)
-					     :start 0
-					     :end 0
-					     :output True
-					     :output_list (list ,@(loop for e in `(X Y Z K L M)
-								   collect
-									`(string ,e)))))
-		 (do0
-		  ,@(loop for e in `((image_position)
-				     (EFY)
-				     (EFY 2 3)
-				     (EP)
-				     EPD
-				     (EX)
-				     (OAL))
-			  collect
-			  `(dot l ,e)))))))
+		  (do0
+		   (plt.figure)
+		   (opr.analysis.Ray_fan l (list 1 2 3)
+					 (list 1 2 3)
+					 ))
+		  (do0
+		   (opr.trace.trace_one_ray l :field_num 2 :wave_num 1
+					      :ray (list 0 -1)
+					      :start 0
+					      :end 0
+					      :output True
+					      :output_list (list ,@(loop for e in `(X Y Z K L M)
+									 collect
+									 `(string ,e)))))
+		  (do0
+		   ,@(loop for e in `((image_position)
+				      (EFY)
+				      (EFY 2 3)
+				      (EP)
+				      EPD
+				      (EX)
+				      (OAL))
+			   collect
+			   `(dot l ,e)))))
+	    ))
+	 
+	 )
     (write-source (format nil "~a/source/~a" *path* *code-file*) code)))
 
 
