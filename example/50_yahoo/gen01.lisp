d43236a929194ff5e163257949e8815faad7101b
Martin Kielhorn <kielhorn.martin@gmail.com>
2021-04-21 22:55:30 +0200

    first jupyter notebook
    validation fails
    
    Notebook validation failed: Additional properties are not allowed ('nbformat_error' was unexpected):

---
+++ example/50_yahoo/gen01.lisp
@@ -0,0 +1,89 @@
+(eval-when (:compile-toplevel :execute :load-toplevel)
+  (ql:quickload "cl-py-generator")
+  (ql:quickload "alexandria"))
+(in-package :cl-py-generator)
+
+
+
+(progn
+  (defparameter *path* "/home/martin/stage/cl-py-generator/example/50_yahoo")
+  (defparameter *day-names*
+    '("Monday" "Tuesday" "Wednesday"
+      "Thursday" "Friday" "Saturday"
+      "Sunday"))
+
+  (write-notebook
+   :nb-file (format nil "~a/source/01_scrape.ipynb" *path*)
+   :nb-code
+   `(
+     (python (do0
+	      (do0 
+	       #-nil(do0
+		  
+		     (imports (matplotlib))
+                                        ;(matplotlib.use (string "QT5Agg"))
+					;"from matplotlib.backends.backend_qt5agg import (FigureCanvas, NavigationToolbar2QT as NavigationToolbar)"
+					;"from matplotlib.figure import Figure"
+		     (imports ((plt matplotlib.pyplot)
+					;  (animation matplotlib.animation) 
+					;(xrp xarray.plot)
+			       ))
+                  
+		     (plt.ion)
+					;(plt.ioff)
+		     ;;(setf font (dict ((string size) (string 6))))
+		     ;; (matplotlib.rc (string "font") **font)
+		     )
+	       (imports (		;os
+					;sys
+			 time
+					;docopt
+			 pathlib
+					;(np numpy)
+					;serial
+			 (pd pandas)
+					;(xr xarray)
+					;(xrp xarray.plot)
+					;skimage.restoration
+					;(u astropy.units)
+					; EP_SerialIO
+					;scipy.ndimage
+					;   scipy.optimize
+					;nfft
+					;sklearn
+					;sklearn.linear_model
+					;itertools
+					;datetime
+					; (np numpy)
+					;(cv cv2)
+					;(mp mediapipe)
+					;jax
+					; jax.random
+					;jax.config
+					; copy
+			 re
+			 json
+			 csv
+			 io.StringIO
+			 bs4.BeautifulSoup
+			 requests
+			   
+			 (np jax.numpy)
+			   
+			 ))
+		 
+
+		 
+		 
+	       ,(format nil "from matplotlib.pyplot import ~{~a~^, ~}"
+			`(plot imshow tight_layout xlabel ylabel
+			       title subplot subplot2grid grid
+			       legend figure gcf xlim ylim))
+		 
+		 
+	       )
+	      ))))
+  )
+
+
+
