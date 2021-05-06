fe4c71661db3201153593abd6d706f80b1f657a1
Martin Kielhorn <kielhorn.martin@gmail.com>
2021-05-06 07:55:15 +0200

    more code

--- example/51_django/gen00.lisp
+++ example/51_django/gen00.lisp
@@ -9,22 +9,79 @@
   (defparameter *path* "/home/martin/stage/cl-py-generator/example/51_django/")
 
   (write-source
-   (format nil "~a/mysite/polls/views.py" *path*)
+   (format nil "~a/mysite/polls/views" *path*)
    `(do0
      (imports (django.http))
      (def index (request)
        (return (django.http.HttpResponse (string "hello world"))))))
   (write-source
-   (format nil "~a/mysite/polls/urls.py" *path*)
+   (format nil "~a/mysite/polls/urls" *path*)
    `(do0
-     (import-from django.urls path include)
-     (import-from django.contrib admin)
+     (import-from django.urls path)
      (import-from "." views)
      
      (setf urlpatterns
 	   (list (path (string "")
 		       views.index
-		       :name (string "index")))))))
+		       :name (string "index"))))))
+  (write-source
+   (format nil "~a/mysite/mysite/urls" *path*)
+   `(do0
+     (import-from django.urls path include)
+     (import-from django.contrib admin)
+
+     
+     
+     (setf urlpatterns
+	   (list (path (string "polls/")
+		       (include (string "polls.urls")))
+		 (path (string "admin/")
+		       admin.site.urls)))))
+
+  (write-source
+   (format nil "~a/mysite/mysite/settings" *path*)
+   `(do0
+     (import-from pathlib Path)
+     (setf BASE_DIR (dot (Path __file__)
+			 (resolve)
+			 parent
+			 parent)
+	   SECRET_KEY (string ,(with-open-file (s "secret")
+				 (read s)))
+	   DEBUG True
+	   ALLOWED_HOSTS (list))
+     (setf INSTALLED_APPS
+	   (list
+	    ,@(loop for e in `(admin auth contenttypes sessions messages staticfiles)
+		    collect
+		    `(string ,(format nil "django.contrib.~a")))))
+     (setf MIDDLEWARE
+	   (list
+	    ,@(loop for e in `(middleware.security.Security
+			       contrib.sessions.middleware.Session
+			       middleware.common.Common
+			       middleware.csrf.CsrfView
+			       contrib.auth.middleware.Authentication
+			       contrib.messages.middleware.Message
+			       middleware.clickjacking.XFrameOptions)
+		    collect
+		    `(string ,(format nil "django.~aMiddleware" e)))))
+     (setf ROOT_URLCONF (string "mysite.urls"))
+     (setf TEMPLATES (list
+		      (dictionary :BACKEND (string "django.template.backends.django.DjangoTemplates")
+				  :DIRS (list)
+				  :APP_DIRS True
+				  :OPTIONS (dictionary :context_processors
+						       (list (string "django.template.context_processors.debug")
+							     (string "django.template.context_processors.request")
+							     (string "django.contrib.auth.context_processors.auth")
+							     (string "django.contrib.messages.context_processors.messages"))))))
+     (setf WSGI_APPLICATION (string "mysite.wsgi.application"))
+     (setf DATABASES
+	   (dictionary :default
+		       (dictionary :ENGINE (string "django.db.backends.sqlite3")
+				   :NAME (/ BASE_DIR (string "db.sqlite3")))))
+     )))
 
 
 
