f114546e1715d9e5847695c64e486e6793ebb9ea
Martin Kielhorn <kielhorn.martin@gmail.com>
2021-06-10 08:37:36 +0200

    look at waveforms

--- example/56_myhdl/gen00.lisp
+++ example/56_myhdl/gen00.lisp
@@ -19,34 +19,66 @@
   (let* (
 	 
 	 (code
-	  `(do0
-	    (do0 
-	     (imports ((sys)))
-		 
-		 (setf
-		  _code_git_version
-		  (string ,(let ((str (with-output-to-string (s)
-					(sb-ext:run-program "/usr/bin/git" (list "rev-parse" "HEAD") :output s))))
-			     (subseq str 0 (1- (length str)))))
-		  _code_repository (string ,(format nil "https://github.com/plops/cl-py-generator/tree/master/example/56_myhdl/source/run_00_flop.py")
-					   )
+	   `(do0
+	     
+	    (imports-from (myhdl *)
+			  (random randrange))
+	    (setf
+	     _code_git_version
+	     (string ,(let ((str (with-output-to-string (s)
+				   (sb-ext:run-program "/usr/bin/git" (list "rev-parse" "HEAD") :output s))))
+			(subseq str 0 (1- (length str)))))
+	     _code_repository (string ,(format nil "https://github.com/plops/cl-py-generator/tree/master/example/56_myhdl/source/run_00_flop.py")
+				      )
 
-		  _code_generation_time
-		  (string ,(multiple-value-bind
-				 (second minute hour date month year day-of-week dst-p tz)
-			       (get-decoded-time)
-			     (declare (ignorable dst-p))
-			     (format nil "~2,'0d:~2,'0d:~2,'0d of ~a, ~d-~2,'0d-~2,'0d (GMT~@d)"
-				     hour
-				     minute
-				     second
-				     (nth day-of-week *day-names*)
-				     year
-				     month
-				     date
-				     (- tz))
-			     )))
-		 ))))
+	     _code_generation_time
+	     (string ,(multiple-value-bind
+			    (second minute hour date month year day-of-week dst-p tz)
+			  (get-decoded-time)
+			(declare (ignorable dst-p))
+			(format nil "~2,'0d:~2,'0d:~2,'0d of ~a, ~d-~2,'0d-~2,'0d (GMT~@d)"
+				hour
+				minute
+				second
+				(nth day-of-week *day-names*)
+				year
+				month
+				date
+				(- tz))
+			)))
+
+	    (def dff (q d clk)
+	      (do0
+	       (@always clk.posedge)
+	       (def logic ()
+		 (setf q.next d)))
+	      (return logic))
+
+	    (def test_dff ()
+	      (setf (ntuple q d clk)
+		    (list
+		     (for-generator (i (range 3))
+				    (Signal (bool 0)))))
+	      (setf dff_inst (dff q d clk))
+	      (do0
+	       (@always (delay 10))
+	       (def clkgen ()
+		 (setf clk.next (not clk))))
+
+	      (do0
+	       (@always clk.negedge)
+	       (def stimulus ()
+		 (setf d.next (randrange 2))))
+	      (return (ntuple dff_inst
+				 clkgen
+				 stimulus)))
+
+	    (def simulate (timesteps)
+	      (setf tb (traceSignals test_dff)
+		    sim (Simulation tb))
+	      (sim.run timesteps))
+	    (simulate 2000)
+	    )))
     (write-source (format nil "~a/source/~a" *path* *code-file*) code)))
 
 
