498dc0c74ed86a200a51063e50bb6345c79e84da
Martin Kielhorn <kielhorn.martin@gmail.com>
2021-06-15 22:44:03 +0200

    start some code for android control

---
+++ example/57_android_automation/gen01.lisp
@@ -0,0 +1,43 @@
+(eval-when (:compile-toplevel :execute :load-toplevel)
+  (ql:quickload "cl-py-generator")
+  (ql:quickload "alexandria"))
+(in-package :cl-py-generator)
+
+
+
+(progn
+  (defparameter *path* "/home/martin/stage/cl-py-generator/example/57_android_automation")
+  (defparameter *code-file* "run_01_android_ctl")
+  (defparameter *source* (format nil "~a/source/" *path*))
+  (defparameter *day-names*
+    '("Monday" "Tuesday" "Wednesday"
+      "Thursday" "Friday" "Saturday"
+      "Sunday"))
+  (let* ((code
+	   `(do0
+	     (imports-from (myhdl *)
+			   (random randrange))
+	     (setf
+	      _code_git_version
+	      (string ,(let ((str (with-output-to-string (s)
+				    (sb-ext:run-program "/usr/bin/git" (list "rev-parse" "HEAD") :output s))))
+			 (subseq str 0 (1- (length str)))))
+	      _code_repository (string ,(format nil "https://github.com/plops/cl-py-generator/tree/master/example/56_myhdl/source/04_tang_lcd/run_04_lcd.py"))
+	      _code_generation_time
+	      (string ,(multiple-value-bind
+			     (second minute hour date month year day-of-week dst-p tz)
+			   (get-decoded-time)
+			 (declare (ignorable dst-p))
+			 (format nil "~2,'0d:~2,'0d:~2,'0d of ~a, ~d-~2,'0d-~2,'0d (GMT~@d)"
+				 hour
+				 minute
+				 second
+				 (nth day-of-week *day-names*)
+				 year
+				 month
+				 date
+				 (- tz)))))
+	)))
+    (write-source (format nil "~a/~a" *source* *code-file*) code)
+    ))
+
