86f9362b6226b7712dd85b667a46beffd7d08675
Martin Kielhorn <kielhorn.martin@gmail.com>
2021-06-23 19:48:07 +0200

    sector growth

--- example/60_py_webull/gen01.lisp
+++ example/60_py_webull/gen01.lisp
@@ -39,7 +39,7 @@
 					;sys
 			 time
 					;docopt
-			 pathlib
+			 ;pathlib
 					;(np numpy)
 					;serial
 			 (pd pandas)
@@ -62,20 +62,25 @@
 					; jax.random
 					;jax.config
 					; copy
-			 re
-			 json
-			 csv
-			 ;io.StringIO
-			 bs4
-			 requests
+			 ;re
+			 ;json
+			 ; csv
+					;io.StringIO
+					;bs4
+					;requests
 			   
-			 (np jax.numpy)
-			 (mpf mplfinance)
+					;(np jax.numpy)
+					;(mpf mplfinance)
 			   
 			 ))
 		 
 	       (imports-from (selenium webdriver)
 			     (selenium.webdriver.common.keys Keys)
+			     (selenium.webdriver.support.ui WebDriverWait)
+			     (selenium.webdriver.common.by By)
+			     (selenium.webdriver.support expected_conditions)
+			     
+			     
 			     )
 		 
 	       
@@ -113,9 +118,69 @@
 	     debug True)))
 
      (python (do0
-	      (setf driver (webdriver.Firefox))
+	      (comments "open browser that can be automated with python")
+	      (comments "zoom out, so that everything is rendered")
+	      (setf fp (webdriver.FirefoxProfile))
+	      (fp.set_preference (string "layout.css.devPixelsPerPx") (string ".5"))
+	      (setf driver (webdriver.Firefox fp))
+	      (driver.set_window_size (* 2 1900) (* 2 1040))
 	      (driver.get (string "https://app.webull.com/market/region/6"))))
-    
+     (python
+      (do0
+       (comments "wait up to 3 seconds for elements to appear")
+       (driver.implicitly_wait 3)))
+
+     #+nil (python
+      (do0 ;; doesnt work in firefox
+       (comments "tables that are not visible in the window will return empty text. ensure that all information is rendered by zooming out")
+       (driver.execute_script (string "document.body.style.zoom='50 %'"))))
+
+     (python
+      (do0
+       (setf a (driver.find_element_by_xpath (string "/html/body/div/div/div[3]/div/div[1]/div/div[2]/div/div/div[3]/div[1]/span/div/div")))
+       (a.click)))
+
+     (python
+      (do0
+       (comments "get header title with sector name")
+					;                                                                               row     col
+					;"/html/body/div/div/div[3]/div/div[1]/div/div[2]/div[1]/div[2]/div/div/div[1]/div[1]/div[1]/div[1]/span[1]"
+					;"/html/body/div/div/div[3]/div/div[1]/div/div[2]/div[1]/div[2]/div/div/div[1]/div[1]/div[2]/div[1]/span[1]"
+					;"/html/body/div/div/div[3]/div/div[1]/div/div[2]/div[1]/div[2]/div/div/div[1]/div[1]/div[3]/div[1]/span[1]"
+					;"/html/body/div/div/div[3]/div/div[1]/div/div[2]/div[1]/div[2]/div/div/div[1]/div[2]/div[1]/div[1]/span[1]"
+					;"/html/body/div/div/div[3]/div/div[1]/div/div[2]/div[1]/div[2]/div/div/div[1]/div[3]/div[1]/div[1]/span[1]"
+					;"/html/body/div/div/div[3]/div/div[1]/div/div[2]/div[1]/div[2]/div/div/div[1]/div[5]/div[3]/div[1]/span[1]"
+       (setf header (driver.find_element_by_xpath
+		     (string "/html/body/div/div/div[3]/div/div[1]/div/div[2]/div[1]/div[2]/div/div/div[1]/div[3]/div[1]/div[1]/span[1]"))
+	     perc (driver.find_element_by_xpath
+		     (string "/html/body/div/div/div[3]/div/div[1]/div/div[2]/div[1]/div[2]/div/div/div[1]/div[3]/div[1]/div[1]/span[2]")))
+       (comments "get growth percentage for the entire sector")
+       (display header.text)
+       (display perc.text)))
+     (python
+      (do0
+       (comments "get all sector names from the grid of tables")
+       (setf res (list ))
+       (for (col (range 1 (+ 1 3)))
+	    (for (row (range 1 (+ 1 5)))
+		 (setf header (driver.find_element_by_xpath
+			       (dot (string "/html/body/div/div/div[3]/div/div[1]/div/div[2]/div[1]/div[2]/div/div/div[1]/div[{}]/div[{}]/div[1]/span[1]")
+				    (format row col))))
+		 (setf perc (driver.find_element_by_xpath
+			       (dot (string "/html/body/div/div/div[3]/div/div[1]/div/div[2]/div[1]/div[2]/div/div/div[1]/div[{}]/div[{}]/div[1]/span[2]")
+				    (format row col))))
+		#+nil  (setf myelem (dot (WebDriverWait driver delay)
+				   (until
+				    expected_conditions.presence_of_element_located
+				    (tuple By.ID
+					   (string "")))))
+		 (res.append (dictionary :row row
+					 :col col
+					 :sector header.text
+					 :sector_growth perc.text))))
+       (setf df (pd.DataFrame res))
+       (dot df (sort_values :by (string "sector_growth")
+			    :ascending False))))
 
      ))
   )
