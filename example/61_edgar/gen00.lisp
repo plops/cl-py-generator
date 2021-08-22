2e382eff22aefbda724bb47fb6f6bfb3c7059d57
Martin Kielhorn <kielhorn.martin@gmail.com>
2021-08-22 23:52:01 +0200

    this downloaded 504MB in a few seconds
    -rw-rw-r--. 1 martin martin 20430567 Aug 22 23:51 2021-QTR3.tsv
    -rw-rw-r--. 1 martin martin 41962351 Aug 22 23:51 2021-QTR2.tsv
    -rw-rw-r--. 1 martin martin 50679861 Aug 22 23:51 2021-QTR1.tsv
    -rw-rw-r--. 1 martin martin 32830100 Aug 22 23:51 2020-QTR4.tsv
    -rw-rw-r--. 1 martin martin 31226027 Aug 22 23:51 2020-QTR3.tsv
    -rw-rw-r--. 1 martin martin 37329956 Aug 22 23:51 2020-QTR2.tsv
    -rw-rw-r--. 1 martin martin 45553918 Aug 22 23:51 2020-QTR1.tsv
    -rw-rw-r--. 1 martin martin 29071262 Aug 22 23:51 2019-QTR4.tsv
    -rw-rw-r--. 1 martin martin 27925701 Aug 22 23:51 2019-QTR3.tsv
    -rw-rw-r--. 1 martin martin 34989897 Aug 22 23:51 2019-QTR2.tsv
    -rw-rw-r--. 1 martin martin 41623665 Aug 22 23:51 2019-QTR1.tsv
    -rw-rw-r--. 1 martin martin 28119073 Aug 22 23:51 2018-QTR4.tsv
    -rw-rw-r--. 1 martin martin 27465928 Aug 22 23:51 2018-QTR3.tsv
    -rw-rw-r--. 1 martin martin 34885866 Aug 22 23:51 2018-QTR2.tsv
    -rw-rw-r--. 1 martin martin 42651392 Aug 22 23:51 2018-QTR1.tsv
    [martin@localhost data]$ less 2018-QTR1.tsv
    [martin@localhost data]$ du -hs
    503M    .

--- example/61_edgar/gen00.lisp
+++ example/61_edgar/gen00.lisp
@@ -89,7 +89,9 @@
 	     (edgar.download_index
 	      (string "/home/martin/stage/cl-py-generator/example/61_edgar/source/data")
 	      2018
-	      (string "Mozilla/5.0 (X11; Fedora; Linux x86_64; rv:90.0) Gecko/20100101 Firefox/90.0")
+	      (string "Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.107 Mobile Safari/537.36"
+		      ;"Mozilla/5.0 (X11; Fedora; Linux x86_64; rv:90.0) Gecko/20100101 Firefox/90.0"
+		      )
 	      :skip_all_present_except_last False)
 	     
 	     )))
