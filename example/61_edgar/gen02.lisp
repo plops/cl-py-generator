bd9ef0e8eb17655a5081671ebf6da0a68b65e0f7
Martin Kielhorn <kielhorn.martin@gmail.com>
2021-08-24 07:04:06 +0200

    the other library (py-xbrl) fails with
    Traceback (most recent call last):
      File "/home/martin/stage/cl-py-generator/example/61_edgar/source/./run_02_parse_edgar.py", line 36, in <module>
        inst = parser.parse_instance_locally(str(fn))
      File "/home/martin/.local/lib/python3.9/site-packages/xbrl/instance.py", line 671, in parse_instance_locally
        return parse_xbrl(path, self.cache, instance_url)
      File "/home/martin/.local/lib/python3.9/site-packages/xbrl/instance.py", line 306, in parse_xbrl
        taxonomy: TaxonomySchema = parse_taxonomy(schema_path, cache)
      File "/home/martin/.local/lib/python3.9/site-packages/xbrl/taxonomy.py", line 231, in parse_taxonomy
        raise TaxonomyNotFound(f"Could not find taxonomy schema at {schema_path}")
    xbrl.TaxonomyNotFound: The taxonomy with namespace Could not find taxonomy schema at xml/mu-20200528.xsd could not be found. Please check if it is imported in the schema file

--- example/61_edgar/gen02.lisp
+++ example/61_edgar/gen02.lisp
@@ -62,8 +62,12 @@
 					;requests
 		       ;xsdata
 					;generated
-		       xbrl
+		     ;  xbrl
 		       ))
+	     (imports (logging
+		       xbrl
+		       xbrl.cache
+		       xbrl.instance))
 	     ;"from generated import *"
 	     ;"from xsdata.formats.dataclass.parsers import XmlParser"
 	     
@@ -94,41 +98,73 @@
 			(dot 
 			 (pathlib.Path (string "xml/"))
 			 (glob (string "*.xml")))))
-					;(setf fn (aref fns 0))
+	     
+	     (do0
+	      (setf user_agent (string "Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.107 Mobile Safari/537.36"))
+	      (logging.basicConfig :level logging.INFO)
+	      (do0 
+			    (comments "py-xbrl")
+			    (setf cache (xbrl.cache.HttpCache (string "./cache")))
+			    (cache.set_connection_params
+			     :delay 500
+			     :retries 5
+			     :backoff_factor .8
+			     :logs True)
+			    (cache.set_headers (dict ((string "From") (string "your.name@company.com"))
+						     ((string "User-Agent") user_agent)))
+			    (setf parser (xbrl.instance.XbrlParser cache
+								   )
+				  )))
 	     (do0 (setf res (list))
-		  (for (fn (tqdm.tqdm fns))
+		  (for (fn (list (aref fns 0))
+			   ;(tqdm.tqdm fns)
+			   )
+		       
 		       (do0
-			(comments "python-xbrl")
-			(try
-			 (do0
-			  (setf parser (xbrl.XBRLParser)
-				xb (parser.parse (open fn))
-				gaap (parser.parseGAAP xb
-						       :doc_date (dot fn
-								      stem
-								      (aref (split (string "-"))
-									    -1))
-						       :context (string "current")
-						       :ignore_errors 0)
-				seri (xbrl.GAAPSerializer)
-				d (seri.dump gaap)
-				)
-			  (setf (aref d (string "filename"))
-			      fn)
-			  (res.append d)
-			  )
-			 ("Exception as e"
-			  ,(lprint "exception" `(e fn))
-			  pass))
+			
+			(do0 ; try
+			   (do0
+			    (do0
+			     (comments "py-xbrl")
+			     (setf inst (parser.parse_instance_locally (str fn)))
+			     (setf d (dictionary :filename (str fn)))
+			     (res.append d)
+			     )
+			    #+nil (do0
+				   (comments "python-xbrl")
+				   (setf parser (xbrl.XBRLParser)
+					 xb (parser.parse (open fn))
+					 gaap (parser.parseGAAP xb
+								:doc_date (dot fn
+									       stem
+									       (aref (split (string "-"))
+										     -1))
+								:context (string "current")
+								:ignore_errors 0)
+					 seri (xbrl.GAAPSerializer)
+					 d (seri.dump gaap)
+					 )
+				   (setf (aref d (string "filename"))
+					 fn)
+				   (res.append d)
+				   ))
+			  #+nil  ("Exception as e"
+			    ,(lprint "exception" `(e fn))
+			  
+			    (res.append (dictionary :filename fn
+						    :comment e))
+			   
+			    pass))
 			
 			)))
 	     (setf df (pd.DataFrame res))
-	     (do0
-	      (comments "remove columns that only contain zeros")
-	      (setf df (dot df (aref loc ":"
-			     (dot (!= df 0)
-				  (any :axis 0))))))
-	     (setf df (df.sort_values :by (string "filename")))
+	     #+nil
+	     (do0 (do0
+	       (comments "remove columns that only contain zeros")
+	       (setf df (dot df (aref loc ":"
+				      (dot (!= df 0)
+					   (any :axis 0))))))
+		  (setf df (df.sort_values :by (string "filename"))))
 	     (df.to_csv (string "mu.csv"))
 	     	     )))
     (write-source (format nil "~a/~a" *source* *code-file*) code)
