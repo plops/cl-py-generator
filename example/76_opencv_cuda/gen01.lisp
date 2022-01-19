8994a5fd8b88d68ddc345d90db55f9b6acd375b4
martin <martin@pop-os.localdomain>
2022-01-19 06:48:27 +0100

    list checkerboard images

--- example/76_opencv_cuda/gen01.lisp
+++ example/76_opencv_cuda/gen01.lisp
@@ -7,7 +7,7 @@
 (progn
   (defparameter *repo-dir-on-host* "/home/martin/stage/cl-py-generator")
   (defparameter *repo-dir-on-github* "https://github.com/plops/cl-py-generator/tree/master/")
-  (defparameter *example-subdir* "example/75_jax_render")
+  (defparameter *example-subdir* "example/76_opencv_cuda")
   (defparameter *path* (format nil "~a/~a" *repo-dir-on-host* *example-subdir*) )
   (defparameter *day-names*
     '("Monday" "Tuesday" "Wednesday"
@@ -15,9 +15,11 @@
       "Sunday"))
   (defparameter *libs*
     `((np numpy)
+      cv2
       (pd pandas)
-      jax
-      gr
+      
+      ;jax
+      ;gr
 					;(xr xarray)
       ;;matplotlib
 					;(s skyfield)
@@ -31,19 +33,7 @@
     :nb-code
     `((python (do0
 	       "# default_exp play01"))
-      (python
-       (do0
-	"#export"
-	
-	(comments "spread jax work over 4 virtual cpu cores:")
-	(imports (os
-		  multiprocessing))
-	(setf cpu_count (multiprocessing.cpu_count))
-	(print (dot (string "jax will spread work to {} cpus")
-		    (format cpu_count)))
-	(setf (aref os.environ (string "XLA_FLAGS"))
-	      (dot (string "--xla_force_host_platform_device_count={}")
-		   (format cpu_count)))))
+      
       (python (do0
 	       
 	       "#export"
@@ -70,7 +60,7 @@
 					;sys
 			  time
 					;docopt
-					;pathlib
+					pathlib
 					;(np numpy)
 					;serial
 					;(pd pandas)
@@ -106,26 +96,25 @@
 					;(nx networkx)
 					;(np jax.numpy)
 					;(mpf mplfinance)
-			  argparse
+			  ;argparse
 			  ;(sns seaborn)
 			  ;skyfield.api
 			  ;skyfield.data
 					;skyfield.data.hipparcos
-			  (jnp jax.numpy)
-			  jax.config
-			  jax.scipy.optimize
-			  jax.experimental.maps
-			  jax.numpy.linalg
-			  jax.nn
-			  numpy.random
+			  ;(jnp jax.numpy)
+			  ;jax.config
+			  ;jax.scipy.optimize
+			  ;jax.experimental.maps
+			  ;jax.numpy.linalg
+			  ;jax.nn
+			  
 			  ))
 		#+nil
 			(imports-from (matplotlib.pyplot
 			       plot imshow tight_layout xlabel ylabel
 			       title subplot subplot2grid grid
 			       legend figure gcf xlim ylim))
-		(imports-from (gr.pygr
-			       mlab))
+		#+nil
 		(imports-from (jax.experimental.maps
 			       xmap))
 
@@ -134,7 +123,6 @@
       (python
        (do0
 	"#export"
-	(jax.config.update (string "jax_enable_x64") True)
 	(setf
 	 _code_git_version
 	 (string ,(let ((str (with-output-to-string (s)
@@ -185,84 +173,14 @@
 
       (python
        (do0
-	(def volumetric_rendering (color sigma z_vals dirs)
-	  (comments "formulate rendering process for semi-transparent volumes that mimic front-to-back additive blending"
-		    "as camera ray traverses volume of inhomogeneous material, it accumulates color in proportion to the local color and density of the material at each point along its path")
-	  (setf eps 1s-10
-		inv_eps (/ 1s0 eps)
-		z_right (aref z_vals "..." (slice 1 ""))
-		z_left (aref z_vals "..." (slice "" -1))
-		dists (- z_right
-			 z_left)
-		)
-	  #+nil (do0
-	   (comments "ray relative distance to absolute distance"
-		     "not necessary if rays_d is normalized")
-	   (setf 
-	    dists (* dists (jnp.linalg.norm
-			    (aref dirs "..."
-				  None (string ":"))
-			    :axis -1))))
-	  (do0
-	   (comments "fraction of light stuck in each voxel")
-	   (setf alpha (- 1s0
-			  (jnp.exp (* -1
-				      (jax.nn.relu sigma)
-				      dists)))))
-	  (setf accum_prod
-		(jnp.concatenate
-		 (list (jnp.ones_like (aref alpha
-					    "..."
-					    (slice "" 1))
-				      alpha.dtype)
-		       (jnp.cumprod (+ -1s0
-				       (aref alpha
-					     "..."
-					     (slice "" -1))
-				       eps)
-				    :axis -1))))
-	  (do0
-	   (comments "absolute amount of light stuck in each voxel")
-	   (setf weights (* alpha accum_prod))
-	   (setf comp_color (dot
-			     (* (aref weights
-				      "..."
-				      None)
-				(jax.nn.sigmoid color))
-			     (sum :axis -2))))
-	  (do0
-	   (comments "weighted average of the depths by contribution to final color")
-	   (setf depth (dot (* weights
-			       z_left)
-			    (sum :axis -1))))
-	  (do0
-	   (comments "total amount of light absorbed along the ray")
-	   (setf acc (dot weights
-			  (sum :axis -1)))
-	   )
-	  (do0
-	   (comments "equivalent to disp = 1/max(eps, where(acc>eps,depth/acc,0))"
-		     "but more efficient and stable"
-		     "to model occlusions the ray accumulates not only color but also opacity"
-		     "if accumulated opacity reaches 1 for example when the ray traverses an opaque region then no further color can be accumulated on the ray (not sure if this code behaves like this)")
-	   (setf disparity (/ acc depth)
-		 disparity (jnp.where (&
-				       (< 0 disparity)
-				       (< disparity inv_eps)
-				       (< eps acc))
-				      disparity
-				      inv_eps)))
-	  #+nil (do0
-	   (comments "include white background in final color")
-	   (setf comp_color (+ comp_color
-			       (- 1s0
-				  (aref acc "..." None)))))
-	  (return comp_color
-		  disparity
-		  acc
-		  weights)
-	  )
-	))
+	(comments "https://docs.opencv.org/4.x/dc/dbb/tutorial_py_calibration.html")
+	(setf fns ("list"
+		   (dot (pathlib.Path (string "/home/martin/src/opencv-4.5.5/samples/data/"))
+			(glob (string "left*.jpg")))))
+	(display fns)
+
+	)
+      )
       
       ))))
 
--- example/76_opencv_cuda/source/run.sh
+++ example/76_opencv_cuda/source/run.sh
@@ -1,2 +1,2 @@
 nbdev_build_lib
-python3 -i render/play01.py
+python3 -i viewcam/play01.py
