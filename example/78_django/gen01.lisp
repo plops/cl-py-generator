21ff7d22b03a45eddf98a91bdd5f52e19c0d2f8e
Martin Kielhorn <kielhorn.martin@gmail.com>
2022-01-29 07:32:13 +0100

    start with django experiment. setup venv

---
+++ example/78_django/gen01.lisp
@@ -0,0 +1,216 @@
+(eval-when (:compile-toplevel :execute :load-toplevel)
+  (ql:quickload "cl-py-generator"))
+
+(in-package :cl-py-generator)
+
+
+(progn
+  (defparameter *repo-dir-on-host* "/home/martin/stage/cl-py-generator")
+  (defparameter *repo-dir-on-github* "https://github.com/plops/cl-py-generator/tree/master/")
+  (defparameter *example-subdir* "example/77_wxpython")
+  (defparameter *path* (format nil "~a/~a" *repo-dir-on-host* *example-subdir*) )
+  (defparameter *day-names*
+    '("Monday" "Tuesday" "Wednesday"
+      "Thursday" "Friday" "Saturday"
+      "Sunday"))
+  (defparameter *libs*
+    `(;(np numpy)
+      ;(cv cv2)
+      (pd pandas)
+      ;(xr xarray)
+      ;lmfit
+      ;rawpy
+      wx
+      ))
+  (defun scale (da &key (perc 2) (range 10))
+    (let ((d `(- ma mi)))
+      `(do0
+	(setf mi (np.nanpercentile ,da ,perc)
+	      ma (np.nanpercentile ,da (- 100
+					 ,perc))
+	      mi1 (- mi (* ,(/ range 100.0) ,d))
+	      ma1 (+ ma (* ,(/ range 100.0) ,d))
+	      ))))
+  (defun lprint (&optional rest)
+    `(print (dot (string ,(format nil "~{~a={}~^\\n~}" rest))
+                 (format  ;(- (time.time) start_time)
+                  ,@rest))))
+  (let ((nb-file "source/01_basic_gui.ipynb"))
+   (write-notebook
+    :nb-file (format nil "~a/~a" *path* nb-file)
+    :nb-code
+    `((python (do0
+	       "# default_exp run01_basic_gui"
+	       (comments "pip3 install --user wxPython"
+			 "note: this seems to compile wxwidgets and can take a while")))
+      (python (do0
+	       "#export"
+	       (do0
+					
+		#+nil (do0
+			;"%matplotlib notebook"
+		      	(imports (matplotlib))
+			(imports ((plt matplotlib.pyplot)
+					;  (animation matplotlib.animation) 
+					;(xrp xarray.plot)
+				  ))
+			(plt.ion)
+					;(plt.ioff)
+			;;(setf font (dict ((string size) (string 6))))
+			;; (matplotlib.rc (string "font") **font)
+			)
+		(imports (		;os
+					;sys
+			  time
+					;docopt
+			  ;pathlib
+					;(np numpy)
+					;serial
+					;(pd pandas)
+			  ;(xr xarray)
+			  ,@*libs*
+					;		(xrp xarray.plot)
+					;skimage.restoration
+					;skimage.morphology
+					;(u astropy.units)
+					; EP_SerialIO
+					;scipy.ndimage
+			  ;scipy.optimize
+			  ;scipy.stats
+			  ;scipy.special
+					;nfft
+					;sklearn
+					;sklearn.linear_model
+					;itertools
+					;datetime
+					; (np numpy)
+					;(cv cv2)
+					;(mp mediapipe)
+					;jax
+					; jax.random
+					;jax.config
+					; copy
+					;re
+					;json
+					; csv
+					;io.StringIO
+					;bs4
+					;requests
+					;(nx networkx)
+					;(np jax.numpy)
+					;(mpf mplfinance)
+			  ;argparse
+			  ;(sns seaborn)
+					;(jnp jax.numpy)
+					;jax.config
+					;jax.scipy.optimize
+					;jax.experimental.maps
+					;jax.numpy.linalg
+					;jax.nn
+			  ;cv2.aruco
+			  ;tqdm
+			  ;decimal
+			  ))
+		#+nil
+			(imports-from (matplotlib.pyplot
+			       plot imshow tight_layout xlabel ylabel
+			       title subplot subplot2grid grid
+			       legend figure gcf xlim ylim))
+		#+nil
+		(imports-from (jax.experimental.maps
+			       xmap))
+
+		 )
+	       ))
+      (python
+       (do0
+	"#export"
+	(setf
+	 _code_git_version
+	 (string ,(let ((str (with-output-to-string (s)
+			       (sb-ext:run-program "/usr/bin/git" (list "rev-parse" "HEAD") :output s))))
+		    (subseq str 0 (1- (length str)))))
+	 _code_repository (string ,(format nil "~a/~a/~a" *repo-dir-on-github* *example-subdir* nb-file))
+	 _code_generation_time
+	 (string ,(multiple-value-bind
+			(second minute hour date month year day-of-week dst-p tz)
+		      (get-decoded-time)
+		    (declare (ignorable dst-p))
+		    (format nil "~2,'0d:~2,'0d:~2,'0d of ~a, ~d-~2,'0d-~2,'0d (GMT~@d)"
+			    hour
+			    minute
+			    second
+			    (nth day-of-week *day-names*)
+			    year
+			    month
+			    date
+			    (- tz)))))
+
+	(setf start_time (time.time)
+	      debug True)))
+      (python
+       (do0
+	"#export"
+	(setf df_status
+	      (pd.DataFrame
+	       (list
+		,@(loop for e in *libs*
+			collect
+			(cond
+			  ((listp e)
+			   (destructuring-bind (nick name) e
+			     `(dictionary
+			       :name (string ,name)
+			       :version
+			       (dot ,nick __version__)
+			       )))
+			  ((symbolp e)
+			   `(dictionary
+			       :name (string ,e)
+			       :version
+			       (dot ,e __version__)
+			       ))
+			  (t (break "problem")))))))
+	(print df_status)))
+      (python
+       (do0
+	"#export"
+	(class ImagePanel (wx.Panel)
+	       (def __init__ (self parent image_size)
+		 (dot (super)
+		      (__init__ parent))
+		 (setf img (wx.Image *image_size)
+		       self.image_ctrl (wx.StaticBitmap self
+							:bitmap (wx.Bitmap img)))
+		 (setf browser_btn (wx.Button self :label (string "Browse")))
+		 (do0
+		  (setf main_sizer (wx.BoxSizer wx.VERTICAL))
+		  (main_sizer.Add self.image_ctrl 0 wx.ALL 5)
+		  (main_sizer.Add browser_btn)
+		  (self.SetSizer main_sizer))
+		 (main_sizer.Fit parent)
+		 (self.Layout)
+		 )
+	       )
+	(class MainFrame (wx.Frame)
+	       (def __init__ (self)
+		 (dot (super)
+		      (__init__ None :title (string "image viewer")))
+		 (do0
+		  (setf panel (ImagePanel self
+					  :image_size (list 240 240)))
+		  (self.Show)))
+	       ))
+       )
+      (python
+       (do0
+	"#export"
+	(when (== __name__ (string "__main__"))
+	  (setf app (wx.App :redirect False)
+		frame (MainFrame)
+		)
+	  (app.MainLoop))))))))
+
+
+
+ 
