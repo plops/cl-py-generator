9232235211c6845f15ce0baa8a48827ed6b1466a
Martin Kielhorn <kielhorn.martin@gmail.com>
2022-01-29 08:26:34 +0100

    typo
    (env) [martin@localhost web]$ sh setup006_makemigrations.sh
    SystemCheckError: System check identified some issues:
    
    ERRORS:
    ?: (urls.E006) The MEDIA_URL setting must end with a slash.

--- example/78_django/source/web/pygram/settings.py
+++ example/78_django/source/web/pygram/settings.py
@@ -122,4 +122,4 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-MEDIA_URL='/media'
+MEDIA_URL='/media/'
