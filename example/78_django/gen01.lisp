fe0b05b55b7bb1fe33a34d0069cc842ec9212743
Martin Kielhorn <kielhorn.martin@gmail.com>
2022-01-29 07:53:06 +0100

    looks like nbdev can emit into particular directories and therefor
    work together with django

--- example/78_django/gen01.lisp
+++ example/78_django/gen01.lisp
@@ -69,143 +69,24 @@
 	 ))
   
   
-  (let ((nb-file "source/01_basic_gui.ipynb"))
+  (let ((nb-file "source/01_posts_models.ipynb"))
    (write-notebook
     :nb-file (format nil "~a/~a" *path* nb-file)
     :nb-code
     `((python (do0
-	       "# default_exp run01_basic_gui"
-	       (comments "pip3 install --user wxPython"
-			 "note: this seems to compile wxwidgets and can take a while")))
+	       "# default_exp posts/models"
+	       ))
       (python (do0
 	       "#export"
 	       (do0
-					
-		#+nil (do0
-			;"%matplotlib notebook"
-		      	(imports (matplotlib))
-			(imports ((plt matplotlib.pyplot)
-					;  (animation matplotlib.animation) 
-					;(xrp xarray.plot)
-				  ))
-			(plt.ion)
-					;(plt.ioff)
-			;;(setf font (dict ((string size) (string 6))))
-			;; (matplotlib.rc (string "font") **font)
-			)
-		(imports (		;os
-					;sys
-			  time
-					;docopt
-			  ;pathlib
-					;(np numpy)
-					;serial
-					;(pd pandas)
-			  ;(xr xarray)
-			  ,@*libs*
-					;		(xrp xarray.plot)
-					;skimage.restoration
-					;skimage.morphology
-					;(u astropy.units)
-					; EP_SerialIO
-					;scipy.ndimage
-			  ;scipy.optimize
-			  ;scipy.stats
-			  ;scipy.special
-					;nfft
-					;sklearn
-					;sklearn.linear_model
-					;itertools
-					;datetime
-					; (np numpy)
-					;(cv cv2)
-					;(mp mediapipe)
-					;jax
-					; jax.random
-					;jax.config
-					; copy
-					;re
-					;json
-					; csv
-					;io.StringIO
-					;bs4
-					;requests
-					;(nx networkx)
-					;(np jax.numpy)
-					;(mpf mplfinance)
-			  ;argparse
-			  ;(sns seaborn)
-					;(jnp jax.numpy)
-					;jax.config
-					;jax.scipy.optimize
-					;jax.experimental.maps
-					;jax.numpy.linalg
-					;jax.nn
-			  ;cv2.aruco
-			  ;tqdm
-			  ;decimal
-			  ))
-		#+nil
-			(imports-from (matplotlib.pyplot
-			       plot imshow tight_layout xlabel ylabel
-			       title subplot subplot2grid grid
-			       legend figure gcf xlim ylim))
-		#+nil
-		(imports-from (jax.experimental.maps
-			       xmap))
-
-		 )
+		(imports-from (django.db models)))
 	       ))
+      
       (python
        (do0
 	"#export"
-	(setf
-	 _code_git_version
-	 (string ,(let ((str (with-output-to-string (s)
-			       (sb-ext:run-program "/usr/bin/git" (list "rev-parse" "HEAD") :output s))))
-		    (subseq str 0 (1- (length str)))))
-	 _code_repository (string ,(format nil "~a/~a/~a" *repo-dir-on-github* *example-subdir* nb-file))
-	 _code_generation_time
-	 (string ,(multiple-value-bind
-			(second minute hour date month year day-of-week dst-p tz)
-		      (get-decoded-time)
-		    (declare (ignorable dst-p))
-		    (format nil "~2,'0d:~2,'0d:~2,'0d of ~a, ~d-~2,'0d-~2,'0d (GMT~@d)"
-			    hour
-			    minute
-			    second
-			    (nth day-of-week *day-names*)
-			    year
-			    month
-			    date
-			    (- tz)))))
-
-	(setf start_time (time.time)
-	      debug True)))
-      (python
-       (do0
-	"#export"
-	(setf df_status
-	      (pd.DataFrame
-	       (list
-		,@(loop for e in *libs*
-			collect
-			(cond
-			  ((listp e)
-			   (destructuring-bind (nick name) e
-			     `(dictionary
-			       :name (string ,name)
-			       :version
-			       (dot ,nick __version__)
-			       )))
-			  ((symbolp e)
-			   `(dictionary
-			       :name (string ,e)
-			       :version
-			       (dot ,e __version__)
-			       ))
-			  (t (break "problem")))))))
-	(print df_status)))
+	(class Post (models.Model)
+	      (setf image (models.ImageField)))))
       ))))
 
 
