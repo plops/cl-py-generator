420782745f7a8b2df09bacdeb12ae764a3d76046
Martin Kielhorn <kielhorn.martin@gmail.com>
2022-01-29 08:41:00 +0100

    add view

--- example/78_django/gen01.lisp
+++ example/78_django/gen01.lisp
@@ -131,6 +131,37 @@
 			    (setf post (models.ForeignKey Post :on_delete models.CASCADE)
 				  text (models.TextField))
 			    ,info)))))
+	    ))
+     (gen `(posts views)
+	  `(
+	    (python (do0
+		     "#export"
+	       (do0 
+		(imports-from (django.views.generic.list ListView)
+			      (posts.models Post)
+			      )
+		(class PostList (ListView)
+		       (setf model Post)
+		       ))
+	       ))
+	    
+	    (python
+	     (do0
+	      "#export"
+	      (comments "the class is a table and each element is a column in the table")
+	      ,(let ((info `(setf author (models.ForeignKey User
+							    :on_delete models.CASCADE)
+				  created models.DateTimeField :auto_now_added True
+				  modified models.DateTimeField :auto_now_added True)))
+	       `(do0 (class Post (models.Model)
+			(setf image (models.ImageField)
+			      description (models.TextField))
+			,info
+			)
+		     (class Comment (models.Model)
+			    (setf post (models.ForeignKey Post :on_delete models.CASCADE)
+				  text (models.TextField))
+			    ,info)))))
 	    ))))
 
   (sb-ext:run-program "/usr/bin/sh" `("/home/martin/stage/cl-py-generator/example/78_django/source/setup01_nbdev.sh")
