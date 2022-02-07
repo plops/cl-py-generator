b66af4ff5c1c5e35e652d759027ebd660980abc6
Martin Kielhorn <kielhorn.martin@gmail.com>
2022-02-07 23:11:28 +0100

    start some experiments with splines

---
+++ example/82_spline/gen01.lisp
@@ -0,0 +1,68 @@
+(eval-when (:compile-toplevel :execute :load-toplevel)
+  (ql:quickload "cl-py-generator"))
+
+(in-package :cl-py-generator)
+
+(progn
+  ;; the following code needs inverted readtable, otherwise symbols
+  ;; and filenames may have the wrong case and everything breaks in
+  ;; horrible ways
+  (assert (eq :invert
+	      (readtable-case *readtable*)))
+  (defparameter *repo-dir-on-host* "/home/martin/stage/cl-py-generator")
+  (defparameter *repo-dir-on-github* "https://github.com/plops/cl-py-generator/tree/master/")
+  (defparameter *example-subdir* "example/82_spline")
+  (defparameter *path* (format nil "~a/~a" *repo-dir-on-host* *example-subdir*) )
+  (defun lprint (&key msg vars)
+    `(print (dot (string ,(format nil "{:7.6f} \\033[31m ~a \\033[0m ~{~a={}~^ ~}" msg vars))
+                 (format
+		  (- (time.time) start_time)
+                  ,@vars))))
+  (let ((nb-counter 1))
+    (flet ((gen (path code)
+	     "create jupyter notebook file in a directory below source/"
+	     (let ((fn  (format nil "source/~3,'0d_~{~a~^_~}.ipynb" nb-counter path)))
+	       (write-notebook
+		:nb-file fn
+		:nb-code (append `((python (do0
+					    (comments
+					     ,(format nil "default_exp ~{~a~^/~}_~2,'0d" path nb-counter)))))
+				 code))
+	       (format t "~&~c[31m wrote Python ~c[0m ~a~%"
+		       #\ESC #\ESC fn))
+	     (incf nb-counter)))
+      (let* ((lam-term `(5e9 316 3.16 ))
+	     (model `(LinearGAM (+ (s 0 :n_splines 20 :lam ,(elt lam-term 0))
+				      (s 1 :n_splines 20 :lam ,(elt lam-term 1))
+				      (f 2 :lam ,(elt lam-term 2)))))
+	     (search-up 5)
+	     (search-down 5))
+       (gen `(gam)
+	    `((python
+	       (cell
+		(comments "python3 -m pip install --user pygam matplotlib pandas")
+		(imports ((plt matplotlib.pyplot)))
+		(plt.ion)
+		(imports (pathlib
+			  time
+			  tqdm
+			  (pd pandas)
+			  (np numpy)
+			  numpy.random
+			  ))
+		(imports-from (matplotlib.pyplot plot figure scatter gca sca subplots subplots_adjust title xlabel ylabel xlim ylim grid))
+		(setf start_time (time.time))))
+	     
+	      (python
+	       (cell
+		,(lprint :msg "generate dataset")
+		
+		))
+	      )))))
+  (sb-ext:run-program "/usr/bin/sh"
+		      `("/home/martin/stage/cl-py-generator/example/82_spline/source/setup01_nbdev.sh"))
+  (format t "~&~c[31m ran nbdev ~c[0m ~%" #\ESC #\ESC ))
+
+
+
+
