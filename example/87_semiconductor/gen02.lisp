c32809f8e3d704282d54962383333b0246fdeecd
Martin Kielhorn <kielhorn.martin@gmail.com>
2022-05-28 08:43:10 +0200

    start with r code

---
+++ example/87_semiconductor/gen02.lisp
@@ -0,0 +1,37 @@
+(eval-when (:compile-toplevel :execute :load-toplevel)
+  (ql:quickload "cl-r-generator"))
+
+(in-package :cl-r-generator)
+
+(progn
+  ;; the following code needs inverted readtable, otherwise symbols
+  ;; and filenames may have the wrong case and everything breaks in
+  ;; horrible ways
+  (assert (eq :invert
+	      (readtable-case *readtable*)))
+  (defparameter *repo-dir-on-host* "/home/martin/stage/cl-py-generator")
+  (defparameter *repo-dir-on-github* "https://github.com/plops/cl-py-generator/tree/master/")
+  (defparameter *example-subdir* "example/87_semiconductor")
+  (defparameter *path* (format nil "~a/~a" *repo-dir-on-host* *example-subdir*))
+  (let ((show-counter 1))
+    (defun show (name code &key width height)
+      (prog1
+	  `(do0
+	    (png (string ,(format nil "~2,'0d_~a.png" show-counter name))
+		 ,@(when width
+		     `(:width ,width))
+		 ,@(when height
+		     `(:height ,height)))
+	    ,code
+	    (dev.off))
+	(incf show-counter))))
+  (write-source (format nil "~a/source/run02_fit" *path*)
+		`(do0
+		  
+		  (require gamlss)
+		   
+		   )))
+
+
+
+
