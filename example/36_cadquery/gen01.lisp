d8545d234061c5ed210df8a7c5d5b2ee799444b2
Martin Kielhorn <kielhorn.martin@gmail.com>
2022-06-07 19:51:44 +0200

    add a sphere

--- example/36_cadquery/gen01.lisp
+++ example/36_cadquery/gen01.lisp
@@ -3,11 +3,12 @@
   (ql:quickload "alexandria"))
 (in-package :cl-py-generator)
 
+;; try to use Arch module
 
 
 (progn
   (defparameter *path* "/home/martin/stage/cl-py-generator/example/36_cadquery")
-  (defparameter *code-file* "run_00_start")
+  (defparameter *code-file* "run_01_arch")
   (defparameter *source* (format nil "~a/source/~a" *path* *code-file*))
   (defparameter *host*
     "10.1.99.12")
@@ -19,42 +20,45 @@
       "Thursday" "Friday" "Saturday"
       "Sunday"))
 
-     
+
   (let* (
-	 
+
 	 (code
 	  `(do0
 	    (do0 "# %% imports"
-		 
+
 		 (imports (		;os
-			   ;sys
-			   ;time
+					;sys
+					;time
 					;docopt
-			   ;pathlib
-			   ;(np numpy)
-			   ;serial
-			   ;(pd pandas)
-			   ;(xr xarray)
-			   ;(xrp xarray.plot)
+					;pathlib
+					;(np numpy)
+					;serial
+					;(pd pandas)
+					;(xr xarray)
+					;(xrp xarray.plot)
 					;skimage.restoration
 					;(u astropy.units)
 					; EP_SerialIO
-			   ;scipy.ndimage
-			   ;scipy.optimize
+					;scipy.ndimage
+					;scipy.optimize
 					;nfft
-			   ;sklearn
-			   ;sklearn.linear_model
-			   ;itertools
+					;sklearn
+					;sklearn.linear_model
+					;itertools
 					;datetime
 
-			   (cq cadquery)))
-		 "from Helpers import show"
+					;(cq cadquery)
+			   Arch
+			   Part
+			   ))
+					;"from Helpers import show"
 		 (setf
 		  _code_git_version
 		  (string ,(let ((str (with-output-to-string (s)
 					(sb-ext:run-program "/usr/bin/git" (list "rev-parse" "HEAD") :output s))))
 			     (subseq str 0 (1- (length str)))))
-		  _code_repository (string ,(format nil "https://github.com/plops/cl-py-generator/tree/master/example/29_ondrejs_challenge/source/run_00_start.py")
+		  _code_repository (string ,(format nil "https://github.com/plops/cl-py-generator/")
 					   )
 
 		  _code_generation_time
@@ -72,32 +76,17 @@
 				     date
 				     (- tz))
 			     )))
-		 (setf length 80.0
-		       height 60.0
-		       thickness 10.0
-		       center_hole_dia 22.0
-		       cbore_hole_dia 2.4
-		       cbore_dia 4.4
-		       cbore_depth 2.1
-		       r (dot (cq.Workplane (string "XY"))
-			      (box length height thickness)
-			      ;; use selector: maximize in z direction
-			      (faces (string ">Z"))
-			      (workplane)
-			      (hole center_hole_dia)
-			      (faces (string ">Z"))
-			      (rect (- length 8)
-				    (- height 8)
-				    :forConstruction True)
-			      ;; create 4 holes at the corners
-			      (vertices)
-			      (cboreHole
-			       cbore_hole_dia
-			       cbore_dia
-			       cbore_depth)
-			      
-			      ))
-		 
-		 (show r (tuple 204 204 204 0.0))))))
+		 )
+
+	    (do0
+	     (setf doc (dot App (newDocument (string "bed")))))
+	    (do0
+	     (setf sph_ (Part.makeSphere 10))
+	     (setf sph (doc.addObject (string "Part::Feature")
+				      (string "sph"))
+		   sph.Shape sph_)
+	     (doc.recompute)
+	     sph.Volume
+	     sph.Area))))
     (write-source (format nil "~a/source/~a" *path* *code-file*) code)))
 
