523f5032fd937f1cb852f7dd512d3b23ae3e33d4
martin <martin@pop-os.localdomain>
2023-02-16 20:26:13 +0100

    start code to read co2 concentration

---
+++ example/103_co2_sensor/source/p00_co2_concentration.py
@@ -0,0 +1,10 @@
+#|default_exp p00_co2_concentration
+import os
+import time
+import pandas as pd
+import datetime
+start_time=time.time()
+debug=True
+_code_git_version="24de2f63591e142b426e68120a8526009f1da901"
+_code_repository="https://github.com/plops/cl-py-generator/tree/master/example/103_co2_sensor/source/"
+_code_generation_time="20:26:04 of Thursday, 2023-02-16 (GMT+1)"
