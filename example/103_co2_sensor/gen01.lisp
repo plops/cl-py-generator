f1855d727d3b8dd6514c563d500d5aae2ea1de51
Martin Kielhorn <kielhorn.martin@gmail.com>
2023-02-25 13:10:18 +0100

    add ransac

--- example/103_co2_sensor/gen01.lisp
+++ example/103_co2_sensor/gen01.lisp
@@ -20,12 +20,89 @@
 		     *source-dir*))
    `(do0
 
-     (include<> deque)
+     (include<> deque
+		random
+		vector
+		cmath)
 
-     "enum { N_FIFO = 240, };"
-
+     ,@(loop for e in `((N_FIFO 240)
+			    (RANSAC_MAX_ITERATIONS 100)
+			    (RANSAC_INLIER_THRESHOLD 0.1 :type float)
+			    (RANSAC_MIN_INLIERS 50))
+	     collect
+	     (destructuring-bind (name val &key (type 'int)) e
+	       (format nil "const ~a ~a = ~a;" type name val)))
+     
      "std::deque<unsigned short> fifo(N_FIFO,0);"
 
+
+     (defstruct0 Point2D
+       (x double)
+       (y double))
+
+     (defun distance (p m b)
+       (declare (type Point2D p)
+		(type double m b)
+		(values double))
+       (return (/ (abs (- p.y
+			  (+ (* m p.x)
+			     b)))
+		  (sqrt (+ 1 (* m m))))))
+
+
+     (defun ransac_line_fit (data m b)
+       (declare (type "std::deque<Point2D>&" data)
+		(type double& m b))
+       (let ((rd (std--random_device))
+	     (gen (std--mt19937 (rd)))
+	     (distrib (std--uniform_int_distribution<> 0 (- (data.size)
+							    1)))
+	     (best_inliers (std--vector<Point2D>))
+	     (best_m 0d0)
+	     (best_b 0d0))
+	 (dotimes (i RANSAC_MAX_ITERATIONS)
+	   (let ((idx1 (distrib gen))
+		 (idx2 (distrib gen))
+		 (p1 (aref data idx1))
+		 (p2 (aref data idx2))
+		 (m (/ (- p2.y p1.y)
+		       (- p2.x p1.x)))
+		 (b (- p1.y
+		       (* m p1.x)))
+		 (inliers (std--vector<Point2D>)))
+	     (foreach (p data)
+		      (when (< (distance p m b)
+			       RANSAC_INLIER_THRESHOLD)
+			(inliers.push_back p)))
+	     (when (< RANSAC_MIN_INLIERS
+		      (inliers.size))
+	       (let ((sum_x 0d0)
+		     (sum_y 0d0))
+		 (foreach (p inliers)
+			  (incf sum_x p.x)
+			  (incf sum_y p.y))
+		 (let ((avg_x (/ sum_x (inliers.size)))
+		       (avg_y (/ sum_y (inliers.size)))
+		       (var_x 0d0)
+		       (cov_xy 0d0))
+		   (foreach (p inliers)
+			    (incf var_x (* (- p.x avg_x)
+					   (- p.x avg_x)))
+
+			    (incf cov_xy (* (- p.x avg_x)
+					    (- p.y avg_y))))
+		   (let ((m (/ cov_xy var_x))
+			 (b (- avg_y (* m avg_x))))
+		     (when (< (best_inliers.size)
+			      (inliers.size))
+		       (setf best_inliers inliers
+			     best_m m
+			     best_b b))))))
+	     ))
+	 (setf m best_m
+	       b best_b)
+	 ))
+     
      (space
       "extern \"C\" "
       (progn
@@ -76,9 +153,9 @@
 	  (when (uart_is_driver_installed CO2_UART)
 	    (return))
 	  (uart_set_pin CO2_UART
-			  4 5 
-			  UART_PIN_NO_CHANGE
-			  UART_PIN_NO_CHANGE)
+			4 5 
+			UART_PIN_NO_CHANGE
+			UART_PIN_NO_CHANGE)
 	  (uart_driver_install CO2_UART (* BUF_SIZE 2)
 			       0 0 nullptr 0)
 	  (let (
@@ -96,24 +173,24 @@
 
 	
 	(defun measureCO2 ()
-	   (progn
-	     ,(let ((l `(#xff #x01 #x86 0 0 0 0 0 #x79)))
-		`(let ((command (curly ,@(loop for e in l
-					       collect
-					       `(hex ,e))))
-		       (response))
-		   (declare (type (array "unsigned char" ,(length l)) command response))
-		   (uart_write_bytes CO2_UART command ,(length l))
-		   (let ((l (uart_read_bytes CO2_UART response ,(length l)
-					     100)))
-		     (when (== 9 l)
-		       (when (logand (== #xff (aref response 0))
-				     (== #x86 (aref response 1)))
-			 (let ((co2 (+ (* 256 (aref response 2))
-				       (aref response 3))))
-			   (when (< N_FIFO (fifo.size))
-			     (fifo.pop_back))
-			   (fifo.push_front co2)))))))))
+	  (progn
+	    ,(let ((l `(#xff #x01 #x86 0 0 0 0 0 #x79)))
+	       `(let ((command (curly ,@(loop for e in l
+					      collect
+					      `(hex ,e))))
+		      (response))
+		  (declare (type (array "unsigned char" ,(length l)) command response))
+		  (uart_write_bytes CO2_UART command ,(length l))
+		  (let ((l (uart_read_bytes CO2_UART response ,(length l)
+					    100)))
+		    (when (== 9 l)
+		      (when (logand (== #xff (aref response 0))
+				    (== #x86 (aref response 1)))
+			(let ((co2 (+ (* 256 (aref response 2))
+				      (aref response 3))))
+			  (when (< N_FIFO (fifo.size))
+			    (fifo.pop_back))
+			  (fifo.push_front co2)))))))))
 
 	(defun drawCO2 (buf )
 	  (declare (type "pax_buf_t*" buf))
@@ -122,12 +199,12 @@
 		(bright 255)
 		(col (pax_col_hsv hue
 				  sat bright)))
-	   (dotimes (i (- (fifo.size) 1)
-		       )
-	     (pax_draw_line buf col i
-			    (aref fifo i)
-			    (+ i 1)
-			    (aref fifo (+ i 1)))))
+	    (dotimes (i (- (fifo.size) 1)
+			)
+	      (pax_draw_line buf col i
+			     (aref fifo i)
+			     (+ i 1)
+			     (aref fifo (+ i 1)))))
 	  )
 	
 	(defun app_main ()
--- example/103_co2_sensor/source01/main.cpp
+++ example/103_co2_sensor/source01/main.cpp
@@ -1,8 +1,69 @@
+#include <cmath>
 #include <deque>
-enum {
-  N_FIFO = 240,
-};
+#include <random>
+#include <vector>
+const int N_FIFO = 240;
+const int RANSAC_MAX_ITERATIONS = 100;
+const float RANSAC_INLIER_THRESHOLD = 0.1;
+const int RANSAC_MIN_INLIERS = 50;
 std::deque<unsigned short> fifo(N_FIFO, 0);
+struct Point2D {
+  double x;
+  double y;
+};
+typedef struct Point2D Point2D;
+
+double distance(Point2D p, double m, double b) {
+  return ((abs(((p.y) - (((m * p.x) + b))))) / (sqrt((1 + (m * m)))));
+}
+
+void ransac_line_fit(std::deque<Point2D> &data, double &m, double &b) {
+  auto rd = std::random_device();
+  auto gen = std::mt19937(rd());
+  auto distrib = std::uniform_int_distribution<>(0, ((data.size()) - (1)));
+  auto best_inliers = std::vector<Point2D>();
+  auto best_m = (0.);
+  auto best_b = (0.);
+  for (auto i = 0; i < RANSAC_MAX_ITERATIONS; i += 1) {
+    auto idx1 = distrib(gen);
+    auto idx2 = distrib(gen);
+    auto p1 = data[idx1];
+    auto p2 = data[idx2];
+    auto m = ((((p2.y) - (p1.y))) / (((p2.x) - (p1.x))));
+    auto b = ((p1.y) - ((m * p1.x)));
+    auto inliers = std::vector<Point2D>();
+    for (auto &p : data) {
+      if (distance(p, m, b) < RANSAC_INLIER_THRESHOLD) {
+        inliers.push_back(p);
+      }
+    };
+    if (RANSAC_MIN_INLIERS < inliers.size()) {
+      auto sum_x = (0.);
+      auto sum_y = (0.);
+      for (auto &p : inliers) {
+        sum_x += p.x;
+        sum_y += p.y;
+      };
+      auto avg_x = ((sum_x) / (inliers.size()));
+      auto avg_y = ((sum_y) / (inliers.size()));
+      auto var_x = (0.);
+      auto cov_xy = (0.);
+      for (auto &p : inliers) {
+        var_x += (((p.x) - (avg_x)) * ((p.x) - (avg_x)));
+        cov_xy += (((p.x) - (avg_x)) * ((p.y) - (avg_y)));
+      };
+      auto m = ((cov_xy) / (var_x));
+      auto b = ((avg_y) - ((m * avg_x)));
+      if (best_inliers.size() < inliers.size()) {
+        best_inliers = inliers;
+        best_m = m;
+        best_b = b;
+      }
+    }
+  }
+  m = best_m;
+  b = best_b;
+}
 extern "C" {
 #include "driver/uart.h"
 #include "esp_system.h"
