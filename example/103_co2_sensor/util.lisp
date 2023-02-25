14b177ac8b571a01ebfaa44f7acd493bc55ddc29
martin <martin@pop-os.localdomain>
2023-02-25 18:15:35 +0100

    value is always zero

---
+++ example/103_co2_sensor/util.lisp
@@ -0,0 +1,15 @@
+(defun lprint (&key (msg "")
+		
+		 (vars nil)
+		
+		    )
+  `(fmt--print
+    (string ,(format nil "~a~{ ~a~}\\n"
+		     msg
+		     (loop for e in vars
+			   collect
+			   (format nil " ~a='{}'" (emit-c :code e)))
+		     ))
+    ,@vars))
+
+
