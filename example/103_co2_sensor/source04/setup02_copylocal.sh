44eec95ead8019092a295973f074ae1b7c3ea3b2
Martin Kielhorn <martin.kielhorn@ideapad>
2023-04-16 15:18:36 +0200

    more code

--- example/103_co2_sensor/source04/setup02_copylocal.sh
+++ example/103_co2_sensor/source04/setup02_copylocal.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+cp -r \
+   /home/martin/stage/cl-py-generator/example/103_co2_sensor/source04/proto \
+   /home/martin/src/my_fancy_app_name/main
+./setup03_proto.sh
+
+cp \
+    /home/martin/stage/cl-py-generator/example/103_co2_sensor/source04/gen/*.{c,h} \
+    /home/martin/src/my_fancy_app_name/main
 cp \
  /home/martin/stage/cl-py-generator/example/103_co2_sensor/source04/{CMake*,*.{c,cpp,h,hpp}} \
  /home/martin/src/my_fancy_app_name/main
