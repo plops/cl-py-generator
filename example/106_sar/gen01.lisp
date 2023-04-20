fe3342f0a6244db71e585d67179e4c782a7c67e7
Martin Kielhorn <martin.kielhorn@ideapad>
2023-04-20 22:17:24 +0200

    more code

--- example/106_sar/gen01.lisp
+++ example/106_sar/gen01.lisp
@@ -5,8 +5,8 @@
 (in-package :cl-py-generator)
 
 (progn
-  (defparameter *project* "105_amd_opencv")
-  (defparameter *idx* "04")
+  (defparameter *project* "106_sar")
+  (defparameter *idx* "01")
   (defparameter *path* (format nil "/home/martin/stage/cl-py-generator/example/~a" *project*))
   (defparameter *day-names*
     '("Monday" "Tuesday" "Wednesday"
@@ -23,27 +23,34 @@
                             ,@vars)))))
 
   
+  (defun doc (def)
+    `(do0
+      ,@(loop for e in def
+	      collect
+	      (destructuring-bind (&key name val (unit "-") (help name)) e
+		`(do0
+		  (comments ,(format nil "~a (~a)" help unit))
+		  (setf ,name ,val))))))
   
-  (let* ((notebook-name "opencv_mediapipe_face")
+  (let* ((notebook-name "sar_simulator")
 	 #+nil (cli-args `(
-		     (:short "-v" :long "--verbose" :help "enable verbose output" :action "store_true" :required nil))))
+			   (:short "-v" :long "--verbose" :help "enable verbose output" :action "store_true" :required nil))))
     (write-source
      (format nil "~a/source/p~a_~a" *path* *idx* notebook-name)
      `(do0
        "#!/usr/bin/python"
        (do0
-	,(format nil "#|default_exp p~a_~a" *idx* notebook-name))
-	 (do0
-	  (comment "sudo pacman -S python-opencv rocm-opencl-runtime python-mss"
-		   "python3 -m pip install --user mediapipe")
-	  (imports (;	os
+	)
+       (do0
+	
+	(imports (			;	os
 					;sys
-			time
+		  time
 					;docopt
-			;pathlib
+					;pathlib
 					;(np numpy)
-			;serial
-			;(pd pandas)
+					;serial
+					;(pd pandas)
 					;(xr xarray)
 					;(xrp xarray.plot)
 					;skimage.restoration
@@ -56,8 +63,8 @@
 					;sklearn.linear_model
 					;itertools
 					;datetime
-					 (np numpy)
-					(cv cv2)
+		  (np numpy)
+					;(cv cv2)
 					;(mp mediapipe)
 					;jax
 					; jax.random
@@ -69,136 +76,116 @@
 					;io.StringIO
 					;bs4
 					;requests
-					mss
+					;mss
 			
 					;(np jax.numpy)
 					;(mpf mplfinance)
 
 					;argparse
 					;torch
-					(mp mediapipe)
+					;(mp mediapipe)
 					
-			)))
+		  )))
 	 
-	 (setf start_time (time.time)
-	       debug True)
-	 (setf
-	  _code_git_version
-	  (string ,(let ((str (with-output-to-string (s)
-				(sb-ext:run-program "/usr/bin/git" (list "rev-parse" "HEAD") :output s))))
-		     (subseq str 0 (1- (length str)))))
-	  _code_repository (string ,(format nil
-					    "https://github.com/plops/cl-py-generator/tree/master/example/~a/source/"
-					    *project*))
-	  _code_generation_time
-	  (string ,(multiple-value-bind
-			 (second minute hour date month year day-of-week dst-p tz)
-		       (get-decoded-time)
-		     (declare (ignorable dst-p))
-		     (format nil "~2,'0d:~2,'0d:~2,'0d of ~a, ~d-~2,'0d-~2,'0d (GMT~@d)"
-			     hour
-			     minute
-			     second
-			     (nth day-of-week *day-names*)
-			     year
-			     month
-			     date
-			     (- tz)))))
+       (setf start_time (time.time)
+	     debug True)
+       (setf
+	_code_git_version
+	(string ,(let ((str (with-output-to-string (s)
+			      (sb-ext:run-program "/usr/bin/git" (list "rev-parse" "HEAD") :output s))))
+		   (subseq str 0 (1- (length str)))))
+	_code_repository (string ,(format nil
+					  "https://github.com/plops/cl-py-generator/tree/master/example/~a/source/"
+					  *project*))
+	_code_generation_time
+	(string ,(multiple-value-bind
+		       (second minute hour date month year day-of-week dst-p tz)
+		     (get-decoded-time)
+		   (declare (ignorable dst-p))
+		   (format nil "~2,'0d:~2,'0d:~2,'0d of ~a, ~d-~2,'0d-~2,'0d (GMT~@d)"
+			   hour
+			   minute
+			   second
+			   (nth day-of-week *day-names*)
+			   year
+			   month
+			   date
+			   (- tz)))))
 
 
-	 
-	 (do0
-	  ,(lprint :vars `((cv.ocl.haveOpenCL)))
-	  (setf loop_time (time.time))
-	  (setf mp_drawing mp.solutions.drawing_utils
-		mp_drawing_styles mp.solutions.drawing_styles
-		mp_face_mesh mp.solutions.face_mesh
-		)
-	  (setf clahe (cv.createCLAHE :clipLimit 15.0
-				      :tileGridSize (tuple 32 18)))
-	  (setf drawing_spec (mp_drawing.DrawingSpec :thickness 1
-						     :circle_radius 1))
+       (do0
+	  
+	,(doc `((:name c :val 3e8 :unit m/s :help "speed of light" )
+		(:name fc :val 5.5e9  :unit Hz :help "center frequency")
+		(:name wavelength :val (/ c fc) :unit m)
+		(:name platform_speed :val 100 :unit m/s )
+		(:name altitude :val 5000 :unit m :help "platform altitude")
+		(:name duration :val 10 :unit s :help "simulation duration")
+		(:name sample_rate :val 1000 :unit Hz)
+		(:name bw :val 50e6 :unit Hz :help "bandwidth")
+		(:name T_p :val 10e-6 :unit s :help "pulse duration")
+		(:name K :val (/ bw T_p) :unit Hz/s :help "chirp rate")
+		(:name scatter_points  :val (np.array (list (list 3000 5000)
+							    (list 5000 8000)
+							    (list 8000 15000)))
+		 :unit m :help "scattering targets on the ground plane")
+		(:name tt :val (np.arange 0 duration (/ 1 sample_rate)) :unit s :help "time array")
+		(:name platform_positions :val (* platform_speed tt) :unit m :help "position array")
+		(:name slant_ranges :val (np.sqrt (+ (** altitude 2)
+						     (** (- (aref scatter_points ":" 0)
+							    (aref platform_positions ":" np.newaxis))
+							 2)))
+		 :unit m :help "slant ranges for each scatter point")
+		(:name time_delays :val (* 2 (/ slant_ranges c)) :unit s :help "round-trip time delay")
+		(:name t_chirp :val (np.arange 0 T_p (/ 1 sample_rate)) :unit s :help "time axis for pulse")
+		(:name transmitted_signal :val (np.exp (* 1j np.pi K (** t_chirp 2) ))
+		 :unit amplitude :help "chirped radar pulse amplitude")
+		(:name v_radial :val (/ (* -platform_speed
+					   (- (aref scatter_points ":" 0)
+					      (aref platform_positions ":" np.newaxis)))
+					slant_ranges)
+		       ;; m/s * m / m
+		 :unit m/s
+		 :help "radial speed difference between platform and target")
+		(:name doppler_shifts :val (* 2 (/ v_radial wavelength))
+		       ;; m/s / m
+		 :unit Hz)
+		(:name received_signal :val (np.zeros (tuple (len tt)
+							     (len transmitted_signal))
+						      :dtype complex)
+		 :unit amplitude)
+			 
+					;(:name :val :unit :help)
+		))
 
-	  #+nil (setf gpu_options (mp.GpuAccelerationOptions :enable True
-						       :device_id 0))
-	  (with (as (mp_face_mesh.FaceMesh
-		     :static_image_mode  False
-		     :max_num_faces 1
-		     ;:gpu_options gpu_options
-		     :refine_landmarks True
-		     :min_detection_confidence .15
-		     :min_tracking_confidence .15)
-		    face_mesh)
-		(comments "https://github.com/google/mediapipe/blob/master/docs/solutions/face_mesh.md"
-			  "select the attention model using refine_landmarks option to home improve accuracy around lips, eyes and irises")
-		(with (as (mss.mss)
-			  sct)
-		      (setf loop_start (time.time))
-		      (while True
-			     (do0
-			      
-			      (setf img (np.array (sct.grab (dictionary :top 160
-									:left 0
-									:width (// 1920 2)
-									:height (// 1080 2))))
-				    )
-			      
-			
-			      (setf timestamp_ms (int (* 1000 (- (time.time)
-								 loop_start))))
-			
-			
-			      (setf imgr (cv.cvtColor img 
-						      cv.COLOR_BGR2RGB))
-			      (setf results (face_mesh.process imgr))
-			      (setf lab (cv.cvtColor img 
-						     cv.COLOR_RGB2LAB)
-				    lab_planes (cv.split lab)
-				    lclahe
-				    (clahe.apply (aref lab_planes 0))
-				    lab (cv.merge (list lclahe 
-							(aref lab_planes 1)
-							(aref lab_planes 2)))
-				    imgr (cv.cvtColor lab cv.COLOR_LAB2RGB))
-			      (when results.multi_face_landmarks
-				(for (face_landmarks results.multi_face_landmarks)
-				     ,@(loop for e in
-					     `((:connections FACEMESH_TESSELATION :spec get_default_face_mesh_tesselation_style)
-					       (:connections FACEMESH_CONTOURS :spec get_default_face_mesh_contours_style)
-					       (:connections FACEMESH_IRISES :spec get_default_face_mesh_iris_connections_style)
-					       )
-					     collect
-					     (destructuring-bind (&key connections spec) e
-						
-					       `(mp_drawing.draw_landmarks
-						 :image imgr
-						 :landmark_list face_landmarks
-						 :landmark_drawing_spec None
-						 :connections (dot mp_face_mesh ,connections)
-						 :connection_drawing_spec
-						 (dot 
-						  mp_drawing_styles (,spec)))))
-				     ))
-			      (cv.imshow (string "screen")
-					 imgr) 
-			
-			
-			      (do0
-			       (setf delta (- (time.time)
-					      loop_time))
-			       (setf target_period (- (/ 1 60.0)
-						      .0001))
-			       (when (< delta target_period)
-				 (time.sleep (- target_period delta)
-					     ))
-			       (setf fps (/ 1 delta)
-				     fps_wait (/ 1 (- (time.time)
-						      loop_time)))
-			       (setf loop_time (time.time))
-			       (when (== 0 (% timestamp_ms 2000))
-				 ,(lprint :vars `(fps fps_wait))))
-			      (when (== (ord (string "q"))
-					(cv.waitKey 1))
-				(cv.destroyAllWindows)
-				break))))))))))
+	(for (scatter_idx (range (dot scatter_points
+				      (aref shape 0))))
+	     (setf delay_samples (dot np (round (* (aref time_delays ":" scatter_idx)
+						   sample_rate))
+				      (astype int)))
+	     (for ((ntuple idx delay)
+		   (enumerate delay_samples))
+		  (when (and (<= 0 delay )
+			     (< delay (len transmitted_signal)))
+		    (incf (aref received_signal idx)
+			  (* (np.exp (* 1j 2 np.pi
+					(aref doppler_shifts idx scatter_idx)
+					t_chirp))
+			     (aref transmitted_signal np.newaxis
+				   (- (slice 0 (len transmitted_signal))
+				      delay)))))))
+
+	,(doc
+	  `(;(:name :val :unit :help)
+	    (:name noise_level :val 1e-5 :unit amplitude )
+	    (:name received_signal :val (+ received_signal
+					   (* noise_level
+					      (+ (np.random.randn *received_signal.shape)
+						 (* 1j (np.random.randn *received_signal.shape))))))
+	    )
+	  )
+	  
+
+	  
+	)))))
 
---
+++ example/106_sar/source/p01_sar_simulator.py
@@ -0,0 +1,57 @@
+#!/usr/bin/python
+import time
+import numpy as np
+start_time=time.time()
+debug=True
+_code_git_version="38d874c73920c43822c427e6cb2bdc6ce104c3a3"
+_code_repository="https://github.com/plops/cl-py-generator/tree/master/example/106_sar/source/"
+_code_generation_time="22:17:13 of Thursday, 2023-04-20 (GMT+1)"
+# speed of light (m/s)
+c=(3.0e+8)
+# center frequency (Hz)
+fc=(5.5e+9)
+# wavelength (m)
+wavelength=((c)/(fc))
+# platform_speed (m/s)
+platform_speed=100
+# platform altitude (m)
+altitude=5000
+# simulation duration (s)
+duration=10
+# sample_rate (Hz)
+sample_rate=1000
+# bandwidth (Hz)
+bw=(5.0e+7)
+# pulse duration (s)
+T_p=(1.00e-5)
+# chirp rate (Hz/s)
+K=((bw)/(T_p))
+# scattering targets on the ground plane (m)
+scatter_points=np.array([[3000, 5000], [5000, 8000], [8000, 15000]])
+# time array (s)
+tt=np.arange(0, duration, ((1)/(sample_rate)))
+# position array (m)
+platform_positions=((platform_speed)*(tt))
+# slant ranges for each scatter point (m)
+slant_ranges=np.sqrt(((((altitude)**(2)))+(((((scatter_points[:,0])-(platform_positions[:,np.newaxis])))**(2)))))
+# round-trip time delay (s)
+time_delays=((2)*(((slant_ranges)/(c))))
+# time axis for pulse (s)
+t_chirp=np.arange(0, T_p, ((1)/(sample_rate)))
+# chirped radar pulse amplitude (amplitude)
+transmitted_signal=np.exp(((1j)*(np.pi)*(K)*(((t_chirp)**(2)))))
+# radial speed difference between platform and target (m/s)
+v_radial=((((-platform_speed)*(((scatter_points[:,0])-(platform_positions[:,np.newaxis])))))/(slant_ranges))
+# doppler_shifts (Hz)
+doppler_shifts=((2)*(((v_radial)/(wavelength))))
+# received_signal (amplitude)
+received_signal=np.zeros((len(tt),len(transmitted_signal),), dtype=complex)
+for scatter_idx in range(scatter_points.shape[0]):
+    delay_samples=np.round(((time_delays[:,scatter_idx])*(sample_rate))).astype(int)
+    for idx, delay in enumerate(delay_samples):
+        if ( ((((0)<=(delay))) and (((delay)<(len(transmitted_signal))))) ):
+            received_signal[idx] += ((np.exp(((1j)*(2)*(np.pi)*(doppler_shifts[idx,scatter_idx])*(t_chirp))))*(transmitted_signal[np.newaxis,((0:len(transmitted_signal))-(delay))]))
+# noise_level (amplitude)
+noise_level=(1.00e-5)
+# received_signal (-)
+received_signal=((received_signal)+(((noise_level)*(((np.random.randn(*received_signal.shape))+(((1j)*(np.random.randn(*received_signal.shape)))))))))
