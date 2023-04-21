ee451503b5ef4421baf7c96cad246fa22e09f1c2
Martin Kielhorn <martin.kielhorn@ideapad>
2023-04-22 00:01:57 +0200

    don't print so much

--- example/103_co2_sensor/gen05.lisp
+++ example/103_co2_sensor/gen05.lisp
@@ -121,29 +121,30 @@
 		  (while True
 			 (setf (ntuple conn addr) (s.accept))
 			 (with conn
-			       ,(lprint :msg "connection" :vars `(addr))
-			       ,(lprint :msg "wait for DataResponse message")
+			       ;,(lprint :msg "connection" :vars `(addr))
+			       ;,(lprint :msg "wait for DataResponse message")
 			       (setf data (conn.recv 1024))
 			       (setf buf data)
 			       
 			       (while data
 				      (setf data (conn.recv 1024))
 				      (incf buf data))
-			       ,(lprint :msg "finished reading" :vars `(buf))
+			       ;,(lprint :msg "finished reading" :vars `(buf))
 			       (setf imsg (DataResponse))
 			       
-			       ,(lprint :vars `((dot imsg
+			       #+nil,(lprint :vars `((dot imsg
 				    (ParseFromString buf))))
 			       ,(lprint :vars `(,@(loop for e in `(index datetime pressure humidity temperature
 									 co2_concentration)
 							collect
 							`(dot imsg ,e))))
-			       ,(lprint :msg "send DataRequest message")
+			       ;,(lprint :msg "send DataRequest message")
 			       (setf omsg (dot (DataRequest :start_index 123
 							    :count 42) 
 					       (SerializeToString)))
 			       (conn.sendall omsg)
-			       ,(lprint :msg "connection closed"))))
+			       ;,(lprint :msg "connection closed")
+			       )))
 	    )
 	  (listen)
 	  
--- example/103_co2_sensor/source04/p05_pb_server.py
+++ example/103_co2_sensor/source04/p05_pb_server.py
@@ -7,9 +7,9 @@ import struct
 from data_pb2 import Packet, DataRequest, DataResponse
 start_time=time.time()
 debug=True
-_code_git_version="c1e8efb198d5d8ebaebe797f0a9075035a971764"
+_code_git_version="17ae302b260d342c0bf28a020136eac321b9b851"
 _code_repository="https://github.com/plops/cl-py-generator/tree/master/example/103_co2_sensor/source/"
-_code_generation_time="23:58:47 of Friday, 2023-04-21 (GMT+1)"
+_code_generation_time="00:01:35 of Saturday, 2023-04-22 (GMT+1)"
 def listen():
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         s.bind(("192.168.2.122",12345,))
@@ -18,19 +18,13 @@ def listen():
         while (True):
             conn, addr=s.accept()
             with conn:
-                print("{} connection addr={}".format(((time.time())-(start_time)), addr))
-                print("{} wait for DataResponse message ".format(((time.time())-(start_time))))
                 data=conn.recv(1024)
                 buf=data
                 while (data):
                     data=conn.recv(1024)
                     buf += data
-                print("{} finished reading buf={}".format(((time.time())-(start_time)), buf))
                 imsg=DataResponse()
-                print("{} nil imsg.ParseFromString(buf)={}".format(((time.time())-(start_time)), imsg.ParseFromString(buf)))
                 print("{} nil imsg.index={} imsg.datetime={} imsg.pressure={} imsg.humidity={} imsg.temperature={} imsg.co2_concentration={}".format(((time.time())-(start_time)), imsg.index, imsg.datetime, imsg.pressure, imsg.humidity, imsg.temperature, imsg.co2_concentration))
-                print("{} send DataRequest message ".format(((time.time())-(start_time))))
                 omsg=DataRequest(start_index=123, count=42).SerializeToString()
                 conn.sendall(omsg)
-                print("{} connection closed ".format(((time.time())-(start_time))))
 listen()
