e85a3ffc8f7fd95775a0927fb894d8f6750fdee4
Martin Kielhorn <martin.kielhorn@ideapad>
2023-04-30 00:08:14 +0200

    more code

---
+++ example/107_ofdm/gen01.lisp
@@ -0,0 +1,120 @@
+(eval-when (:compile-toplevel :execute :load-toplevel)
+  (ql:quickload "cl-py-generator")
+  (ql:quickload "cl-change-case"))
+
+(in-package :cl-py-generator)
+
+(progn
+  (defparameter *project* "107_ofdm")
+  (defparameter *idx* "01")
+  (defparameter *path* (format nil "/home/martin/stage/cl-py-generator/example/~a" *project*))
+  (defparameter *day-names*
+    '("Monday" "Tuesday" "Wednesday"
+      "Thursday" "Friday" "Saturday"
+      "Sunday"))
+  (defun lprint (&key msg vars)
+    `(do0 ;when args.verbose
+       (print (dot (string ,(format nil "{} ~a ~{~a={}~^ ~}"
+				    msg
+				    (mapcar (lambda (x)
+					      (emit-py :code x))
+					    vars)))
+                   (format  (- (time.time) start_time)
+                            ,@vars)))))
+
+  
+  (defun doc (def)
+    `(do0
+      ,@(loop for e in def
+	      collect
+	      (destructuring-bind (&key name val (unit "-") (help name)) e
+		`(do0
+		  (comments ,(format nil "~a (~a)" help unit))
+		  (setf ,name ,val))))))
+  
+  (let* ((notebook-name "ofdm")
+	 #+nil (cli-args `(
+			   (:short "-v" :long "--verbose" :help "enable verbose output" :action "store_true" :required nil))))
+    (write-source
+     (format nil "~a/source/p~a_~a" *path* *idx* notebook-name)
+     `(do0
+       "#!/usr/bin/python"
+       (do0
+	)
+       (do0
+	
+	(imports (			;	os
+					;sys
+		  time
+					;docopt
+					;pathlib
+					;(np numpy)
+					;serial
+					;(pd pandas)
+					;(xr xarray)
+					;(xrp xarray.plot)
+					;skimage.restoration
+					;(u astropy.units)
+					; EP_SerialIO
+					;scipy.ndimage
+					;   scipy.optimize
+					;nfft
+					;sklearn
+					;sklearn.linear_model
+					;itertools
+					;datetime
+		  (np numpy)
+					;(cv cv2)
+					;(mp mediapipe)
+					;jax
+					; jax.random
+					;jax.config
+					; copy
+					;re
+					;json
+					; csv
+					;io.StringIO
+					;bs4
+					;requests
+					;mss
+			
+					;(np jax.numpy)
+					;(mpf mplfinance)
+
+					;argparse
+					;torch
+					;(mp mediapipe)
+					
+		  )))
+	 
+       (setf start_time (time.time)
+	     debug True)
+       (setf
+	_code_git_version
+	(string ,(let ((str (with-output-to-string (s)
+			      (sb-ext:run-program "/usr/bin/git" (list "rev-parse" "HEAD") :output s))))
+		   (subseq str 0 (1- (length str)))))
+	_code_repository (string ,(format nil
+					  "https://github.com/plops/cl-py-generator/tree/master/example/~a/source/"
+					  *project*))
+	_code_generation_time
+	(string ,(multiple-value-bind
+		       (second minute hour date month year day-of-week dst-p tz)
+		     (get-decoded-time)
+		   (declare (ignorable dst-p))
+		   (format nil "~2,'0d:~2,'0d:~2,'0d of ~a, ~d-~2,'0d-~2,'0d (GMT~@d)"
+			   hour
+			   minute
+			   second
+			   (nth day-of-week *day-names*)
+			   year
+			   month
+			   date
+			   (- tz)))))
+
+
+       (do0
+
+	  
+	)))))
+
---
+++ example/107_ofdm/source/p01_ofdm.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python
+import time
+import numpy as np
+start_time=time.time()
+debug=True
+_code_git_version="1b8c59c57edc63ff117ecea472995680c25b1c47"
+_code_repository="https://github.com/plops/cl-py-generator/tree/master/example/107_ofdm/source/"
+_code_generation_time="00:07:59 of Sunday, 2023-04-30 (GMT+1)"
