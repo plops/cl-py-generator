3ef85648055643329f15e1b68adac44cfe742980
Martin Kielhorn <wolpumba@gmail.com>
2023-09-10 06:51:51 +0200

    langchain

---
+++ example/117_langchain_azure_openai/README.md
@@ -0,0 +1,2 @@
+
+- https://www.youtube.com/watch?v=jfJbaJHnnP0 LangChain for beginners | full code
---
+++ example/117_langchain_azure_openai/gen00.lisp
@@ -0,0 +1,106 @@
+(eval-when (:compile-toplevel :execute :load-toplevel)
+  (ql:quickload "cl-py-generator"))
+
+(in-package :cl-py-generator)
+
+(progn
+  (defparameter *project* "117_langchain_azure_openai")
+  (defparameter *idx* "00")
+  (defparameter *path* (format nil "/home/martin/stage/cl-py-generator/example/~a" *project*))
+  (defparameter *day-names*
+    '("Monday" "Tuesday" "Wednesday"
+      "Thursday" "Friday" "Saturday"
+      "Sunday"))
+  (defun lprint (cmd &optional rest)
+    `(do0 ;when args.verbose
+       (print (dot (string ,(format nil "{} ~a ~{~a={}~^ ~}"
+				    cmd
+				    (mapcar (lambda (x)
+					      (emit-py :code x))
+					    rest)))
+                   (format  (- (time.time) start_time)
+                            ,@rest)))))
+
+  
+  
+  (let* ((notebook-name "use_gpt")
+	 #+nil (cli-args `(
+		     (:short "-v" :long "--verbose" :help "enable verbose output" :action "store_true" :required nil))))
+    (write-source
+     (format nil "~a/source/p~a_~a" *path* *idx* notebook-name)
+     `(do0
+       (do0
+	,(format nil "#|default_exp p~a_~a" *idx* notebook-name))
+       (comments "python -m venv ~/llm_env; . ~/llm_env/bin/activate; pip install langchain"
+		 ""
+		 "deactivate")
+       (do0
+	  
+	(imports (	os
+					;sys
+			time
+					;docopt
+					;pathlib
+					;(np numpy)
+					;serial
+			;(pd pandas)
+					;(xr xarray)
+					;(xrp xarray.plot)
+					;skimage.restoration
+					;(u astropy.units)
+					; EP_SerialIO
+					;scipy.ndimage
+					;   scipy.optimize
+					;nfft
+					;sklearn
+					;sklearn.linear_model
+					;itertools
+			datetime
+					; (np numpy)
+					;(cv cv2)
+					;(mp mediapipe)
+					;jax
+					; jax.random
+					;jax.config
+					; copy
+					;re
+					;json
+					; csv
+					;io.StringIO
+					;bs4
+					;requests
+			
+					;(np jax.numpy)
+					;(mpf mplfinance)
+
+					;argparse
+			langchain
+			)))
+	 
+	 (setf start_time (time.time)
+	       debug True)
+	 (setf
+	  _code_git_version
+	  (string ,(let ((str (with-output-to-string (s)
+				(sb-ext:run-program "/usr/bin/git" (list "rev-parse" "HEAD") :output s))))
+		     (subseq str 0 (1- (length str)))))
+	  _code_repository (string ,(format nil
+					    "https://github.com/plops/cl-py-generator/tree/master/example/~a/source/"
+					    *project*))
+	  _code_generation_time
+	  (string ,(multiple-value-bind
+			 (second minute hour date month year day-of-week dst-p tz)
+		       (get-decoded-time)
+		     (declare (ignorable dst-p))
+		     (format nil "~2,'0d:~2,'0d:~2,'0d of ~a, ~d-~2,'0d-~2,'0d (GMT~@d)"
+			     hour
+			     minute
+			     second
+			     (nth day-of-week *day-names*)
+			     year
+			     month
+			     date
+			     (- tz)))))
+
+	))))
+
