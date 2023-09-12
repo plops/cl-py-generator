0f01f1a97a48e0fba3ec112c52890b512d6187c1
Martin Kielhorn <wolpumba@gmail.com>
2023-09-12 21:23:36 +0200

    more

---
+++ example/117_langchain_azure_openai/gen02.lisp
@@ -0,0 +1,222 @@
+(eval-when (:compile-toplevel :execute :load-toplevel)
+  (ql:quickload "cl-py-generator"))
+
+(in-package :cl-py-generator)
+
+(progn
+  (defparameter *project* "117_langchain_azure_openai")
+  (defparameter *idx* "02")
+  (defparameter *path* (format nil "/home/martin/stage/cl-py-generator/example/~a" *project*))
+  (defparameter *day-names*
+    '("Monday" "Tuesday" "Wednesday"
+      "Thursday" "Friday" "Saturday"
+      "Sunday"))
+  (defun lprint (cmd &optional rest)
+    `(do0 ;when args.verbose
+       (print (dot (string ,(format nil "{} ~a ~{~a={}~^ ~}"
+				    cmd
+				    (mapcar (lambda (x)
+					      (emit-py :code x))
+					    rest)))
+                   (format  (- (time.time) start_time)
+                            ,@rest)))))
+
+  
+  
+  (let* ((notebook-name "use_qdrant")
+	 #+nil (cli-args `(
+		     (:short "-v" :long "--verbose" :help "enable verbose output" :action "store_true" :required nil))))
+     (write-source
+     (format nil "~a/source/p~a_~a" *path* *idx* notebook-name)
+     `(do0
+       (do0
+	,(format nil "#|default_exp p~a_~a" *idx* notebook-name))
+       
+       (comments "python -m venv ~/llm_env; . ~/llm_env/bin/activate; source ~/llm_environment.sh;"
+		 "pip install qdrant-client langchain[llms] openai sentence-transformers"
+		 "deactivate")
+       (do0
+	  
+	(imports (	os
+					;sys
+			time
+					;docopt
+					;pathlib
+			(np numpy)
+					;serial
+					;(pd pandas)
+					;(xr xarray)
+					;(xrp xarray.plot)
+					;skimage.restoration
+					;(u astropy.units)
+					; EP_SerialIO
+					;scipy.ndimage
+					;   scipy.optimize
+					;nfft
+					;sklearn
+					;sklearn.linear_model
+					;itertools
+					;datetime
+					; (np numpy)
+					;(cv cv2)
+					;(mp mediapipe)
+					;jax
+					; jax.random
+					;jax.config
+					; copy
+					;re
+					;json
+					; csv
+					;io.StringIO
+					;bs4
+					;requests
+			
+					;(np jax.numpy)
+					;(mpf mplfinance)
+
+					;argparse
+					;langchain
+					;langchain.chat_models
+					; langchain.schema
+			; langchain.llms
+			openai
+			))
+	(imports-from (langchain.vectorstores Qdrant)
+		      (langchain.embeddings HuggingFaceEmbeddings)
+		      (langchain.text_splitter RecursiveCharacterTextSplitter)
+		      (qdrant_client QdrantClient)
+		      (qdrant_client.http models)
+		      (qdrant_client.http.models CollectionStatus)
+		      (qdrant_client.models PointStruct Distance VectorParams)
+		      (sentence_transformers SentenceTransformer)
+		      (tqdm ;.notebook
+		       tqdm)))
+	  
+	 (setf start_time (time.time)
+	       debug True)
+	 (setf
+	  _code_git_version
+	  (string ,(let ((str (with-output-to-string (s) 
+				(sb-ext:run-program "/usr/bin/git" (list "rev-parse" "HEAD") :output s))))
+		     (subseq str 0 (1- (length str)))))
+	  _code_repository (string ,(format nil
+					    "https://github.com/plops/cl-py-generator/tree/master/example/~a/source/"
+					    *project*))
+	  _code_generation_time
+	  (string ,(multiple-value-bind
+			 (second minute hour date month year day-of-week dst-p tz)
+		       (get-decoded-time)
+		     (declare (ignorable dst-p))
+		     (format nil "~2,'0d:~2,'0d:~2,'0d of ~a, ~d-~2,'0d-~2,'0d (GMT~@d)"
+			     hour
+			     minute
+			     second
+			     (nth day-of-week *day-names*)
+			     year
+			     month
+			     date
+			     (- tz)))))
+
+	 
+	 (setf chatgpt_deployment_name (string "gpt-35")
+	       chatgpt_model_name (string "gpt-35-turbo")
+	       openai.api_type (string "azure")
+	       openai.api_key (os.getenv (string "OPENAI_API_KEY"))
+	       openai.api_base (os.getenv (string "OPENAI_API_BASE"))
+	       openai.api_version (os.getenv (string "OPENAI_API_VERSION")))
+
+	 (comments "cd ~/src; git clone --depth 1 https://github.com/RGGH/LangChain-Course")
+	 
+	 (comments "cd ~/Downloads ; wget https://github.com/qdrant/qdrant/releases/download/v1.5.1/qdrant-x86_64-unknown-linux-gnu.tar.gz")
+	 (comments "mkdir q; cd q; tar xaf ../q/qdrant*.tar.gz")
+	 (comments "cd ~/Downloads/q; ./qdrant")
+	 (setf COLLECTION_NAME (string "aiw")
+	       TEXTS (list (string "/home/martin/src/LangChain-Course/lc5_indexes/text/aiw.txt"))
+	       vectors (list)
+	       batch_size 512
+	       batch (list))
+
+	 (setf model (SentenceTransformer (string "msmarco-MiniLM-L-6-v3"))
+	       client (QdrantClient :host (string "localhost")
+				    :port 6333
+				    :prefer_grpc False))
+
+	 (def make_collection (client collection_name)
+	   (declare (type str collection_name))
+	   (client.recreate_collection
+	    :collection_name COLLECTION_NAME
+	    :vectors_config (models.VectorParams :size 384
+						 :distance models.Distance.COSINE)))
+
+	 (def make_chunks (input_text )
+	   (declare (type str input_text))
+	   (setf text_splitter (RecursiveCharacterTextSplitter :separators (string "\\n")
+							       :chunk_size 1000
+							       :chunk_overlap 20
+							       :length_function len))
+	   (with (as (open input_text) f)
+		 (setf alice (f.read)))
+	   (setf chunks (text_splitter.create_documents (list alice)))
+	   (return chunks))
+
+	 (setf texts (make_chunks (aref TEXTS 0))) ;; len 170
+
+	 (def gen_vectors (texts model batch batch_size vectors)
+	   (for (part (tqdm texts))
+		(batch.append part.page_content)
+		(when (<= batch_size (len batch))
+		  (vectors.append (model.encode batch))
+		  (setf batch (list))))
+	   (when (< 0 (len batch))
+	     (vectors.append (model.encode batch))
+	     (setf batch (list)))
+	   (setf vectors (np.concatenate vectors))
+	   (setf payload ("list"
+			  (list (for-generator (item texts)
+					       item))))
+	   (setf vectors (list (for-generator (v vectors)
+					      (v.tolist))))
+	   (return (ntuple vectors payload)))
+	 (setf (ntuple fin_vectors
+		       fin_payload)
+	       (gen_vectors :texts texts
+			    :model model
+			    :batch batch
+			    :batch_size batch_size
+			    :vectors vectors))
+
+	 (def upsert_to_qdrant (fin_vectors fin_payload)
+	   (setf collection_info (client.get_collection
+				  :collection_name COLLECTION_NAME))
+	   (client.upsert
+	    :collection_name COLLECTION_NAME
+	    :points (list (for-generator ((ntuple idx vector)
+					  (enumerate fin_vectors))
+					 (PointStruct :id (+ collection_info.vectors_count
+							     idx)
+						      :vector vector
+						      :payload
+						      (dot (aref fin_payload idx)
+							   (to_json)))))))
+	 
+	 (make_collection client COLLECTION_NAME)
+	 (upsert_to_qdrant fin_vectors
+			   fin_payload)
+	 
+	 #+nil
+	 (do0
+	  (comments "this works")
+	  (setf chat (langchain.chat_models.AzureChatOpenAI
+		      :deployment_name chatgpt_deployment_name
+		      :model_name chatgpt_model_name
+		      :temperature 1))
+	  (setf user_input (input (string "Ask me a question: ")))
+
+	  (setf messages (list ;(langchain.schema.SystemMessage :contents (string "You are an angry assistant"))
+			  (langchain.schema.HumanMessage :content user_input)))
+
+	  (print (dot (chat messages)
+		      content)))
+
+	))))
+
---
+++ example/117_langchain_azure_openai/source/p02_use_qdrant.py
@@ -0,0 +1,67 @@
+#|default_exp p02_use_qdrant
+# python -m venv ~/llm_env; . ~/llm_env/bin/activate; source ~/llm_environment.sh;
+# pip install qdrant-client langchain[llms] openai sentence-transformers
+# deactivate
+import os
+import time
+import numpy as np
+import openai
+from langchain.vectorstores import Qdrant
+from langchain.embeddings import HuggingFaceEmbeddings
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from qdrant_client import QdrantClient
+from qdrant_client.http import models
+from qdrant_client.http.models import CollectionStatus
+from qdrant_client.models import PointStruct, Distance, VectorParams
+from sentence_transformers import SentenceTransformer
+from tqdm import tqdm
+start_time=time.time()
+debug=True
+_code_git_version="5d79efa2a15f2455f5d900c082232b0dafa81066"
+_code_repository="https://github.com/plops/cl-py-generator/tree/master/example/117_langchain_azure_openai/source/"
+_code_generation_time="21:21:50 of Tuesday, 2023-09-12 (GMT+1)"
+chatgpt_deployment_name="gpt-35"
+chatgpt_model_name="gpt-35-turbo"
+openai.api_type="azure"
+openai.api_key=os.getenv("OPENAI_API_KEY")
+openai.api_base=os.getenv("OPENAI_API_BASE")
+openai.api_version=os.getenv("OPENAI_API_VERSION")
+# cd ~/src; git clone --depth 1 https://github.com/RGGH/LangChain-Course
+# cd ~/Downloads ; wget https://github.com/qdrant/qdrant/releases/download/v1.5.1/qdrant-x86_64-unknown-linux-gnu.tar.gz
+# mkdir q; cd q; tar xaf ../q/qdrant*.tar.gz
+# cd ~/Downloads/q; ./qdrant
+COLLECTION_NAME="aiw"
+TEXTS=["/home/martin/src/LangChain-Course/lc5_indexes/text/aiw.txt"]
+vectors=[]
+batch_size=512
+batch=[]
+model=SentenceTransformer("msmarco-MiniLM-L-6-v3")
+client=QdrantClient(host="localhost", port=6333, prefer_grpc=False)
+def make_collection(client, collection_name: str):
+    client.recreate_collection(collection_name=COLLECTION_NAME, vectors_config=models.VectorParams(size=384, distance=models.Distance.COSINE))
+def make_chunks(input_text: str):
+    text_splitter=RecursiveCharacterTextSplitter(separators="\n", chunk_size=1000, chunk_overlap=20, length_function=len)
+    with open(input_text) as f:
+        alice=f.read()
+    chunks=text_splitter.create_documents([alice])
+    return chunks
+texts=make_chunks(TEXTS[0])
+def gen_vectors(texts, model, batch, batch_size, vectors):
+    for part in tqdm(texts):
+        batch.append(part.page_content)
+        if ( ((batch_size)<=(len(batch))) ):
+            vectors.append(model.encode(batch))
+            batch=[]
+    if ( ((0)<(len(batch))) ):
+        vectors.append(model.encode(batch))
+        batch=[]
+    vectors=np.concatenate(vectors)
+    payload=list([item for item in texts])
+    vectors=[v.tolist() for v in vectors]
+    return vectors, payload
+fin_vectors, fin_payload=gen_vectors(texts=texts, model=model, batch=batch, batch_size=batch_size, vectors=vectors)
+def upsert_to_qdrant(fin_vectors, fin_payload):
+    collection_info=client.get_collection(collection_name=COLLECTION_NAME)
+    client.upsert(collection_name=COLLECTION_NAME, points=[PointStruct(id=((collection_info.vectors_count)+(idx)), vector=vector, payload=fin_payload[idx].to_json()) for idx, vector in enumerate(fin_vectors)])
+make_collection(client, COLLECTION_NAME)
+upsert_to_qdrant(fin_vectors, fin_payload)
