ddd772b6f900a7b9620c2d761b0e1b50f33c8f20
Martin Kielhorn <wolpumba@gmail.com>
2023-10-25 23:00:53 +0200

    i don't think i should replace - with : in strings. it's not good
    for
    negative numbers
    and i can write colon anyway

--- py.lisp
+++ py.lisp
@@ -580,7 +580,7 @@ entry return-values contains a list of return values. currently supports type, v
 		       (when *warn-breaking*
 			 (format t "~&BREAKING CHANGE ~a is printed as string (used to be symbol, please use (symbol <code>) from now on). I seldomly used this for (\"list\" ...) ~%" code))
 		       )
-	       (substitute #\: #\- (format nil "~a" code))
+	       code ;(substitute #\: #\- (format nil "~a" code))
 	       )
 	      ((numberp code) ;; print constants
 	       (cond ((integerp code) (format str "~a" code))
