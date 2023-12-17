3be0f1259742785e5d20fbebefbe833f007b3f84
Martin Kielhorn <wolpumba@gmail.com>
2023-12-17 01:23:10 +0100

    more

--- example/122_bard/source/p00_use_bard.py
+++ example/122_bard/source/p00_use_bard.py
@@ -1,29 +1,40 @@
-#|default_exp p00_use_bard
+# |default_exp p00_use_bard
 # python -m venv ~/bardapi_env; . ~/bardapi_env/bin/activate; pip install bardapi toml
-# 
+#
 # deactivate
 import os
 import time
 import toml
 from bardapi import BardCookies
-start_time=time.time()
-debug=True
-_code_git_version="8e29c746ccaeea65fcf0c8050e6fb7258c9c6ba6"
-_code_repository="https://github.com/plops/cl-py-generator/tree/master/example/122_bard/source/"
-_code_generation_time="11:22:17 of Sunday, 2023-12-10 (GMT+1)"
+
+start_time = time.time()
+debug = True
+_code_git_version = "8e29c746ccaeea65fcf0c8050e6fb7258c9c6ba6"
+_code_repository = (
+    "https://github.com/plops/cl-py-generator/tree/master/example/122_bard/source/"
+)
+_code_generation_time = "11:22:17 of Sunday, 2023-12-10 (GMT+1)"
 # either set cookies here or read them from env.toml file
-config_path="env.toml"
-if ( os.path.exists(config_path) ):
+config_path = "env.toml"
+if os.path.exists(config_path):
     with open(config_path, "r") as f:
-        data=toml.load(f)
-    cookies=data["cookies"]
-    print("{} Read cookies from config file config_path={} cookies={}".format(((time.time())-(start_time)), config_path, cookies))
+        data = toml.load(f)
+    cookies = data["cookies"]
+    print(
+        "{} Read cookies from config file config_path={} cookies={}".format(
+            ((time.time()) - (start_time)), config_path, cookies
+        )
+    )
 else:
     print("Warning: No .env file found. Please provide API cookies manually.")
-    cookies={("__Secure-1PSID"):("stub"),("__Secure-1PSIDCC"):("stub"),("__Secure-1PAPISID"):("stub")}
-bard=BardCookies(cookie_dict=cookies)
+    cookies = {
+        ("__Secure-1PSID"): ("stub"),
+        ("__Secure-1PSIDCC"): ("stub"),
+        ("__Secure-1PAPISID"): ("stub"),
+    }
+bard = BardCookies(cookie_dict=cookies)
 with open("transcript", "r") as f:
-    text=f.read()
-request=(("Summarize as bullet list: ```")+(text)+("```"))
-print("{} request request={}".format(((time.time())-(start_time)), request))
+    text = f.read()
+request = ("Summarize as bullet list: ```") + (text) + ("```")
+print("{} request request={}".format(((time.time()) - (start_time)), request))
 print(bard.get_answer(request)["content"])
--- example/123_openai/gen00.lisp
+++ example/123_openai/gen00.lisp
@@ -4,7 +4,7 @@
 (in-package :cl-py-generator)
 
 (progn
-  (defparameter *project* "122_bard")
+  (defparameter *project* "123_openai")
   (defparameter *idx* "00")
   (defparameter *path* (format nil "/home/martin/stage/cl-py-generator/example/~a" *project*))
   (defparameter *day-names*
@@ -23,7 +23,7 @@
 
   
   
-  (let* ((notebook-name "use_bard")
+  (let* ((notebook-name "use_openai")
 	 #+nil (cli-args `(
 		     (:short "-v" :long "--verbose" :help "enable verbose output" :action "store_true" :required nil))))
     (write-source
@@ -31,9 +31,15 @@
      `(do0
        (do0
 	,(format nil "#|default_exp p~a_~a" *idx* notebook-name))
-       (comments "python -m venv ~/bardapi_env; . ~/bardapi_env/bin/activate; pip install bardapi toml"
+       (comments "python -m venv ~/bardapi_venv")
+       (comments "python -m venv ~/bardapi_env; . ~/bardapi_env/bin/activate; pip install openai toml"
 		 ""
 		 "deactivate")
+       (comments "https://platform.openai.com/docs/api-reference/authentication"
+		 "https://github.com/openai/openai-python")
+       (comments "env.toml contains this:"
+		 "[keys]"
+		 "key_token = \" ... \" ")
        (do0
 	  
 	(imports (	os
@@ -43,7 +49,7 @@
 					;pathlib
 					;(np numpy)
 					;serial
-			;(pd pandas)
+					;(pd pandas)
 					;(xr xarray)
 					;(xrp xarray.plot)
 					;skimage.restoration
@@ -69,16 +75,12 @@
 					;io.StringIO
 					;bs4
 					;requests
-			
 					;(np jax.numpy)
 					;(mpf mplfinance)
-
 					;argparse
 			toml
-			
-					
 			)))
-       (imports-from (bardapi BardCookies))
+       (imports-from (openai OpenAI))
 	  
        (setf start_time (time.time)
 	     debug True)
@@ -114,105 +116,26 @@
 				   (string "r"))
 			     f)
 			 (setf data (toml.load f)))
-		   (setf cookies (aref data (string "cookies")))
-		   ,(lprint "Read cookies from config file"
-			    `(config_path cookies))
+		   (setf keys (aref data (string "keys")))
+		  #+nil ,(lprint :vars `(keys))
 		   )
 	      (do0
-	       (print (string "Warning: No .env file found. Please provide API cookies manually."))
-	       (setf cookies (dict
-			      ((string "__Secure-1PSID")
-			       (string "stub"))
-			      ((string "__Secure-1PSIDCC")
-			       (string "stub"))
-			      ((string "__Secure-1PAPISID")
-			       (string "stub"))))))
-	  
-	  (setf bard (BardCookies :cookie_dict cookies))
-	  (with (as (open (string "transcript")
+	       (print (string "Warning: No env.toml file found."))
+	       ))
+	  (setf client (OpenAI :api_key (aref keys (string "key_token"))))
+	  (setf chat_completion (client.chat.completions.create
+				 :messages (list (dictionary
+						  :role (string "user")
+						  :content (string "Say this is a test")))
+				 :model (string "gpt-3.5-turbo")))
+
+	 ;; openai.RateLimitError: Error code: 429 - {'error': {'message': 'You exceeded your current quota, please check your plan and billing details. For more information on this error, read the docs: https://platform.openai.com/docs/guides/error-codes/api-errors.', 'type': 'insufficient_quota', 'param': None, 'code': 'insufficient_quota'}}
+
+	  #+nil (setf bard (BardCookies :cookie_dict cookies))
+
+	  #+nil (with (as (open (string "transcript")
 			  (string "r"))
 		    f)
 		(setf text (f.read)))
-	  (setf request (+
-			 (string "I will ask you to summarize the transcript of a video. Here is an example transcript:
-```
-0:00
-hey guys previously on this channel I
-0:01
-have uploaded a video about the magic
-0:03
-anime repository So based on that
-0:05
-repository you have to provide the input
-0:06
-of the image and a den poose sequence
-0:09
-video motion sequence video and in
-0:11
-return you going to get uh the motion
-0:13
-sequence applied to the input image PR
-0:16
-that project was really good you can try
-0:18
-this watching this video so in that
-0:20
-video uh the problem was I got few
-0:22
-comments regarding asking how to create
-0:25
-this dense POS video how to create the
-0:27
-custom Den POS video I was using a
-0:29
-pre-downloaded pre- provided D post
-0:31
-video so in this video I'm going to tell
-0:33
-you how to convert your input video into
-0:35
-a dense post video by using DET
-0:37
-detectron 2 of Facebook research and
-0:40
-again D post so let's start the video so
-0:43
-all agenda of this video would be after
-0:46
-this video you would be able to convert
-0:48
-the input video into the output video
-0:50
-which will be the dens POS video based
-0:52
-on the input normal sequence for
-0:54
-starting up in the description of this
-0:56
-video you going to get a GitHub
-0:58
-repository link just click on that link
-```
-A summary could look like:
-```
-- The video is about converting an input video into a dense pose video using Detectron 2 and DensPose.
-- The presenter provides a GitHub repository link for downloading the code and instructions for installation.
-```
-")
-			 (string "Summarize as bullet list: ```")
-			    text
-			    (string "```")))
-	  ,(lprint "request" `(request))
-	  (print (aref  (bard.get_answer
-			 request)
-			(string "content"))))
-
-	 ;; input: What model are you using?
-	 ;; output: I'm using the Gemini language model. It's a large
-	 ;; language model chatbot developed by Google AI, trained on
-	 ;; a massive dataset of text and code. It can generate text,
-	 ;; translate languages, write different kinds of creative
-	 ;; content, and answer your questions in an informative way.
-
-	))))
+	  )))))
 
