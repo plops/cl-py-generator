616c2d0149858f6dca45e5a6b1db4bfcaa753ad3
Martin Kielhorn <wolpumba@gmail.com>
2024-04-24 00:01:32 +0200

    more

--- example/129_color_conv/gen01.lisp
+++ example/129_color_conv/gen01.lisp
@@ -29,7 +29,14 @@
 		      :help "Approximate number of words per chunk")
 		     #+nil (:short "p" :long "prompt" :type str
 		      :default (string "Summarize the following video transcript as a bullet list.")
-		      :help "The prompt to be prepended to the output file(s)."))))
+		      :help "The prompt to be prepended to the output file(s).")))
+	 		       
+	 (l-coef `((:name coeff_matrix :value (1 0 0
+						 0 1 0
+						 0 0 1) :vary True :dim (3 3))
+		   (:name offsets :value (0 0 0) :dim (3))
+		   (:name gains :value (1 1 1) :dim (3))
+		   (:name gamma :value (2.2) :dim (1)))))
     (write-source
      (format nil "~a/source/p~a_~a" *path* *idx* notebook-name)
      `(do0
@@ -41,36 +48,46 @@
 		 (cv cv2)
 		 (pd pandas)
 		 lmfit))
-       
-       (setf start_time (time.time)
-	     debug True)
-       (setf
-	_code_git_version
-	(string ,(let ((str (with-output-to-string (s)
-			      (sb-ext:run-program "/usr/bin/git" (list "rev-parse" "HEAD") :output s))))
-		   (subseq str 0 (1- (length str)))))
-	_code_repository (string ,(format nil
-					  "https://github.com/plops/cl-py-generator/tree/master/example/~a/source/"
-					  *project*))
-	_code_generation_time
-	(string ,(multiple-value-bind
-		       (second minute hour date month year day-of-week dst-p tz)
-		     (get-decoded-time)
-		   (declare (ignorable dst-p))
-		   (format nil "~2,'0d:~2,'0d:~2,'0d of ~a, ~d-~2,'0d-~2,'0d (GMT~@d)"
-			   hour
-			   minute
-			   second
-			   (nth day-of-week *day-names*)
-			   year
-			   month
-			   date
-			   (- tz)))))
+
+       #+nil(do0
+	(setf start_time (time.time)
+	      debug True)
+	(setf
+	 _code_git_version
+	 (string ,(let ((str (with-output-to-string (s)
+			       (sb-ext:run-program "/usr/bin/git" (list "rev-parse" "HEAD") :output s))))
+		    (subseq str 0 (1- (length str)))))
+	 _code_repository (string ,(format nil
+					   "https://github.com/plops/cl-py-generator/tree/master/example/~a/source/"
+					   *project*))
+	 _code_generation_time
+	 (string ,(multiple-value-bind
+			(second minute hour date month year day-of-week dst-p tz)
+		      (get-decoded-time)
+		    (declare (ignorable dst-p))
+		    (format nil "~2,'0d:~2,'0d:~2,'0d of ~a, ~d-~2,'0d-~2,'0d (GMT~@d)"
+			    hour
+			    minute
+			    second
+			    (nth day-of-week *day-names*)
+			    year
+			    month
+			    date
+			    (- tz))))))
 
        #+nil (do0 (setf bgr (cv.Mat (list (list (list 10 120 13)))))
 	    (cv.cvtColor bgr cv.COLOR_BGR2YCrCb))
-
-       (def bgr_to_ycbcr_model (bgr coeff_matrix offsets gains gamma)
+       " "
+       (def bgr_to_ycbcr_model (bgr ,@(let ((count 0))
+					 (loop for e in l-coef
+					       appending
+					       (destructuring-bind (&key name value dim (vary 'True)) e
+						 (loop for v in value
+						       collect
+						       (prog1
+							   (intern (string-upcase (format nil "~a~a" name count)))
+							 (incf count)))
+						 ))))
 	 (string3 "Model for BGR to YCbCr color transformation with adjustable parameters.
 
   Args:
@@ -83,39 +100,35 @@
   Returns:
     A numpy array of shape (3,) representing the Y, Cb, Cr values.")
 
+	 (setf params (np.array (list ,@(let ((count 0))
+					 (loop for e in l-coef
+					       appending
+					       (destructuring-bind (&key name value dim (vary 'True)) e
+						 (loop for v in value
+						       collect
+						       (prog1
+							   (intern (string-upcase (format nil "~a~a" name count)))
+							 (incf count)))
+						 ))))))
+	 ,@(let ((count 0))
+	     (loop for e in l-coef
+		   collect
+		   (destructuring-bind (&key name value dim (vary 'True)) e
+		     (let ((inc (length value)))
+		       (prog1
+			   `(setf ,name (dot (aref params (slice ,count ,(+ count inc)))
+					     (reshape (tuple ,@dim))))
+			 (incf count inc))))))
 	 (setf bgr_gamma (np.power (/ bgr 255s0)
 				   (/ 1s0 gamma)))
 	 (setf ycbcr (+ (* (np.dot coeff_matrix bgr_gamma) gains )
 			offsets))
 	 (return ycbcr))
-
-       (def fit_bgr_to_ycbcr (df)
-	 (string3 "Fits the BGR to YCbCr model to data using lmfit.
-
-  Args:
-    df: A pandas DataFrame with columns 'B', 'G', 'R', 'Y', 'Cb', 'Cr'.
-
-  Returns:
-    An lmfit ModelResult object containing the fitted parameters.")
-	 (setf model (lmfit.Model bgr_to_ycbcr_model)
-	       params (model.make_params
-		       :coeff_matrix (np.identity 3)
-		       :offsets (np.zeros 3)
-		       :gains (np.ones 3)
-		       :gamma 2.2
-		       )
-	       result (model.fit (dot (aref df (list (string "Y")
-						  (string "Cb")
-						  (string "Cr")))
-				      values)
-				 params
-				 :bgr (dot (aref df (list (string "B")
-						  (string "G")
-						  (string "R")))
-				      values)))
-	 (return result))
+       " "
+       
 
        (do0
+	" "
 	(setf num_colors 100)
 	(setf bgr_colors (np.random.randint 0 256 :size (tuple num_colors 3)))
 	(setf res (list))
@@ -131,9 +144,39 @@
 				     :Cr (aref ycbcr 2)
 				     
 				     )))
-	
+	" "
+
 	(setf df (pd.DataFrame res))
-	(setf result (fit_bgr_to_ycbcr df))
+
+	(do0 
+	    ; def fit_bgr_to_ycbcr (df)
+	    (string3 "Fits the BGR to YCbCr model to data using lmfit.
+
+  Args:
+    df: A pandas DataFrame with columns 'B', 'G', 'R', 'Y', 'Cb', 'Cr'.
+
+  Returns:
+    An lmfit ModelResult object containing the fitted parameters.")
+	 (setf model (lmfit.Model bgr_to_ycbcr_model)
+	       params (model.make_params
+		       :coeff_matrix (np.identity 3)
+		       :offsets (np.zeros 3)
+		       :gains (np.ones 3)
+		       :gamma "2.2"
+		       )
+	       result (model.fit (dot (aref df (list (string "Y")
+						  (string "Cb")
+						  (string "Cr")))
+				      values)
+				 params
+				 :bgr (dot (aref df (list (string "B")
+						  (string "G")
+						  (string "R")))
+				      values)))
+	 ;(return result)
+	 )
+
+	;(setf result (fit_bgr_to_ycbcr df))
 	(print (result.fit_report)))
 
        
--- example/129_color_conv/source/p01_conv.py
+++ example/129_color_conv/source/p01_conv.py
@@ -5,12 +5,8 @@ import numpy as np
 import cv2 as cv
 import pandas as pd
 import lmfit
-start_time=time.time()
-debug=True
-_code_git_version="fbe9ab259e457c53f1559ae626c647d1da67da16"
-_code_repository="https://github.com/plops/cl-py-generator/tree/master/example/129_color_conv/source/"
-_code_generation_time="23:35:40 of Tuesday, 2024-04-23 (GMT+1)"
-def bgr_to_ycbcr_model(bgr, coeff_matrix, offsets, gains, gamma):
+ 
+def bgr_to_ycbcr_model(bgr, coeff_matrix0, coeff_matrix1, coeff_matrix2, coeff_matrix3, coeff_matrix4, coeff_matrix5, coeff_matrix6, coeff_matrix7, coeff_matrix8, offsets9, offsets10, offsets11, gains12, gains13, gains14, gamma15):
     """Model for BGR to YCbCr color transformation with adjustable parameters.
 
   Args:
@@ -22,27 +18,32 @@ def bgr_to_ycbcr_model(bgr, coeff_matrix, offsets, gains, gamma):
 
   Returns:
     A numpy array of shape (3,) representing the Y, Cb, Cr values."""
+    params=np.array([coeff_matrix0, coeff_matrix1, coeff_matrix2, coeff_matrix3, coeff_matrix4, coeff_matrix5, coeff_matrix6, coeff_matrix7, coeff_matrix8, offsets9, offsets10, offsets11, gains12, gains13, gains14, gamma15])
+    coeff_matrix=params[0:9].reshape((3,3,))
+    offsets=params[9:12].reshape((3,))
+    gains=params[12:15].reshape((3,))
+    gamma=params[15:16].reshape((1,))
     bgr_gamma=np.power(((bgr)/((255.    ))), (((1.0    ))/(gamma)))
     ycbcr=((((np.dot(coeff_matrix, bgr_gamma))*(gains)))+(offsets))
     return ycbcr
-def fit_bgr_to_ycbcr(df):
-    """Fits the BGR to YCbCr model to data using lmfit.
-
-  Args:
-    df: A pandas DataFrame with columns 'B', 'G', 'R', 'Y', 'Cb', 'Cr'.
-
-  Returns:
-    An lmfit ModelResult object containing the fitted parameters."""
-    model=lmfit.Model(bgr_to_ycbcr_model)
-    params=model.make_params(coeff_matrix=np.identity(3), offsets=np.zeros(3), gains=np.ones(3), gamma=(2.20    ))
-    result=model.fit(df[["Y", "Cb", "Cr"]].values, params, bgr=df[["B", "G", "R"]].values)
-    return result
+ 
+ 
 num_colors=100
 bgr_colors=np.random.randint(0, 256, size=(num_colors,3,))
 res=[]
 for bgr in bgr_colors:
     ycbcr=cv.cvtColor(np.uint8([[bgr]]), cv.COLOR_BGR2YCrCb)[0,0]
     res.append(dict(B=bgr[0], G=bgr[1], R=bgr[2], Y=ycbcr[0], Cb=ycbcr[1], Cr=ycbcr[2]))
+ 
 df=pd.DataFrame(res)
-result=fit_bgr_to_ycbcr(df)
+"""Fits the BGR to YCbCr model to data using lmfit.
+
+  Args:
+    df: A pandas DataFrame with columns 'B', 'G', 'R', 'Y', 'Cb', 'Cr'.
+
+  Returns:
+    An lmfit ModelResult object containing the fitted parameters."""
+model=lmfit.Model(bgr_to_ycbcr_model)
+params=model.make_params(coeff_matrix=np.identity(3), offsets=np.zeros(3), gains=np.ones(3), gamma=2.2)
+result=model.fit(df[["Y", "Cb", "Cr"]].values, params, bgr=df[["B", "G", "R"]].values)
 print(result.fit_report())
