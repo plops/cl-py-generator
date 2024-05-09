c4e9cfdde50d6135bed970993b5a0acac807556a
Martin Kielhorn <wolpumba@gmail.com>
2024-05-09 23:23:04 +0200

    i will have to generate and add the following python files
    to the repo
    
    0.06022763252258301 the following folders need python file g20.short=112      116_yt_comment_scraper/gen00
    15              14_megatron_gpt/gen00
    8                    44_zernike/gen00
    49                 47_jax_trace/gen00
    78                      49_wgpu/gen00
    98                     50_yahoo/gen01
    68                    53_topopt/gen00
    84              55_freecad_part/gen00
    19                     58_migen/gen00
    102             63_scrape_graph/gen01
    3                      64_flask/gen01
    79     65_gaussian_variance_mse/gen01
    0             67_self_reference/gen01
    5                      68_arith/gen01
    139                  69_dataset/gen01
    70              70_star_tracker/gen01
    129                   72_jaxopt/gen01
    126               75_jax_render/gen01
    133                   82_spline/gen01
    34                       84_lte/gen01
    75            85_o3d_pointcloud/gen01
    14                86_playwright/gen01
    64                    88_plotly/gen01
    140                   89_justpy/gen00
    138                     90_lona/gen00
    40                       94_tor/gen01

---
+++ example/14_megatron_gpt/source/run_00_nlp.py
@@ -0,0 +1,9 @@
+# 
+# export LANG=en_US.utf8
+import matplotlib
+import matplotlib.pyplot as plt
+plt.ion()
+import os
+import pathlib
+from fastai2.text.all import *
+path=pathlib.Path("/home/martin/txt_utf8")
