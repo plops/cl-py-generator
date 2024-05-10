bb109ace03dab958f7b8f5b88c548ff5717ba363
Martin Kielhorn <wolpumba@gmail.com>
2024-05-10 17:53:32 +0200

    make language a variable

--- example/131_whisper/transcribe.sh
+++ example/131_whisper/transcribe.sh
@@ -1,10 +1,16 @@
 NAME=_pq6Rp8W6bo
+LANG=chinese
 ODIR=.
 MODEL=~/src/whisper.cpp/models/ggml-large-v3.bin
+
 # sudo apt install yt-dlp ffmpeg
 
 yt-dlp -f 251 $NAME -o $NAME.webm
 
 ffmpeg -y -i $NAME.webm -ar 16000 -ac 1 -c:a pcm_s16le $ODIR/output.wav
 
-~/src/whisper.cpp/bgpu/bin/main -l chinese -m $MODEL -f $ODIR/output.wav -otxt -nt -of chinese.txt   -osrt chinese.srt -ovtt chinese.vtt -olrc chinese.lrc 2>&1 | tee chinese_tee.txt
+~/src/whisper.cpp/bgpu/bin/main -l $LANG -m $MODEL -f $ODIR/output.wav -otxt -nt -of $LANG.txt -osrt $LANG.srt 2>&1 | tee $LANG_tee.txt
+
+echo -e 'Summarize the following video transcript as a bullet list. Prepend each bullet point with the starting timestamp. Do not show the end timestamps. Write names in english (pinyin) transliteration as well as chinese characters. Also split the summary into sections and create section titles:\n```\n' > $NAME.md 
+awk -F ' --> ' '/^[0-9]+$/{next} NF==2{gsub(",[0-9]+", "", $1); print $1} NF==1' *.srt >> $NAME.md
+echo -e '```\nShow the english summary.' >> $NAME.md
