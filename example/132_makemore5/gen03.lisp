9363d7828c2a2a73f4047dceb00e3c6be90f8782
Martin Kielhorn <wolpumba@gmail.com>
2024-05-12 12:05:31 +0200

    model has 170k parameters now

--- example/132_makemore5/gen03.lisp
+++ example/132_makemore5/gen03.lisp
@@ -561,11 +561,25 @@ MLP is a probability distribution over the vocabulary.")
 	 (setf model (Sequential
 				 (list
 				  (Embedding vocab_size n_embed)
-				  (FlattenConsecutive block_size)
-				  (Linear (* n_embed block_size)
+
+				  (FlattenConsecutive 2)
+				  (Linear (* n_embed 2)
 					  n_hidden
 					  :bias False)
 				  (BatchNorm1d n_hidden)
+
+				  (FlattenConsecutive 2)
+				  (Linear (* n_hidden 2)
+					  n_hidden
+					  :bias False)
+				  (BatchNorm1d n_hidden)
+
+				  (FlattenConsecutive 2)
+				  (Linear (* n_hidden 2)
+					  n_hidden
+					  :bias False)
+				  (BatchNorm1d n_hidden)
+
 				  (Tanh)
 				  (Linear n_hidden vocab_size))))
 
