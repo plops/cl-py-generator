64ad3b70441a754b34ea21f5eb0bde248c3232c1
Martin Kielhorn <wolpumba@gmail.com>
2024-06-23 07:58:39 +0200

    main

--- example/139_pyqt6/p01_simple.py
+++ example/139_pyqt6/p01_simple.py
@@ -1,9 +1,12 @@
-from PyQt6.QtWidgets import QApplication, QWidget
+from PyQt6.QtWidgets import QApplication, QMainWindow
 import sys
 
 app = QApplication(sys.argv)
 
-window = QWidget()
+window = QMainWindow()
+
+window.statusBar().showMessage('Hello, World!')
+window.menuBar().addMenu('File')
 
 window.show()
 
