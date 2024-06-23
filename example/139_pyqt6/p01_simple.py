dacbc4b978a7d70284af48697bc444e410eb8c6d
Martin Kielhorn <wolpumba@gmail.com>
2024-06-23 08:04:20 +0200

    window

--- example/139_pyqt6/p01_simple.py
+++ example/139_pyqt6/p01_simple.py
@@ -1,12 +1,17 @@
-from PyQt6.QtWidgets import QApplication, QMainWindow
+from PyQt6.QtWidgets import QApplication, QMainWindow, QWidget
 import sys
+
+
+class Window(QWidget):
+    def __init__(self):
+        super().__init__()
+
+        self.setWindowTitle('PyQt6 Window')
+        self.setGeometry(100, 100, 800, 600)
 
 app = QApplication(sys.argv)
 
-window = QMainWindow()
-
-window.statusBar().showMessage('Hello, World!')
-window.menuBar().addMenu('File')
+window = Window()
 
 window.show()
 
