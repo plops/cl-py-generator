97b0066886ca7e3a42dbcd7e51a4d9a95d0b0572
Martin Kielhorn <wolpumba@gmail.com>
2024-08-08 21:55:35 +0200

    gapi

--- example/143_helium_gemini/gapi.py
+++ example/143_helium_gemini/gapi.py
@@ -2,12 +2,14 @@
 # micromamba activate
 # pip install google-generativeai PySocks
 
-import google.generativeai as genai
-import socket
-import socks
+# export http_proxy="socks4://localhost:8080"; export https_proxy="socks4://localhost:8080"
 
-socks.set_default_proxy(socks.SOCKS5, "localhost", 8080)
-socket.socket = socks.socksocket
+import google.generativeai as genai
+#import socket
+#import socks
+
+#socks.set_default_proxy(socks.SOCKS5, "localhost", 8080)
+#socket.socket = socks.socksocket
 
 
 # read api key from file
@@ -16,6 +18,9 @@ with open("/home/martin/api_key.txt") as f:
 
 genai.configure(api_key=key)
 
-model = genai.GenerativeModel('gemini-1.5-flash')
-response =model.generate_content("Give me python code to sort a list")
+model = genai.GenerativeModel('gemini-1.5-flash')\
+prompt = "Tell me a joke"
+response =model.generate_content(prompt,
+                                 generation_config={"temperature": 2.0},
+                                 safety_settings={"threshold": BLOCK_NONE})
 print(response.text)
