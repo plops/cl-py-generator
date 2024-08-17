0fb8b904c3a93f076368cf205077e19c86d34e18
Martin Kielhorn <wolpumba@gmail.com>
2024-08-17 23:29:44 +0200

    vscode has execution, paredit, but i don't like the formatter for common lisp

--- example/143_helium_gemini/gen01.lisp
+++ example/143_helium_gemini/gen01.lisp
@@ -5,47 +5,53 @@
 (in-package :cl-py-generator)
 
 (progn
-  (defparameter *project* "143_helium_gemini")
-  (defparameter *idx* "01")
-  (defparameter *path* (format nil "/home/martin/stage/cl-py-generator/example/~a" *project*))
-  (defparameter *day-names*
-    '("Monday" "Tuesday" "Wednesday"
-      "Thursday" "Friday" "Saturday"
-      "Sunday"))
-  (defun lprint (&key msg vars)
-    `(do0 ;when args.verbose
-       (print (dot (string ,(format nil "{} ~a ~{~a={}~^ ~}"
-				    msg
-				    (mapcar (lambda (x)
-					      (emit-py :code x))
-					    vars)))
-                   (format  (- (time.time) start_time)
-                            ,@vars)))))
-  (defun doc (def)
-    `(do0
-      ,@(loop for e in def
-	      collect
-	      (destructuring-bind (&key name val (unit "-") (help name)) e
-		`(do0
-		  (comments ,(format nil "~a (~a)" help unit))
-		  (setf ,name ,val))))))
-  
-  (let* ((notebook-name "host")
-		 )
-    (write-source
+ (defparameter *project* "143_helium_gemini")
+ (defparameter *idx* "01")
+ (defparameter *path* (format nil "/home/martin/stage/cl-py-generator/example/~a" *project*))
+ (defparameter *day-names*
+               '("Monday" "Tuesday" "Wednesday"
+                          "Thursday" "Friday" "Saturday"
+                          "Sunday"))
+ (defun lprint (&key msg vars)
+   `(do0 ;when args.verbose
+        (print (dot (string ,(format nil "{} ~a ~{~a={}~^ ~}"
+                               msg
+                               (mapcar (lambda (x)
+                                         (emit-py :code x))
+                                   vars)))
+                    (format (- (time.time) start_time)
+                        ,@vars)))))
+ (defun doc (def)
+   `(do0
+     ,@(loop for e in def
+             collect
+               (destructuring-bind (&key name val (unit "-") (help name)) e
+                 `(do0
+                   (comments ,(format nil "~a (~a)" help unit))
+                   (setf ,name ,val))))))
+
+ (let* ((notebook-name "host"))
+   (write-source
      (format nil "~a/source/p~a_~a" *path* *idx* notebook-name)
      `(do0
        "#!/usr/bin/env python3"
-	"# pip install -U google-generativeai"
-           ; M-S-Enter in alive to execute top-level expression
-       
-		(imports (os
-           	(genai google.generativeai))
-           )
-		(imports-from (fasthtml.common *))
-               
-        (genai.configure :api_key (os.environ (string "API_KEY")))
+       "# pip install -U google-generativeai"
+       ; M-S-Enter in alive to execute top-level expression
 
-       
-       ))))
+       (imports ((genai google.generativeai)))
+       (imports-from (fasthtml.common *))
 
+       (with (as (open (string "api_key.txt"))
+                 f)
+             (setf api_key (dot f (read) (strip))))
+
+       (genai.configure :api_key api_key)
+
+       (@rt (string "/"))
+       (def get ()
+            (setf frm (Form
+                       (Group
+                        (Textarea :placeholder (string "Paste YouTube transcript here"
+                                  :name (string "transcript"))
+                        (Select (Option (string "gemini-1.5-pro-exp-0801")
+                                                      (Option ))))))))))))
