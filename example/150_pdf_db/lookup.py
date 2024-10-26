2345d4c0c720685d2911c2aba445f4bb862fec02
Martin Kielhorn <wolpumba@gmail.com>
2024-10-26 11:35:48 +0200

    full text search seems to work

--- example/150_pdf_db/lookup.py
+++ example/150_pdf_db/lookup.py
@@ -13,3 +13,26 @@ args = parser.parse_args()
 
 
 db = sqlite_utils.Database(args.db_path)
+
+try:
+    fts_columns = ['text','path']
+    # ['text','path','pdfinfo','pdfinfo_url']
+    db['pdfs'].enable_fts(fts_columns)
+    db['pdfs'].populate_fts(fts_columns)
+except Exception as e:
+    print(e)
+    pass
+
+# after update of the table you need to run .populate_fts to update search index
+# or use create_trigger=True. However, this code doesn't change the database entries
+
+rows = list(db['pdfs'].search('opengl'))
+
+q = db['pdfs'].detect_fts()
+
+db.add_fts_index('items', columns=['name'])
+
+# Perform a search
+results = db.execute("SELECT * FROM items WHERE items MATCH 'fruit'")
+for row in results:
+    print(row)
