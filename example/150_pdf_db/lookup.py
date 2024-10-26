50244d79206294e4468f6351ef129befc8ffced1
Martin Kielhorn <wolpumba@gmail.com>
2024-10-26 11:48:29 +0200

    cleanup

--- example/150_pdf_db/lookup.py
+++ example/150_pdf_db/lookup.py
@@ -1,24 +1,19 @@
 import argparse
 import sqlite_utils
-#import subprocess
-import time
-import json
-#from pathlib import Path
-#from multiprocessing import Pool
-
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--db_path", default="pdfs.db", help="Path to the SQLite database.")
+parser.add_argument("--search", default="OpenGL", help="Search term to search in the SQLite database.")
 args = parser.parse_args()
-
 
 db = sqlite_utils.Database(args.db_path)
 
 try:
-    fts_columns = ['text','path']
-    # ['text','path','pdfinfo','pdfinfo_url']
-    db['pdfs'].enable_fts(fts_columns)
-    db['pdfs'].populate_fts(fts_columns)
+    if db['pdfs'].detect_fts() is None:
+        fts_columns = ['text','path']
+        # ['text','path','pdfinfo','pdfinfo_url']
+        db['pdfs'].enable_fts(fts_columns)
+        db['pdfs'].populate_fts(fts_columns)
 except Exception as e:
     print(e)
     pass
@@ -26,5 +21,6 @@ except Exception as e:
 # after update of the table you need to run .populate_fts to update search index
 # or use create_trigger=True. However, this code doesn't change the database entries
 
-rows = list(db['pdfs'].search(db.quote_fts('H.264')))
-print(rows)
+rows = list(db['pdfs'].search(db.quote_fts(args.search)))
+for row in rows:
+    print(row['path'])
