7107f9b873f52ccfebb6265da47277cf8707e13c
Martin Kielhorn <wolpumba@gmail.com>
2025-02-17 07:15:36 +0100

    more

--- example/143_helium_gemini/source04/tsum/start_tinyproxy.sh
+++ example/143_helium_gemini/source04/tsum/start_tinyproxy.sh
@@ -1,1 +1,3 @@
+# this is only needed if the generateai google server must run in US and the website host is outside of US
+# tinyproxy allows to route the transaction via SSH socks5 proxy to an exit host in the US
 tinyproxy -d -c tinyproxy.conf
