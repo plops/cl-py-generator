e038fccfff05fb8d2d0faf8013dc179a10243b77
Martin Kielhorn <martin.kielhorn@hexagon.com>
2025-03-01 11:47:04 +0100

    Build 237 more packages

--- example/110_gentoo/docker/setup02_run_container.sh
+++ example/110_gentoo/docker/setup02_run_container.sh
@@ -1,1 +1,3 @@
+# Run the docker image. If you want to copy files from inside of the container
+# to the host, copy them to /tmp/outside.
 docker run -it --privileged -v /dev/shm:/tmp/outside gentoo-ideapad
