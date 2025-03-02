bcc76ad83dc1c65db94135f7e095402b61e9e914
Martin Kielhorn <martin.kielhorn@hexagon.com>
2025-03-02 13:21:49 +0100

    create an initramfs that will work on a non-encrypted disk

--- example/110_gentoo/docker/copy_files.sh
+++ example/110_gentoo/docker/copy_files.sh
@@ -7,3 +7,4 @@ mkdir -p /tmp/outside/${TARGET}
 cp /gentoo.squashfs /tmp/outside/${TARGET}/
 cp /boot/vmlinuz /tmp/outside/${TARGET}/
 cp /boot/initramfs_squash_crypt-x86_64.img /tmp/outside/${TARGET}/
+cp /boot/initramfs_squash_nvme0n1p5-x86_64.img /tmp/outside/${TARGET}/
