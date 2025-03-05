1ffe86e7f9d4d5595580253571aaf9f15cfae8ff
Martin Kielhorn <martin.kielhorn@hexagon.com>
2025-03-05 17:08:40 +0100

    more

--- example/110_gentoo/docker_min/setup02_run_container.sh
+++ example/110_gentoo/docker_min/setup02_run_container.sh
@@ -1,4 +1,4 @@
 # Run the docker image. If you want to copy files from inside of the container
 # to the host, copy them to /tmp/outside.
 
-docker run -it --privileged -v /dev/shm:/tmp/outside gentoo-ideapad_20250301         
+docker run -it --privileged -v /dev/shm:/tmp/outside gentoo-ideapad-min_20250305         
