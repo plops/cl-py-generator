7f11fe8ccde143ebb3a4769c9ab33d2eccbf5960
Martin Kielhorn <martin.kielhorn@hexagon.com>
2025-03-09 19:29:32 +0100

    grub is now properly installed on the image but doesn't boot in qemu yet

--- example/110_gentoo/docker_min/setup04_create_qemu.sh
+++ example/110_gentoo/docker_min/setup04_create_qemu.sh
@@ -15,14 +15,20 @@ parted /dev/loop0 mklabel msdos
 parted /dev/loop0 mkpart primary ext4 1Mib 1Gib
 # Set the boot flag
 parted /dev/loop0 set 1 boot on
+
+# Update partition table
+losetup -d /dev/loop0
+losetup -fP qemu/nvme0n1.img 
+
 # Create a file system
-mkfs.ext4 /dev/loop0
+mkfs.ext4 /dev/loop0p1
 # Mount the file system
-mount /dev/loop0 /mnt
+mount /dev/loop0p1 /mnt
 # Create folder for grub
 mkdir -p /mnt/boot/grub
+mkdir -p /mnt/boot/efi
 # Install grub
-grub-install --boot-directory=/mnt/boot/grub /dev/loop0
+grub-install --target=i386-pc --boot-directory=/mnt/boot/grub /dev/loop0
 # Copy squashfs, initramfs and kernel
 cp /dev/shm/gentoo*/{*.squashfs,*.img,vmlinuz} /mnt
 
--- example/110_gentoo/docker_min/setup05_run_qemu.sh
+++ example/110_gentoo/docker_min/setup05_run_qemu.sh
@@ -1,6 +1,5 @@
 # -vnc unix:/home/user/.qemu-vnc-socket
 # -cpu host 
 qemu-system-x86_64 \
-    -cpu host \
  -drive file=qemu/nvme0n1.img,format=raw \
  -m 8G
