71cd62433e3d8ecf240d2a9d32f79b61b8f05334
Martin <wolpumba@gmail.com>
2025-03-13 17:41:36 +0100

    add gemma3

--- example/143_helium_gemini/gen02.lisp
+++ example/143_helium_gemini/gen02.lisp
@@ -60,6 +60,7 @@
 			    (:name gemma-2-2b-it :input-price -1 :output-price -1 :context-length 128_000 :harm-civic nil)
 			    (:name gemma-2-9b-it :input-price -1 :output-price -1 :context-length 128_000 :harm-civic nil)
 			    (:name gemma-2-27b-it :input-price -1 :output-price -1 :context-length 128_000 :harm-civic nil)
+			    (:name gemma-3-27b-it :input-price -1 :output-price -1 :context-length 128_000 :harm-civic nil)
 			    (:name gemini-1.5-flash :input-price ,iflash :output-price ,oflash :context-length 128_000 :harm-civic nil)
 			    (:name gemini-1.5-pro :input-price ,ipro :output-price ,opro :context-length 128_000 :harm-civic nil)
 			    (:name gemini-1.0-pro :input-price .5 :output-price 1.5 :context-length 128_000 :harm-civic nil)
