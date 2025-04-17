44e7c1dade57e7f4c5fdf5b8d6f6fd9cb0a2f23a
Martin <wolpumba@gmail.com>
2025-04-17 06:57:07 +0200

    add command line arguments

--- example/160_udp_holepunch/README.md
+++ example/160_udp_holepunch/README.md
@@ -3,6 +3,7 @@
 | gen02 | just execute itself via ssh |
 | gen03 | send packets (2x)           |
 | gen04 | send many packets           |
+| gen05 | determine ip addresses      |
 |       |                             |
 
 https://gist.github.com/gordol/d3f66e5fa0a53a9ccef30b77885042be
--- example/160_udp_holepunch/gen05.lisp
+++ example/160_udp_holepunch/gen05.lisp
@@ -9,7 +9,7 @@
 
 (let ()
   (defparameter *project* "160_udp_holepunch")
-  (defparameter *idx* "04") 
+  (defparameter *idx* "05") 
   (defparameter *path* (format nil "/home/martin/stage/cl-py-generator/example/~a" *project*))
   (defparameter *day-names*
     '("Monday" "Tuesday" "Wednesday"
@@ -37,11 +37,12 @@
   (let* ((notebook-name "simultaneous_packets")
 	 )
     (write-source
-     (format nil "~a/source01/p~a_~a" *path* "04" notebook-name)
+     (format nil "~a/source01/p~a_~a" *path* *idx* notebook-name)
      `(do0
        "#!/usr/bin/env python3"
        
-       (imports (subprocess sys os socket time))
+       (imports (subprocess sys os socket time
+			    argparse))
        (imports-from (subprocess Popen PIPE DEVNULL))
        ;; curl ifconfig.me
        (setf port 60001
@@ -65,17 +66,57 @@
 	  ("Exception as e"
 	   (print (fstring "exception {e}")))
 	  (finally (sock.close))))
-       
-       (def run_self_on_remote (host)
+
+       (def run_cmd_and_wait (host cmds)
 	 (with (as (open __file__ (string "rb"))
 		   f)
 	       (setf script_content (f.read)))
-	 (setf cmd (list (string "ssh")
-			 host
-			 (string "python3")
-			 (string "-")
+	 (setf cmd (+ (list (string "ssh")
+				 host
+				 )
+			   cmds))
+	 (print (fstring "run {cmd}"))
+	 (setf result
+	       (subprocess.run
+		cmd
+		;:input script_content
+		:capture_output True
+		:check False ;; prevent raising error on non-zero exit
+		:text False
+		))
+	 (print (fstring "remote script {result}"))
+	 (return (dot result stdout (decode (string "utf-8"))))
+	 )
+
+       (def run_local_cmd_and_wait (host cmds)
+	 (with (as (open __file__ (string "rb"))
+		   f)
+	       (setf script_content (f.read)))
+	 (setf cmd cmds)
+	 (print (fstring "run {cmd}"))
+	 (setf result
+	       (subprocess.run
+		cmd
+		;:input script_content
+		:capture_output True
+		:check False ;; prevent raising error on non-zero exit
+		:text False
+		))
+	 (print (fstring "local script {result}"))
+	 (return (dot result stdout (decode (string "utf-8"))))
+	 )
+       
+       (def run_self_on_remote (host cmds)
+	 (with (as (open __file__ (string "rb"))
+		   f)
+	       (setf script_content (f.read)))
+	 (setf cmd (+ (list (string "ssh")
+			  host
+			  (string "python3")
+			  (string "-")
 					;(str port)
-			 ))
+			  )
+		      cmds))
 	 (print (fstring "run {cmd}"))
 	 (setf process (Popen cmd
 			      :stdin PIPE
@@ -86,15 +127,72 @@
 	 )
        (when (== __name__ (string "__main__"))
 	   (print (fstring "start {sys.argv}"))
+	   (do0
+	    (setf parser (argparse.ArgumentParser))
+	    #+nil (parser.add_argument (string "-r")
+				 (string "--remote")
+				 :action (string "store_true")
+				 :help (string "script executes on remote host"))
+	    (parser.add_argument (string "-j")
+				 (string "--jump-host")
+				 :type str
+				 :default (string "tinyeu")
+				 :help (string "ssh hostname of jump host"))
+	    (parser.add_argument (string "-s")
+				 (string "--server-host")
+				 :type str
+				 :default (string "tux")
+				 :help (string "ssh hostname of server host"))
+	    (parser.add_argument (string "-S")
+				 (string "--server-ip")
+				 :type str
+				 :default (string "1.2.3.4")
+				 :help (string "ip of server host"))
+	    (parser.add_argument (string "-J")
+				 (string "--jump-ip")
+				 :type str
+				 :default (string "1.3.3.4")
+				 :help (string "ip of jump host"))
+	    (parser.add_argument (string "-L")
+				 (string "--local-ip")
+				 :type str
+				 :default (string "1.4.3.4")
+				 :help (string "ip of local computer"))
+	    (setf args (parser.parse_args)))
 	   (if (== (aref sys.argv 0)
 		   (string "-"))
 	       (do0
 		(comments "remote running script name is '-'")
 		(print (string "remote script"))
-		(emit any_ip client_ip))
+		#+nil (emit any_ip client_ip))
 	       (do0
 		(print (string "local script"))
-		(run_self_on_remote (string "tux"))
+		(setf local_ip
+		      (run_local_cmd_and_wait (string "tux")
+					      (list (string "curl")
+						    (string "ifconfig.me"))
+					      )
+		      )
+		(setf server_ip
+		      (run_cmd_and_wait (string "tux")
+					(list (string "curl")
+					      (string "ifconfig.me"))
+					)
+
+		      )
+		(setf jump_ip
+		      (run_cmd_and_wait (string "tinyeu")
+					(list (string "curl")
+					      (string "ifconfig.me"))
+					)
+		      )
+		(run_self_on_remote (string "tux")
+				    (list (string "-L")
+					  local_ip
+					  (string "-J")
+					  jump_ip
+					  (string "-S")
+					  server_ip))
 		(emit any_ip server_ip)))
 	   ))))
   )
