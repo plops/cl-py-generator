e2e68a348185b25d5fc9e2bfcb4bde7149069f66
Martin Kielhorn <wolpumba@gmail.com>
2025-08-17 11:33:42 +0200

    update Dockerfile and build scripts for newer Gentoo images; add additional packages to accept keywords and licenses

--- example/110_gentoo/docker_min_hpz6/setup01_build_image.sh
+++ example/110_gentoo/docker_min_hpz6/setup01_build_image.sh
@@ -1,4 +1,6 @@
 # Build the docker image based on a gentoo stage3 image
 
 # use plain to show container output (e.g. user password)
-DOCKER_BUILDKIT=1 docker build -t gentoo-z6-min --progress=plain .
+DOCKER_BUILDKIT=1 docker build \
+    --build-arg http_proxy="http://10.60.120.81:3142" \
+    -t gentoo-z6-min --progress=plain .
