4b149f7bd2ab1d17a5718fc486371ba2f2ab6701
wol <wolpumba@gmail.com>
2025-09-30 06:32:02 +0000

    plan work

--- example/161_sqlite_embed/gen01.lisp
+++ example/161_sqlite_embed/gen01.lisp
@@ -13,4 +13,23 @@
 				(merge-pathnames #P"p01_data.py"
 						 "example/161_sqlite_embed/"))
  `(do0
-    (imports (np numpy))))
+    (imports ((np numpy)
+	      (pd pandas)))
+    (imports-from (sqlite_minutils *))
+    (setf db (Database (string "/home/kiel/summaries_20250929.db"))
+	  tab (db.table (string "items")))))
+
+
+;; convert a subset of the columns to pandas
+;; filter out bad summaries
+;; compute 4d and 2d umap
+;; compute hdbscan clusters
+;; find names for the clusters
+
+;; store the umap so that new emedding entries can be assigned a 2d position and a cluster name
+
+;; visualize the full map
+
+;; display a segment of the full map in the neighborhood of a new embedding entry
+
+
