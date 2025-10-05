df3e54d1a5bdfcf4afe1983abd9b28684617909b
wol <wolpumba@gmail.com>
2025-10-05 06:36:22 +0000

    sse example works but something is still weird:
      File "/home/kiel/stage/cl-py-generator/example/163_fasthtml_sse/.venv/lib/python3.13/site-packages/starlette/_exception_handler.py", line 53, in wrapped_app
        raise exc
      File "/home/kiel/stage/cl-py-generator/example/163_fasthtml_sse/.venv/lib/python3.13/site-packages/starlette/_exception_handler.py", line 42, in wrapped_app
        await app(scope, receive, sender)
      File "/home/kiel/stage/cl-py-generator/example/163_fasthtml_sse/.venv/lib/python3.13/site-packages/starlette/routing.py", line 76, in app
        await response(scope, receive, send)
      File "/home/kiel/stage/cl-py-generator/example/163_fasthtml_sse/.venv/lib/python3.13/site-packages/starlette/responses.py", line 270, in __call__
        with collapse_excgroups():
             ~~~~~~~~~~~~~~~~~~^^
      File "/usr/lib/python3.13/contextlib.py", line 162, in __exit__
        self.gen.throw(value)
        ~~~~~~~~~~~~~~^^^^^^^
      File "/home/kiel/stage/cl-py-generator/example/163_fasthtml_sse/.venv/lib/python3.13/site-packages/starlette/_utils.py", line 85, in collapse_excgroups
        raise exc
      File "/home/kiel/stage/cl-py-generator/example/163_fasthtml_sse/.venv/lib/python3.13/site-packages/starlette/responses.py", line 274, in wrap
        await func()
      File "/home/kiel/stage/cl-py-generator/example/163_fasthtml_sse/.venv/lib/python3.13/site-packages/starlette/responses.py", line 254, in stream_response
        async for chunk in self.body_iterator:
        ...<2 lines>...
            await send({"type": "http.response.body", "body": chunk, "more_body": True})
      File "/home/kiel/stage/cl-py-generator/example/163_fasthtml_sse/p01_top.py", line 40, in number_generator
        await time.sleep(1)

--- example/163_fasthtml_sse/gen01.lisp
+++ example/163_fasthtml_sse/gen01.lisp
@@ -13,15 +13,10 @@
 				(merge-pathnames #P"p01_top"
 						 "example/163_fasthtml_sse/"))
  `(do0
-   
-   (imports-from (fasthtml.common *))
-       
-   (imports-from
-					
-    (loguru logger)
-    )
-   
-   
+   (imports (random time))
+   (imports-from (loguru logger)
+		 (fasthtml.common *))
+          
    (do0
     (logger.remove)
     (logger.add
@@ -33,6 +28,36 @@
      ))
 
     (logger.info (string "Logger configured"))
+
+
+   (comments "https://www.npmjs.com/package/htmx-ext-sse https://x.com/jeremyphoward/status/1829897065440428144/photo/1")
+
+   (setf hdrs (tuple (Script :src (string "https://unpkg.com/htmx-ext-sse@2.2.3/sse.js")))
+	 (ntuple app rt) (fast_app :hdrs hdrs))
+
+   (do0
+    @rt
+    (def index ()
+      (return (ntuple
+	       (Titled (string "SSE Random Number Generator")
+		       (P (string "Generate pairs of random numbers, as the list grows scroll downwards."))
+		       (Div :hx_ext (string "sse")
+			    :sse_connect (string "/number-stream")
+			    :hx_swap (string "beforeend show:bottom")
+			    :sse_swap (string "message")))))))
+
+   (setf shutdown_event (signal_shutdown))
+   (space async (def number_generator ()
+		  (while (not (shutdown_event.is_set))
+			 (setf data (Div (Article (random.randint 1 100))
+					 (Article (random.randint 1 100))))
+			 (yield (sse_message data))
+			 (await (time.sleep 1)))))
+
+   (@rt (string "/number-stream"))
+   (space async (def get ()
+		  (return (EventStream (number_generator)))))
+   (serve)
 ))
 
 
--- example/163_fasthtml_sse/p01_top.py
+++ example/163_fasthtml_sse/p01_top.py
@@ -1,5 +1,7 @@
-from fasthtml.common import *
+import random
+import time
 from loguru import logger
+from fasthtml.common import *
 
 logger.remove()
 logger.add(
@@ -9,3 +11,38 @@ logger.add(
     level="DEBUG",
 )
 logger.info("Logger configured")
+# https://www.npmjs.com/package/htmx-ext-sse https://x.com/jeremyphoward/status/1829897065440428144/photo/1
+hdrs = (Script(src="https://unpkg.com/htmx-ext-sse@2.2.3/sse.js"),)
+app, rt = fast_app(hdrs=hdrs)
+
+
+@rt
+def index():
+    return Titled(
+        "SSE Random Number Generator",
+        P("Generate pairs of random numbers, as the list grows scroll downwards."),
+        Div(
+            hx_ext="sse",
+            sse_connect="/number-stream",
+            hx_swap="beforeend show:bottom",
+            sse_swap="message",
+        ),
+    )
+
+
+shutdown_event = signal_shutdown()
+
+
+async def number_generator():
+    while not (shutdown_event.is_set()):
+        data = Div(Article(random.randint(1, 100)), Article(random.randint(1, 100)))
+        yield (sse_message(data))
+        await time.sleep(1)
+
+
+@rt("/number-stream")
+async def get():
+    return EventStream(number_generator())
+
+
+serve()
