a3e8ca2ffe95bc4cac00ceddb380bfea8f618bd2
wol <wolpumba@gmail.com>
2025-10-10 05:04:15 +0200

    chore: update Python version requirement and modify setup script for JIT
    try pypy
    
    a fasthtml dependency fails to install:
    
          /home/kiel/.cache/uv/builds-v0/.tmpCAmhrw/lib/pypy3.11/site-packages/setuptools/_distutils/dist.py:289: UserWarning: Unknown distribution option: 'test_suite'
            warnings.warn(msg)
          /home/kiel/.cache/uv/builds-v0/.tmpCAmhrw/lib/pypy3.11/site-packages/setuptools/dist.py:759: SetuptoolsDeprecationWarning: License classifiers are deprecated.
          !!
    
                  ********************************************************************************
                  Please consider removing the following classifiers in favor of a SPDX license expression:
    
                  License :: OSI Approved :: MIT License
    
                  See https://packaging.python.org/en/latest/guides/writing-pyproject-toml/#license for details.
                  ********************************************************************************
    
          !!
            self._finalize_license_expression()
          httptools/parser/parser.c:15388:12: fatal error: internal/pycore_frame.h: No such file or directory
          15388 |   #include "internal/pycore_frame.h"
                |            ^~~~~~~~~~~~~~~~~~~~~~~~~
          compilation terminated.
          error: command '/usr/bin/gcc' failed with exit code 1
    
          hint: This error likely indicates that you need to install a library that provides "internal/pycore_frame.h" for `httptools@0.6.4`
      help: `httptools` (v0.6.4) was included because `163-fasthtml-sse` (v0.1.0) depends on `python-fasthtml` (v0.12.29) which depends on `uvicorn[standard]` (v0.37.0) which depends on `httptools`

--- example/165_fasthtml_sse_genai/setup01_run.sh
+++ example/165_fasthtml_sse_genai/setup01_run.sh
@@ -2,7 +2,9 @@
 
 export GEMINI_API_KEY=`cat ~/api_key.txt`
 export PATH="/home/kiel/.local/bin:$PATH"
+
 # enable jit
-export PYTHON_JIT=1 
-uv run python -i p01_top.py -vv
- 
+# export PYTHON_JIT=1 ;uv run python p01_top.py -vv
+
+uv run --python pypy3 p01_top.py -vv
+
