898dc59af4d5611b6208878ed8541f498d9e621a
wol <wolpumba@gmail.com>
2025-10-10 05:11:40 +0200

    chore: enable JIT in setup script and update comments for clarity

--- example/165_fasthtml_sse_genai/setup01_run.sh
+++ example/165_fasthtml_sse_genai/setup01_run.sh
@@ -4,7 +4,8 @@ export GEMINI_API_KEY=`cat ~/api_key.txt`
 export PATH="/home/kiel/.local/bin:$PATH"
 
 # enable jit
-# export PYTHON_JIT=1 ;uv run python p01_top.py -vv
+ export PYTHON_JIT=1 ;uv run python p01_top.py -vv
 
-uv run --python pypy3 p01_top.py -vv
+# alternative: try pypy (fasthtml doesn't work with that)
+#uv run --python pypy3 p01_top.py -vv
 
