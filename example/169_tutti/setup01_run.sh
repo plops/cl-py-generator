aae5c1c394cff77cac139f6b5e8b6a2ef029ee92
wol <wolpumba@gmail.com>
2025-12-06 08:46:50 +0100

    feat(setup): update parameters for e03_watch.py execution

--- example/169_tutti/setup01_run.sh
+++ example/169_tutti/setup01_run.sh
@@ -1,3 +1,3 @@
 export GEMINI_API_KEY=`cat ~/api_key.txt`
-uv run python  e03_watch.py -l w.log -p 300 --debug --dry-run -m 13 -M 90 --category phones
+uv run python  e03_watch.py -l w.log -p 300 --debug --dry-run -m 27 -M 167 --category phones
 uv run python  e03_watch.py -l w.log -p 300 --debug --dry-run -m 13 -M 90 --category watches
