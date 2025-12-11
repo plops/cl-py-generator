986bd1457c6039d2c7982adfe60d970586378344
wol <wolpumba@gmail.com>
2025-12-11 01:15:42 +0100

    feat(setup): add additional execution command for e03_watch.py

--- example/169_tutti/setup01_run.sh
+++ example/169_tutti/setup01_run.sh
@@ -1,3 +1,4 @@
 export GEMINI_API_KEY=`cat ~/api_key.txt`
-uv run python  e03_watch.py -l w.log -p 300 --debug --dry-run -m 27 -M 167 --category phones
 uv run python  e03_watch.py -l w.log -p 300 --debug --dry-run -m 13 -M 90 --category watches
+uv run python  e03_watch.py -l w.log -p 300 --debug --dry-run -m 27 -M 167 --category phones
+
