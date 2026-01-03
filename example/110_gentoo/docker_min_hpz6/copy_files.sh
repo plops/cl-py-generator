d2483f62e6da286ca4a81ef629c1e53db5b3fb3e
wol <wolpumba@gmail.com>
2026-01-03 23:22:55 +0100

    feat(copy_files): add package list generation to copy_files.sh

--- example/110_gentoo/docker_min_hpz6/copy_files.sh
+++ example/110_gentoo/docker_min_hpz6/copy_files.sh
@@ -10,3 +10,4 @@ cp /boot/vmlinuz /tmp/outside/${TARGET}/
 cp /boot/initramfs_squash_sda1-x86_64.img /tmp/outside/${TARGET}/
 cp /boot/initramfs_squash_from_disk.img /tmp/outside/${TARGET}/
 cp /boot/initramfs-with-ssh.img /tmp/outside/${TARGET}/
+qlist -Iv > /tmp/outside/packages.txt
