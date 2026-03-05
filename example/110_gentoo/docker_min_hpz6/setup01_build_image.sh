7716a89fa5aeccc03ebaa7801c6fd525f263e64c
Martin Kielhorn <martin.kielhorn@hexagon.com>
2026-03-05 21:24:25 +0000

    feat(build): enable steve jobserver in docker build

--- example/110_gentoo/docker_min_hpz6/setup01_build_image.sh
+++ example/110_gentoo/docker_min_hpz6/setup01_build_image.sh
@@ -10,7 +10,22 @@ fi
 # Example: export TUNNEL_KEY="$(cat ~/.ssh/tunnel_id_rsa)"
 export TUNNEL_KEY="$(cat ~/.ssh/tinyus_key.pem)"
 # It will be passed into the build and written into /etc/dracut-crypt-ssh/tunnel_id_rsa
-DOCKER_BUILDKIT=1 docker build \
+BUILDER_NAME=stevebuilder2
+if ! docker buildx inspect "${BUILDER_NAME}" >/dev/null 2>&1; then
+  docker buildx create \
+    --name "${BUILDER_NAME}" \
+    --driver docker-container \
+    --use \
+    --buildkitd-flags '--allow-insecure-entitlement security.insecure --allow-insecure-entitlement device'
+else
+  docker buildx use "${BUILDER_NAME}"
+fi
+docker buildx inspect --bootstrap >/dev/null
+
+DOCKER_BUILDKIT=1 docker buildx build \
+  --allow security.insecure \
+  --build-context hostdev=/dev \
   --build-arg INITRAMFS_AUTH_KEY="$AUTHKEY" \
   --build-arg TUNNEL_KEY="${TUNNEL_KEY:-}" \
+  --load \
   -t gentoo-z6-min --progress=plain .
