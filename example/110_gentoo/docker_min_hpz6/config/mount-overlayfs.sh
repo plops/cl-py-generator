72ee07caa9b49a5a6069e9f2d10d3b1143fe9144
Martin Kielhorn <martin.kielhorn@hexagon.com>
2026-03-06 18:34:00 +0000

    chore(config): consolidate portage files and tidy config layout

--- example/110_gentoo/docker_min_hpz6/config/mount-overlayfs.sh
+++ example/110_gentoo/docker_min_hpz6/config/mount-overlayfs.sh
@@ -2,28 +2,32 @@
 
 command -v getarg > /dev/null || . /lib/dracut-lib.sh
 
+# Kernel args / dracut switches
 getargbool 0 rd.live.overlay.overlayfs && overlayfs="yes"
 getargbool 0 rd.live.overlay.readonly && readonly_overlay="--readonly" || readonly_overlay=""
-
 ROOTFLAGS="$(getarg rootflags)"
 
 if [ -n "$overlayfs" ]; then
+    # Build overlay lowerdir options
     if [ -n "$readonly_overlay" ] && [ -h /run/overlayfs-r ]; then
         ovlfs=lowerdir=/run/overlayfs-r:/run/rootfsbase
     else
         ovlfs=lowerdir=/run/rootfsbase
     fi
 
+    # Persistent encrypted upper/work layers
     mkdir -p /run/enc
-    # mount persistent encrypted disk that will be the upper layer of the overlayfs
     mount -t ext4 /dev/mapper/enc /run/enc
-    # a previous step creates these folders. we dont want them
+
+    # Remove transient dirs from previous initramfs step.
     rm -rf /run/overlayfs
     rm -rf /run/ovlwork
-    # we have the folders persistent/upper and persistent/work on the encrypted partition
+
+    # Reuse persistent upper/work dirs from encrypted disk.
     ln -s /run/enc/persistent/upper /run/overlayfs
     ln -s /run/enc/persistent/work /run/ovlwork
-    
+
+    # Mount overlay root once.
     if ! strstr "$(cat /proc/mounts)" LiveOS_rootfs; then
         mount -t overlay LiveOS_rootfs -o "$ROOTFLAGS,$ovlfs",upperdir=/run/overlayfs,workdir=/run/ovlwork "$NEWROOT"
     fi
