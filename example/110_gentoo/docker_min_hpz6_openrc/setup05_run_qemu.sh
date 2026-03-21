e9538d49d711c8d8f5b8dc3efdd415b6651a9ba5
Martin Kielhorn <martin.kielhorn@hexagon.com>
2026-03-21 21:45:46 +0000

    feat(qemu): add console mode selection

--- example/110_gentoo/docker_min_hpz6_openrc/setup05_run_qemu.sh
+++ example/110_gentoo/docker_min_hpz6_openrc/setup05_run_qemu.sh
@@ -12,6 +12,7 @@ INITRAMFS_PATH="${QEMU_DIR}/initramfs_squash_sda1-x86_64.img"
 CMDLINE_PATH="${QEMU_DIR}/cmdline.txt"
 MEMORY="${QEMU_MEMORY:-4096}"
 SMP_COUNT="${QEMU_SMP:-4}"
+CONSOLE_MODE="${QEMU_CONSOLE_MODE:-curses}"
 
 if [[ ! -f "${IMAGE_PATH}" ]]; then
   echo "missing image: ${IMAGE_PATH}" >&2
@@ -33,6 +34,26 @@ else
   ACCEL_ARGS=(-accel tcg -cpu max)
 fi
 
+DISPLAY_ARGS=()
+case "${CONSOLE_MODE}" in
+  serial)
+    DISPLAY_ARGS=(-serial mon:stdio -display none)
+    ;;
+  curses)
+    CMDLINE="$(
+      printf '%s\n' "${CMDLINE}" \
+        | sed -E 's/(^| )console=ttyS0,115200//g; s/(^| )console=tty0/ console=tty1/g' \
+        | tr -s ' '
+    ) consoleblank=0"
+    DISPLAY_ARGS=(-display curses -serial none -monitor none)
+    ;;
+  *)
+    echo "unsupported QEMU_CONSOLE_MODE: ${CONSOLE_MODE}" >&2
+    echo "supported modes: serial, curses" >&2
+    exit 1
+    ;;
+esac
+
 exec qemu-system-x86_64 \
   -machine q35 \
   "${ACCEL_ARGS[@]}" \
@@ -44,6 +65,5 @@ exec qemu-system-x86_64 \
   -drive "file=${IMAGE_PATH},format=raw,if=virtio" \
   -netdev user,id=n1 \
   -device virtio-net-pci,netdev=n1 \
-  -serial mon:stdio \
-  -display none \
+  "${DISPLAY_ARGS[@]}" \
   -no-reboot
