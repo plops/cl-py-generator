0064291cfd0b92da511f635e3362bcc17969fa27
Martin Kielhorn <martin.kielhorn@hexagon.com>
2026-03-22 09:49:07 +0000

    feat(gentoo-openrc): add spark/nvidia image variants and artifact checks

--- example/110_gentoo/docker_min_hpz6_openrc/copy_files.sh
+++ example/110_gentoo/docker_min_hpz6_openrc/copy_files.sh
@@ -5,9 +5,11 @@ TODAY=$(date +%Y%m%d)
 TARGET=gentoo-z6-min-openrc_${TODAY}
 mkdir -p /tmp/outside/${TARGET}
 cp /gentoo.squashfs /tmp/outside/${TARGET}/
+cp /gentoo.squashfs_nv /tmp/outside/${TARGET}/ || true
 cp /gentoo.ext4 /tmp/outside/${TARGET}/ || true
 cp /gentoo.squashfs_e14 /tmp/outside/${TARGET}/ || true
 cp /boot/vmlinuz /tmp/outside/${TARGET}/
 cp /boot/initramfs_squash_sda1-x86_64.img /tmp/outside/${TARGET}/
-qlist -Iv > /tmp/outside/${TARGET}/packages.txt
+cp /packages.txt /tmp/outside/${TARGET}/packages.txt || qlist -Iv > /tmp/outside/${TARGET}/packages.txt
+cp /packages.tsv /tmp/outside/${TARGET}/packages.tsv || true
 chmod -R a+rwx /tmp/outside/${TARGET}
