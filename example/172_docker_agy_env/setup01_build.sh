12bc5a00e1e136e0b2d0b259256139c7166e913e
KIELHORN Martin <martin.kielhorn@hexagon.com>
2026-07-01 14:07:31 +0000

    more scripts

---
+++ example/172_docker_agy_env/setup01_build.sh
@@ -0,0 +1,1 @@
+docker build -t antigravity-sandbox:26.04 .
---
+++ example/172_docker_agy_env/setup02_run.sh
@@ -0,0 +1,8 @@
+#docker run -it \
+#  -e GEMINI_API_KEY="your_actual_gemini_api_key" \
+#  -v /path/to/your/local/code:/workspace/src \
+#  antigravity-sandbox:26.04
+
+docker run -it \
+  -v /path/to/your/local/code:/workspace/src \
+  antigravity-sandbox:26.04
