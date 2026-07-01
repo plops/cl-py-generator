f7f966b9029a0a49a8448ee5850fc4ec9c9cf10e
KIELHORN Martin <martin.kielhorn@hexagon.com>
2026-07-01 14:18:18 +0000

    chore: add Antigravity CLI alias and update Docker run command in setup script

--- example/172_docker_agy_env/setup02_run.sh
+++ example/172_docker_agy_env/setup02_run.sh
@@ -3,6 +3,12 @@
 #  -v /path/to/your/local/code:/workspace/src \
 #  antigravity-sandbox:26.04
 
+
+# Create the folder on your host first so Docker doesn't generate it as 'root'
+mkdir -p "$HOME/.gemini"
+
 docker run -it \
-  -v /path/to/your/local/code:/workspace/src \
+  -e ANTIGRAVITY_PLAINTEXT_AUTH=1 \
+  -v "$HOME/.gemini:/root/.gemini" \
+  -v "/home/kiel/stage:/workspace/src" \
   antigravity-sandbox:26.04
